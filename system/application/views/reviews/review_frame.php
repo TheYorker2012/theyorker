e7afb95f01ae0c91033697eb77774818e6fe6878
Richard Rout <rr512@york.ac.uk>
2006-11-23 14:43:00 +0000

    Faffing - review section

--- system/application/views/reviews/review_frame.php
+++ system/application/views/reviews/review_frame.php
@@ -14,33 +14,49 @@
 			<img alt="<?php echo $food ?>" src="/images/prototype/reviews/reviews_07.jpg" />
 			<br />
 			<?php echo $foodtext ?>
+			<div class="WhyNotTry">
+			<h3>Why Not Try?</h3>
+			<ul>
+				<li>Chicken
+				<li>Beef
+				<li>Duck
+				<li>Tuna
+				<li>Pork
+			</ul>
+			</div>
 		</div>
 		<div class="ReviewSet">
 			<h2><?php echo $drink ?></h2>
 			<img alt="<?php echo $drink ?>" src="/images/prototype/reviews/reviews_07.jpg" />
 			<br />
 			<?php echo $drinktext ?>
+			<div class="WhyNotTry">
+			<h3>Why Not Try?</h3>
+			<ul>
+				<li>Coke
+				<li>Hob Goblin Ale
+				<li>Liquified Cake
+				<li>Anal Rape
+				<li>Warm Milk
+			</ul>
+			</div>
 		</div>
 		<div class="ReviewSet">
 			<h2><?php echo $culture ?></h2>
 			<img alt="<?php echo $culture ?>" src="/images/prototype/reviews/reviews_07.jpg" />
 			<br />
 			<?php echo $culturetext ?>
+			<div class="WhyNotTry">
+			<h3>Why Not Try?</h3>
+			<ul>
+				<li>Budgie-Jumping
+				<li>Parrot-Shooting
+				<li>Hen-Gliding
+			</ul>
+			</div>
 		</div>
+		<div class="clear">&nbsp;</div>
 	</div>
-		<div class="WhyNotTry">
-			no<br />dousch<br />
-		</div>
-		<div class="WhyNotTry">
-			<br />
-			Why Not Try?<br />
-			I AM COOL
-		</div>
-		<div class="WhyNotTry">
-			<br />
-			Why Not Try?<br />
-			I AM COOL
-		</div>
 <!-- Writer Footer -->
   </body>
 </html>
