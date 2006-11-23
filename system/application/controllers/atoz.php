f9d0a5a90fdb090b998561078f4031f635395211
James Hogan <james_hogan@theyorker.co.uk>
2006-11-23 14:39:36 +0000

    spaces into tabs

--- system/application/controllers/atoz.php
+++ system/application/controllers/atoz.php
@@ -7,12 +7,12 @@
  */
 class Atoz extends Controller {
 	/**
-	* Displays the AtoZ for organisations of a particular type
-	* \param organisation_type_id specifies the filter on organisation_type_id.
-	*/
-    function type($organisation_type_id)
-    {
-        $this->load->model('Atoz_model'); //Load the model
+	 * Displays the AtoZ for organisations of a particular type
+	 * \param organisation_type_id specifies the filter on organisation_type_id.
+	 */
+	function type($organisation_type_id)
+	{
+		$this->load->model('Atoz_model'); //Load the model
 		
 		$this->load->library('character_lib'); //This character libary is used by the view, so load it here
 
@@ -21,7 +21,7 @@ class Atoz extends Controller {
 			'organisation_array' => $this->Atoz_model->get_all_organisations_of_type($organisation_type_id)
 		);
 		$this->load->view('frames/student_frame',$data);
-    }
+	}
 }
 
 ?>
