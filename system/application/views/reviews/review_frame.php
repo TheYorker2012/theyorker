121b1d993d61636729da7adfcfb28a44fe978c0d
Richard Rout <rr512@york.ac.uk>
2006-11-23 11:44:14 +0000

    Review page creation started

---
+++ system/application/views/reviews/review_frame.php
@@ -0,0 +1,47 @@
+<!--Writer Header -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <title><?php echo $title?></title>
+    <link rel="stylesheet" href="/stylesheets/stylesheet.css" type="text/css" />
+  </head>
+  <body>
+<!-- Page start -->
+	<div id="TitleBar"><h1><?php echo $title ?></h1></div>
+	<div id="ReviewSetBlock">
+		<div class="ReviewSet">
+			<h2><?php echo $food ?></h2>
+			<img alt="<?php echo $food ?>" src="/images/prototype/reviews/reviews_07.jpg" />
+			<br />
+			<?php echo $foodtext ?>
+		</div>
+		<div class="ReviewSet">
+			<h2><?php echo $drink ?></h2>
+			<img alt="<?php echo $drink ?>" src="/images/prototype/reviews/reviews_07.jpg" />
+			<br />
+			<?php echo $drinktext ?>
+		</div>
+		<div class="ReviewSet">
+			<h2><?php echo $culture ?></h2>
+			<img alt="<?php echo $culture ?>" src="/images/prototype/reviews/reviews_07.jpg" />
+			<br />
+			<?php echo $culturetext ?>
+		</div>
+	</div>
+		<div class="WhyNotTry">
+			no<br />dousch<br />
+		</div>
+		<div class="WhyNotTry">
+			<br />
+			Why Not Try?<br />
+			I AM COOL
+		</div>
+		<div class="WhyNotTry">
+			<br />
+			Why Not Try?<br />
+			I AM COOL
+		</div>
+<!-- Writer Footer -->
+  </body>
+</html>
+
