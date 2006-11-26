9f898c61615e67c8606446a97fc68d2f441e7a5c
Nick Evans <nse500@york.ac.uk>
2006-11-26 14:15:24 +0000

    Updated atoz for directory

--- system/application/controllers/atoz.php
+++ system/application/controllers/atoz.php
@@ -3,17 +3,17 @@
  * This controller displays the A to Z page.
  * It uses the Atoz_model, and the character_lib to display an interactive A to Z page.
  *
- * \author Nick Evans
+ * @author Nick Evans
  */
 class Atoz extends Controller {
 	/**
 	 * Displays the AtoZ for organisations of a particular type
-	 * \param organisation_type_id specifies the filter on organisation_type_id.
+	 * @param organisation_type_id specifies the filter on organisation_type_id.
 	 */
 	function type($organisation_type_id)
 	{
 		$this->load->model('Atoz_model'); //Load the model
-		
+
 		$this->load->library('character_lib'); //This character libary is used by the view, so load it here
 
 		$data = array(
@@ -22,6 +22,23 @@ class Atoz extends Controller {
 		);
 		$this->load->view('frames/student_frame',$data);
 	}
+
+	/**
+	 * Displays the AtoZ for all organisations
+	 */
+	function directory()
+	{
+		$this->load->model('Atoz_model'); //Load the model
+
+		$this->load->library('character_lib'); //This character libary is used by the view, so load it here
+
+		$data = array(
+			'content_view' => 'general/atoz',
+			'organisation_array' => $this->Atoz_model->get_all_organisations()
+		);
+		$this->load->view('frames/student_frame',$data);
+	}
+
 }
 
 ?>
