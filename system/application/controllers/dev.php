acb276d556ef83bec8e780e886fbcae7178cc581
Mark Goodall <mg512@york.ac.uk>
2006-11-29 23:19:08 +0000

    added a dev page so you can see if its stopped updating, v basic

---
+++ system/application/controllers/dev.php
@@ -0,0 +1,13 @@
+<?php
+
+class Dev extends Controller {
+
+	function index()
+	{
+		echo 'Devr\'s Status page\n';
+		echo 'If you think this is wrong then email mg512\n';
+		echo 'Info dumps follow:\n';
+		echo system('svn info');
+	}
+}
+?>
