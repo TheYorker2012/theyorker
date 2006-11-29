183d25429fc9a40b5a4e8db6433b2fecc53f07e7
Frazer Barnsley <frazzle@gmail.com>
2006-11-29 23:12:02 +0000

    Added add how do I page

---
+++ system/application/views/faq/addhowdoi.php
@@ -0,0 +1,8 @@
+        <form name='a_search_form' id='a_search_form' action='<?php echo site_url('faq/addhowdoi'); ?>' method='post'>
+            <fieldset id='SearchForm' title='Add new how do I'>
+                <legend>Add new how do I</legend>
+                <p><br /><label for='question'>Question:</label> <input type='text' name='question' id='question' value='' /></p>
+                <p><label for='answer'>Answer:</label> <textarea name='answer' id='answer' rows='8' cols='50'></textarea></p>
+                <p><label for='submit'>&nbsp;</label><input type='submit' name='submit' id='submit' value='Add' class='submit' /></p>
+            </fieldset>
+        </form>
