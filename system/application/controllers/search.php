d939f3a69af20788e58c3a4d07a470ad501667bc
Mark Goodall <mg512@york.ac.uk>
2006-11-30 18:17:54 +0000

    Basic search pages

--- system/application/controllers/search.php
+++ system/application/controllers/search.php
@@ -5,7 +5,18 @@ class Search extends Controller {
 	function index()
 	{
 		$data = array(
-			'content_view' => 'search/search'
+			'content_view' => 'search/minibox'
+		);
+		$this->load->view('frames/student_frame',$data);
+	}
+	
+	function submit() {
+		//ajax calls to this
+	}
+	
+	function results() {
+		$data = array(
+			'content_view' => 'search/results'
 		);
 		$this->load->view('frames/student_frame',$data);
 	}
