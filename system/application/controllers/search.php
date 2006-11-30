670d53f0a264ea505015299a5f03bc12858b99fc
Mark Goodall <mg512@york.ac.uk>
2006-11-30 18:40:10 +0000

    less basic search pages, still not active

--- system/application/controllers/search.php
+++ system/application/controllers/search.php
@@ -1,5 +1,5 @@
 <?php
-
+//TODO Write comments
 class Search extends Controller {
 
 	function index()
@@ -11,10 +11,26 @@ class Search extends Controller {
 	}
 	
 	function submit() {
-		//ajax calls to this
+		//TODO ajax submit
+	}
+	
+	function get() {
+		$this->load->helper('url');
+		$this->load->library('session');
+		$search_keywords = $this->input->post('search');
+		$search_category = $this->input->post('category');
+		$url = url_title($search_keywords).'/'.url_title($search_category);
+		$data = array('search_keywords' => $search_keywords,
+		              'search_category' => $search_category,
+		              'url' => $url);
+		$this->session->set_userdata($data);
+		redirect('/search/results/'. $url, 'location');
 	}
 	
 	function results() {
+		$this->load->library('session');
+		//TODO do mysql query on details stored in session
+		//TODO display results
 		$data = array(
 			'content_view' => 'search/results'
 		);
