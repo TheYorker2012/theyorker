760e096fc5bf0c62ad839fb515186a45a9640aed
Owen Jones <oj502@york.ac.uk>
2006-12-02 13:22:48 +0000

    Updated version of directory/directory_view.php its now closer to the original spec.

--- system/application/views/directory/directory_view.php
+++ system/application/views/directory/directory_view.php
@@ -5,120 +5,50 @@
 	<div style='margin-left: 10px;'>
 		<form id='form1' name='form1' action='#' method='POST'>
 			<strong>Show:</strong> 
-			Organisations<input type='checkbox' name='searchrange' value="orgs" checked>
+			Venues<input type='checkbox' name='searchrange' value="venues" checked>
 			Societies<input type='checkbox' name='searchrange' value="socs" checked>
+			Athletics Union<input type='checkbox' name='searchrange' value="au" checked>
 			<br />
 			<input type='text' name='search'>
 			<input type='submit' name='Submit' value='Search'>
 		</form>
 	</div>
 </div>
-<div class='LeftToolbar' style='background-color: Teal; width: 19%; float: left; padding-left: 5px; padding-right: 5px;'>
+<div class='LeftToolbar' style='background-color: Teal; width: 24%; float: left; padding-left: 5px; padding-right: 5px;'>
 	<h3>FragSoc</h3>
 	<h4>Society</h4>
 	<img src='/images/prototype/reviews/reviews_07.jpg' width='100%' />
-	<h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla lorem magna, tincidunt sed, feugiat nec, consectetuer vitae, nisl. Vestibulum gravida ipsum non justo. Vivamus sem. Quisque ut sem vitae elit luctus lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h6>
+	<h5>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla lorem magna, tincidunt sed, feugiat nec, consectetuer vitae, nisl. Vestibulum gravida ipsum non justo. Vivamus sem. Quisque ut sem vitae elit luctus lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</h5>
+	<h6>Website: <a href='http://www.fragsoc.com'>www.fragsoc.com</a><br>
+	Location: Goodricke College<br>
+	Opening Times: Every Other Weekend</h6>
 </div>
-<div style='width: 79%; float: right;'>
-	<h2>Events:</h2>
-	<div style='background-color: #CED8D9;'>
-		<table border='1' width='100%'>
-			<tr>
-				<td>
-					1
-				</td>
-				<td>
-					2
-				</td>
-				<td>
-					3
-				</td>
-				<td>
-					4
-				</td>
-				<td>
-					5
-				</td>
-			</tr>
-			<tr>
-				<td>
-					1
-				</td>
-				<td>
-					2
-				</td>
-				<td>
-					3
-				</td>
-				<td>
-					4
-				</td>
-				<td>
-					5
-				</td>
-			</tr>
-			<tr>
-				<td>
-					1
-				</td>
-				<td>
-					2
-				</td>
-				<td>
-					3
-				</td>
-				<td>
-					4
-				</td>
-				<td>
-					5
-				</td>
-			</tr>
-			<tr>
-				<td>
-					1
-				</td>
-				<td>
-					2
-				</td>
-				<td>
-					3
-				</td>
-				<td>
-					4
-				</td>
-				<td>
-					5
-				</td>
-			</tr>
-			<tr>
-				<td>
-					1
-				</td>
-				<td>
-					2
-				</td>
-				<td>
-					3
-				</td>
-				<td>
-					4
-				</td>
-				<td>
-					5
-				</td>
-			</tr>
-		</table>
+<div style='width: 74%; float: right;'>
+	<h2>Upcoming events:</h2>
+	<div>
+		<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+		<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+		<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+		<h5><a href='#'>Watch this stream of events</a></h5>
+	</div>
+	<h2>Recent reviews:</h2>
+	<div>
+		<p><a href='/review/'>Food Review</a> by <a href='/contact/'>Brian Peppers</a> 1/1/1970</p>
+		<p><a href='/review/'>Food Review</a> by <a href='/contact/'>Brian Peppers</a> 1/1/1970</p>
+		<p><a href='/review/'>Food Review</a> by <a href='/contact/'>Brian Peppers</a> 1/1/1970</p>
+		<h5><a href='#'>Show all reviews</a></h5>
 	</div>
 	<h2>Members:</h2>
 	<div class='Memberbox'>
 		<div class='MemberboxText'>
-			<h3>Matthew Tole</h3>
+			<h3>Editor - Matthew Tole</h3>
 			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
 		</div>
 		<div class='MemberboxPhoto'>
-			<img src='/images/prototype/reviews/reviews_07.jpg' width='200'/>
+			<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+			<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
 		</div>
 		<div class="clear">&nbsp;</div>
 	</div>
+	<h5><a href='#'>Show all members</a></h5>
 </div>
