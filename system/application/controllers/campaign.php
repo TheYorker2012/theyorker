170173b9ad3b6799696ad776607e1f10ad9571fa
Mark Fairbairn <thebaron88@gmail.com>
2006-12-02 16:52:19 +0000

    Fixed from conflict.

--- system/application/controllers/campaign.php
+++ system/application/controllers/campaign.php
@@ -3,22 +3,19 @@ class Campaign extends Controller {
 	function index()
 	{
 		if(1==1){ // change to if deadline not passed then...
-			$subdata = array(
+			$data = array(
+				'content_view' => 'campaign/CampaignSelection',
 				'Description' => '<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!<b>testing blurb</b> and it <i>works!</i> Yay!',
 				'Picture' => 'http://localhost/images/prototype/campaign/field.jpg',
 				'DeadLine' => '23 May 2019'
 			);
-			$data = array(
-				//'content_view' => 'campaign/Index',
-				'content_view' => 'campaign/CampaignSelection',
-				'subdata' => $subdata
-			);
 			$this->load->view('frames/student_frame',$data);
 		} else { // Campaign is chosen
-			$subdata = array(
+			$data = array(
+				'content_view' => 'campaign/CampaignVote',
 				'Title' => 'Pie Eating',
 				'Picture' => 'http://localhost/images/prototype/campaign/field.jpg',
-				'Summary' => 'This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. This is the campaign Summary. ',
+				'Summery' => 'This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. ',
 				'NumOfSignatures' => '89546',
 				'Username' => 'Tom Jones',
 				'ProgressItems' => array(
@@ -27,26 +24,38 @@ class Campaign extends Controller {
 							array('good'=>'n','details'=>'Progress Report 3, Progress Report 3, Progress Report 3, Progress Report 3, Progress Report 3.')
 							)
 			);
-			$data = array(
-				'content_view' => 'campaign/CampaignVote',
-				'subdata' => $subdata
-			);
 			$this->load->view('frames/student_frame',$data);
 		}
 	}
+	
 	function Details($SelectedCampaign = 'Pie Eating')
 	{
-		$subdata = array(
+		$data = array(
+			'content_view' => 'campaign/CampaignDetials',
 			'Title' => $SelectedCampaign,
 			'Description' => 'Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!Lets all eat pie!',
 			'Author' => 'Mr Jones',
 			'Society' => 'Vegetarian Soc',
 			'Links' => 'www.ThisIsALink.com',
-			'Picture' => 'http://localhost/images/prototype/campaign/field.jpg',
+			'Picture' => 'http://localhost/images/prototype/campaign/field.jpg'
 		);
+		$this->load->view('frames/student_frame',$data);
+	}
+	
+	function Test($SelectedCampaign = 'Pie Eating')
+	{
 		$data = array(
-			'content_view' => 'campaign/CampaignDetails',
-			'subdata' => $subdata
+			'content_view' => 'campaign/CampaignVote',
+			'Title' => 'Pie Eating',
+			'Picture' => 'http://localhost/images/prototype/campaign/field.jpg',
+			'Summery' => 'This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. This is the campaign summery. ',
+			'NumOfSignatures' => '89546',
+			'Username' => 'Tom Jones',
+			'ProgressItems' => array(
+				array('good'=>'y','details'=>'Progress Report 1, Progress Report 1, Progress Report 1, Progress Report 1, Progress Report 1.'),
+				array('good'=>'n','details'=>'Progress Report 2, Progress Report 2, Progress Report 2, Progress Report 2, Progress Report 2.'),
+				array('good'=>'n','details'=>'Progress Report 3, Progress Report 3, Progress Report 3, Progress Report 3, Progress Report 3.')
+				)
 		);
 		$this->load->view('frames/student_frame',$data);
 	}
