26d996b5495bbd3d73decdcd0cfaceaa85f9c02c
Mark Fairbairn <thebaron88@gmail.com>
2006-12-03 17:07:20 +0000

    None functional prototypes complete.

--- system/application/views/campaign/CampaignsEditSelect.php
+++ system/application/views/campaign/CampaignsEditSelect.php
@@ -1,51 +1,12 @@
 	<br>
 	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
-	<div class="title">Campaign: <?php echo $Title; ?></div>
-	<div class="Blurb"><img src="<?php echo $Picture; ?>"><br><br><?php echo $Description; ?></div>
-		<div class="FactBox">
-			<div class="FactBoxTitle">Fact Box - Why Pool?</div>
-			<div class="FactBoxBullets">
-				<ul>
-					<li style="font-weight: bold; margin-top:3px;">First Important Fact</li>
-					<li style="font-weight: bold; margin-top:3px;">Second Important Fact</li>
-				</ul>
-			</div>
-			<div class="FactBoxBlurb">Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. </div>
-		</div>
-	<div class="funnybox">
-		<div class="funnyboxpt1">Find out more</div><br>
-		<div class="funnyboxpt2">Contact</div>
-		<div class="funnyboxpt3">><?php echo $Author; ?></div>
-		<div class="funnyboxpt4"><?php echo $Society; ?></div>
+	<div class="title">Edit Campaign</div>
+	<div class="subsubtitle">Please select a campaign to edit.</div>
+		<ul>
+			<a href="http://localhost/index.php/Campaign/Edit/Swimming Pool"><li style="font-weight: bold;">SWIMMING POOL</li></a>
+			<a href="http://localhost/index.php/Campaign/Edit/Anti-Arms"><li style="font-weight: bold;">ANTI-ARMS</li></a>
+			<a href="http://localhost/index.php/Campaign/Edit/Kill All Ants"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
+			<a href="http://localhost/index.php/Campaign/Edit/Buddhist Campus"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
+		</ul>
+		<input type="submit" value="New Campaign" style="width: 20%;">
 	</div>
-	<div class="Container">
-		<div class="HalfBox">
-			<div style="font-weight: bold; font-size: 20px;">RELATED NEWS STORIES</div>
-			<div style="font-weight: bold; font-size: 12px;">>BOY CAN FLY</div>
-		</div>
-		<div class="HalfBox">
-			<div style="font-weight: bold; font-size: 20px;">EXTERNAL LINKS</div>
-			<div style="font-weight: bold; font-size: 12px;">>BOY CAN FLY</div>
-		</div>
-	</div>
-	<BR><BR>
-	<div class="">
-		<div class="subheader">The Blog</div>
-		<div style="padding-top:10px;">
-			<div style="float: left;font-weight: bold;">John Doe</div><div style="float: right;font-weight: bold;">Mon Nov 6:05</div><br>
-			<div style="float: left;font-weight: bold;color: FF6A00;">No Place For Swimmers</div><br>
-			<div style="float: left;">Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text.</div>
-		</div>
-		<div style="padding-top:10px;">
-			<div style="float: left;font-weight: bold;">John Doe</div><div style="float: right;font-weight: bold;">Mon Nov 6:05</div><br>
-			<div style="float: left;font-weight: bold;color: FF6A00;">No Place For Swimmers</div><br>
-			<div style="float: left;">Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text.</div>
-		</div>
-		<div style="padding-top:10px;">
-			<div style="float: left;font-weight: bold;">John Doe</div><div style="float: right;font-weight: bold;">Mon Nov 6:05</div><br>
-			<div style="float: left;font-weight: bold;color: FF6A00;">No Place For Swimmers</div><br>
-			<div style="float: left;">Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text. Descriptive Text.</div>
-		</div>
-	</div>
-	</div>
-	<br>
