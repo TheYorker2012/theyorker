d51c0b1de54a24c21a4b9549a6215c5fdd89d7a2
Mark Fairbairn <thebaron88@gmail.com>
2006-12-03 16:26:14 +0000

    Fixed so that they load, they broke after svn update. Fixed links again so they look like the specification.

--- system/application/views/campaign/CampaignSelection.php
+++ system/application/views/campaign/CampaignSelection.php
@@ -1,16 +1,16 @@
 	<br>
 	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
 	<div class="title">CAMPAIGN</div>
-	<div class="Blurb"><img src="<?php echo $subdata['Picture']; ?>"><br><br><?php echo $subdata['Description']; ?></div>
+	<div class="Blurb"><img src="<?php echo $Picture; ?>"><br><br><?php echo $Description; ?></div>
 	<div class="subtitle">CHOOSE YOUR CAMPAIGN</div>
-	<div class="subsubtitle">CLICK ON THE CAMPAIGN TO FIND OUT MORE</div><Br>
+	<div class="subsubtitle">CLICK ON THE CANPAIGN TO FIND OUT MORE</div><Br>
 	<div class="Container">
 	<div class="HalfBox">
 		<ul>
-			<li style="font-weight: bold;">SWIMMING POOL</li>
-			<li style="font-weight: bold;">ANTI-ARMS</li>
-			<li style="font-weight: bold;">KILL ALL ANTS</li>
-			<li style="font-weight: bold;">BUDDHIST CAMPUS</li>
+			<li style="font-weight: bold;">>SWIMMING POOL</li>
+			<li style="font-weight: bold;">>ANTI-ARMS</li>
+			<li style="font-weight: bold;">>KILL ALL ANTS</li>
+			<li style="font-weight: bold;">>BUDDHIST CAMPUS</li>
 		</ul>
 	</div>
 	<div class="HalfBox">
@@ -27,13 +27,13 @@
 	<div class="breaker"></div> <!-- Remove This Hack Sometime -->
 	</div>
 	<div class="subtitle">CURRENT RESULTS</div>
-	<div class="subsubtitle">CHOISE DEADLINE FOR STUDENTS IS <?php echo $subdata['DeadLine']; ?></div><br>
+	<div class="subsubtitle">CHOISE DEADLINE FOR STUDENTS IS <?php echo $DeadLine; ?></div><br>
 	<div style="float: left;">
 		<ul>
-			<li style="font-weight: bold;margin-top:3px;">SWIMMING POOL</li>
-			<li style="font-weight: bold;margin-top:3px;">ANTI-ARMS</li>
-			<li style="font-weight: bold;margin-top:3px;">KILL ALL ANTS</li>
-			<li style="font-weight: bold;margin-top:3px;">BUDDHIST CAMPUS</li>
+			<li style="font-weight: bold;margin-top:3px;">>SWIMMING POOL</li>
+			<li style="font-weight: bold;margin-top:3px;">>ANTI-ARMS</li>
+			<li style="font-weight: bold;margin-top:3px;">>KILL ALL ANTS</li>
+			<li style="font-weight: bold;margin-top:3px;">>BUDDHIST CAMPUS</li>
 		</ul>
 	</div>
 	<div style="padding-left: 30px;float: left;"><div class="votebar" style="width: 50px;"></div>92<br><div class="votebar" style="width: 198px;"></div>198<br><div class="votebar" style="width: 213px;"></div>213<br><div class="votebar" style="width: 92px;"></div>92</div>
