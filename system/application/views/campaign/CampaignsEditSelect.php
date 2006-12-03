b50b95e39af5072b44ae0f562144e816822932f5
Mark Fairbairn <thebaron88@gmail.com>
2006-12-03 18:26:03 +0000

    Made links work

--- system/application/views/campaign/CampaignsEditSelect.php
+++ system/application/views/campaign/CampaignsEditSelect.php
@@ -3,10 +3,10 @@
 	<div class="title">Edit Campaign</div>
 	<div class="subsubtitle">Please select a campaign to edit.</div>
 		<ul>
-			<a href="http://localhost/index.php/Campaign/Edit/Swimming Pool"><li style="font-weight: bold;">SWIMMING POOL</li></a>
-			<a href="http://localhost/index.php/Campaign/Edit/Anti-Arms"><li style="font-weight: bold;">ANTI-ARMS</li></a>
-			<a href="http://localhost/index.php/Campaign/Edit/Kill All Ants"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
-			<a href="http://localhost/index.php/Campaign/Edit/Buddhist Campus"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
+			<a href="edit/Swimming Pool"><li style="font-weight: bold;">SWIMMING POOL</li></a>
+			<a href="edit/Anti-Arms"><li style="font-weight: bold;">ANTI-ARMS</li></a>
+			<a href="edit/Kill All Ants"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
+			<a href="edit/Buddhist Campus"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
 		</ul>
 		<input type="submit" value="New Campaign" style="width: 20%;">
 	</div>
