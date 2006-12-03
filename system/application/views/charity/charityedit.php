527df354e0e57688cd753f973f69de839236ec13
Mark Fairbairn <thebaron88@gmail.com>
2006-12-03 19:21:27 +0000

    Charity views relocated tot folder and edit dialogue created.

---
+++ system/application/views/charity/charityedit.php
@@ -0,0 +1,12 @@
+	<br>
+	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
+		<div class="title">Edit Charity Details</div>
+		Charity Detials: <br/>
+		<textarea style="width: 100%; height: 500px;"></textarea>
+		Goal Amount:<br/><input type="text" style="width: 40%;" /><br/>
+		Goal Detials:<br/>
+		<textarea style="width: 100%; height: 100px;"></textarea>
+		External Links:<br/><input type="text" style="width: 80%;" /><br/><br/>
+		<input type="submit" value="Save Changes" style="width: 20%;">
+	</div>
+	<br>
