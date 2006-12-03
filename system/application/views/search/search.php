b705de98ab8c34780c02b7adf795311fa416bbe4
Frazer Barnsley <frazzle@gmail.com>
2006-12-03 16:16:25 +0000

    The layout of the search page :)

---
+++ system/application/views/search/search.php
@@ -0,0 +1,60 @@
+
+<!-- a_search_form defined twice, remember to recreate into class !! :) //-->
+
+
+<div id='minibox'>
+    <div id='title'>Categorys</div>
+    <a href='#'><b>All results</b> <i>(112)</i></a>
+    <a href='#'>News <i>(41)</i></a>
+    <a href='#'>Reviews <i>(31)</i></a>
+    <a href='#'>Campaigns <i>(12)</i></a>
+    <a href='#'>News <i>(28)</i></a>
+</div>
+
+<div id='searchresults'>
+    <form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
+        <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
+        <input type='submit' name='submit' id='submit' value='Search' /></p>
+    </form>
+    <ol>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+        <li>
+            <a href="#">Some search results</a>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+            adipiscing nisi.
+        </li>
+    </ol>
+    <form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
+        <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
+        <input type='submit' name='submit' id='submit' value='Search' /></p>
+    </form>
+</div>
