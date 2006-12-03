1e65c65cf7141e77ba9e2e9b095162448bc02504
Frazer Barnsley <frazzle@gmail.com>
2006-12-03 19:25:10 +0000

    Updated to link to new page which holds answer

--- system/application/views/faq/howdoi.php
+++ system/application/views/faq/howdoi.php
@@ -11,26 +11,24 @@
         </div>
     </div>
     <h2>How Do I?</h2>
+    Some Catagory
     <ul>
-        <li><a href='#'>Something here</a></li>
-        <li><a href='#'>Something here</a></li>
-        <li><a href='#'>Something here</a></li>
-        <li><a href='#'>Something here</a></li>
-        <li><a href='#anchor-one'>Click to jump to question</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
     </ul>
+    Some Catagory
+    <ul>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+    </ul>
+    Some Catagory
+    <ul>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+        <li><a href='/faq/howdoi/1'>Something here</a></li>
+    </ul>
+
     <br />
-    <h3><a id="">Something here</a></h3>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam tempus risus in eros. Donec velit turpis, scelerisque eget, euismod vitae, pretium ut, mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ipsum felis, tincidunt malesuada, accumsan eu, pellentesque tincidunt, lorem. Proin consectetuer, risus quis ultricies scelerisque, justo mauris pretium augue, et vulputate est felis sed dui. Integer eu felis. Integer congue libero sit amet sapien. Sed id eros. Sed commodo lorem in ipsum. In hac habitasse platea dictumst. Morbi pellentesque ligula nec sem. Donec dictum, sapien ultrices facilisis bibendum, diam neque aliquet nibh, non ornare turpis odio in urna. Phasellus elementum. Vivamus lacus felis, posuere a, luctus eget, tristique vel, turpis. Suspendisse non tortor. Maecenas lectus justo, suscipit eu, sodales vitae, sagittis lobortis, metus. Nam vehicula felis ac metus. Pellentesque sit amet lorem et sem rhoncus congue.
-    <br />
-    <h3><a id="">Something here</a></h3>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam tempus risus in eros. Donec velit turpis, scelerisque eget, euismod vitae, pretium ut, mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ipsum felis, tincidunt malesuada, accumsan eu, pellentesque tincidunt, lorem. Proin consectetuer, risus quis ultricies scelerisque, justo mauris pretium augue, et vulputate est felis sed dui. Integer eu felis. Integer congue libero sit amet sapien. Sed id eros. Sed commodo lorem in ipsum. In hac habitasse platea dictumst. Morbi pellentesque ligula nec sem. Donec dictum, sapien ultrices facilisis bibendum, diam neque aliquet nibh, non ornare turpis odio in urna. Phasellus elementum. Vivamus lacus felis, posuere a, luctus eget, tristique vel, turpis. Suspendisse non tortor. Maecenas lectus justo, suscipit eu, sodales vitae, sagittis lobortis, metus. Nam vehicula felis ac metus. Pellentesque sit amet lorem et sem rhoncus congue.
-    <br />
-    <h3><a id="">Something here</a></h3>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam tempus risus in eros. Donec velit turpis, scelerisque eget, euismod vitae, pretium ut, mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ipsum felis, tincidunt malesuada, accumsan eu, pellentesque tincidunt, lorem. Proin consectetuer, risus quis ultricies scelerisque, justo mauris pretium augue, et vulputate est felis sed dui. Integer eu felis. Integer congue libero sit amet sapien. Sed id eros. Sed commodo lorem in ipsum. In hac habitasse platea dictumst. Morbi pellentesque ligula nec sem. Donec dictum, sapien ultrices facilisis bibendum, diam neque aliquet nibh, non ornare turpis odio in urna. Phasellus elementum. Vivamus lacus felis, posuere a, luctus eget, tristique vel, turpis. Suspendisse non tortor. Maecenas lectus justo, suscipit eu, sodales vitae, sagittis lobortis, metus. Nam vehicula felis ac metus. Pellentesque sit amet lorem et sem rhoncus congue.
-    <br />
-    <h3><a id="">Something here</a></h3>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam tempus risus in eros. Donec velit turpis, scelerisque eget, euismod vitae, pretium ut, mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ipsum felis, tincidunt malesuada, accumsan eu, pellentesque tincidunt, lorem. Proin consectetuer, risus quis ultricies scelerisque, justo mauris pretium augue, et vulputate est felis sed dui. Integer eu felis. Integer congue libero sit amet sapien. Sed id eros. Sed commodo lorem in ipsum. In hac habitasse platea dictumst. Morbi pellentesque ligula nec sem. Donec dictum, sapien ultrices facilisis bibendum, diam neque aliquet nibh, non ornare turpis odio in urna. Phasellus elementum. Vivamus lacus felis, posuere a, luctus eget, tristique vel, turpis. Suspendisse non tortor. Maecenas lectus justo, suscipit eu, sodales vitae, sagittis lobortis, metus. Nam vehicula felis ac metus. Pellentesque sit amet lorem et sem rhoncus congue.
-    <br />
-    <h3><a id="anchor-one">Click to jump to question</a></h3>
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam tempus risus in eros. Donec velit turpis, scelerisque eget, euismod vitae, pretium ut, mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ipsum felis, tincidunt malesuada, accumsan eu, pellentesque tincidunt, lorem. Proin consectetuer, risus quis ultricies scelerisque, justo mauris pretium augue, et vulputate est felis sed dui. Integer eu felis. Integer congue libero sit amet sapien. Sed id eros. Sed commodo lorem in ipsum. In hac habitasse platea dictumst. Morbi pellentesque ligula nec sem. Donec dictum, sapien ultrices facilisis bibendum, diam neque aliquet nibh, non ornare turpis odio in urna. Phasellus elementum. Vivamus lacus felis, posuere a, luctus eget, tristique vel, turpis. Suspendisse non tortor. Maecenas lectus justo, suscipit eu, sodales vitae, sagittis lobortis, metus. Nam vehicula felis ac metus. Pellentesque sit amet lorem et sem rhoncus congue.
-    <br /><br />
