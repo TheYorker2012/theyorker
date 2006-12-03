3ad7c4891aa15bde6d4d60c35663251afb3d22b1
Frazer Barnsley <frazzle@gmail.com>
2006-12-03 19:50:52 +0000

    List of links to pages made so far

---
+++ system/application/views/general/list.php
@@ -0,0 +1,79 @@
+This is just a basic list to all the pages that are made so far. I have probably missed some out so can you add links  to them please :)<br /><br />
+
+Home (takes you to the normal home page)
+  <ul>
+  <li><a href="/home/main/">/home/main/</a></li>
+  </ul>
+
+About
+  <ul>
+  <li><a href="/about/">/about/</a></li>
+  <li><a href="/contact/">/contact/</a></li>
+  </ul>
+
+Campaign
+  <ul>
+  <li><a href="/campaign/">/campaign/</a></li>
+  <li>(problems linking to other pages)</li>
+  </ul>
+
+Listings
+  <ul>
+  <li><a href="/listings/">/listings/</a></li>
+  </ul>
+
+News
+  <ul>
+  <li><a href="/news/">/news/</a>
+  <li><a href="/news/national/">/news/national/</a></li>
+  <li><a href="/news/features/">/news/features/</a></li>
+  <li><a href="/news/lifestyle/">/news/lifestyle/</a></li>
+  <li><a href="/news/article/">/news/article/</a></li>
+  <li><a href="/news/archive/">/news/archive/</a></li>
+  </ul>
+
+A to Z
+  <ul>
+  <li><a href="/atoz/directory/">/atoz/directory</a></li>
+  <li><a href="/atoz/type/">/atoz/type/</a></li>
+  </ul>
+
+Reviews
+  <ul>
+  <li><a href="/review/">/review/</a></li>
+  <li><a href="/review/food/">/review/food/</a></li>
+  <li><a href="/review/drink/">/review/drink/</a></li>
+  <li><a href="/review/culture/">/review/culture/</a></li>
+  <li><a href="/review/foodreview/">/review/foodreview/</a></li>
+  <li><a href="/review/culturereview/">/review/culturereview/</a></li>
+  <li><a href="/review/barcrawl/">/review/barcrawl/</a></li>
+  </ul>
+
+Directory
+  <ul>
+  <li><a href="/directory/">/directory/</a></li>
+  <li><a href="/directory/view/">/directory/view/</a></li>
+  </ul>
+
+Listings
+  <ul>
+  <li><a href="/listings/">/listings/</a></li>
+  </ul>
+
+FAQ
+  <ul>
+  <li><a href="/faq/">/faq/</a></li>
+  <li><a href="/faq/howdoi/">/faq/howdoi/</a></li>
+  <li><a href="/faq/addfaq/">/faq/addfaq/</a></li>
+  <li><a href="/faq/addhowdoi/">/faq/addhowdoi/</a></li>
+  </ul>
+
+
+
+
+
+
+
+
+
+
