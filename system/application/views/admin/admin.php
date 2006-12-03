b273b67fa14fafb5c4be7b6df5713f3c4b4693d3
Frazer Barnsley <frazzle@gmail.com>
2006-12-03 20:13:22 +0000

    Slight visual mod

--- system/application/views/admin/admin.php
+++ system/application/views/admin/admin.php
@@ -8,9 +8,9 @@ For the moment lets just have links to the current admin pages, add yours as you
 </p>
 
 <a href='/admin/yorkerdirectory/'>Edit the directory</a><br>
-<a href='/admin/yorkerdirectoryview/'>Edit entry in the directory</a>
+<a href='/admin/yorkerdirectoryview/'>Edit entry in the directory</a><br>
 <br />
-<a href="/admin/addfaq/">Add faq</a></li>
-<a href="/admin/addhowdoi/">Add to how do I</a></li>
-<a href="/admin/editfaq/">Edit FAQ entry</a></li>
+<a href="/admin/addfaq/">Add faq</a></li><br>
+<a href="/admin/addhowdoi/">Add to how do I</a></li><br>
+<a href="/admin/editfaq/">Edit FAQ entry</a></li><br>
 </div>
