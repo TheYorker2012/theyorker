59917ea88b12dc3d8f8c4eb803443e50889c9968
Mark Fairbairn <thebaron88@gmail.com>
2006-12-03 19:41:20 +0000

    Typo in file name.

---
+++ system/application/views/campaign/CampaignsEditDetails.php
@@ -0,0 +1,12 @@
+	<br>
+	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
+		<div class="title">Edit Campaign Details: <?php echo $CampaignTitle; ?></div>
+		Title: <br/>
+		<input type="text" style="width: 100%;" value="<?php echo $CampaignTitle; ?>" />
+		Detials: <br/>
+		<textarea style="width: 100%; height: 500px;"></textarea>
+		Author: <input type="text" style="width: 40%;" /> Society: <input type="text" style="width: 40%;" /><br/>
+		External Links: <input type="text" style="width: 80%;" /><br/><br/>
+		<input type="submit" value="Save Changes" style="width: 20%;"> ¦ <input type="submit" value="Delete Campaign" style="width: 20%;">
+	</div>
+	<br>
