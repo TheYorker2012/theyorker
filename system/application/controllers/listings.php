7badbc93b44c45dc07d9d2bf6a7b233439aa3dce
James Hogan <james_hogan@theyorker.co.uk>
2006-12-05 08:31:29 +0000

    Documentation and automatic slicing of event occurrences. Automatic formatting of dates and times from timestamps.

--- system/application/controllers/listings.php
+++ system/application/controllers/listings.php
@@ -1,13 +1,29 @@
 <?php
 
+/**
+ * @brief Controller for event manager.
+ * @author David Harker (dhh500@york.ac.uk)
+ * @author James Hogan (jh559@cs.york.ac.uk)
+ */
 class Listings extends Controller {
 
+	/**
+	 * @brief Default constructor.
+	 */
 	function Listings()
 	{
-		parent::Controller();	
+		parent::Controller();
+		
+		// Used for processing the events
+		$this->load->library('event_manager');
+		
+		// Used for producing friendly date strings
+		$this->load->library('academic_calendar');
 	}
 	
-	// default function
+	/**
+	 * @brief Default function.
+	 */
 	function index()
 	{
 		// Sorry about the clutter, this will be moved in a bit but it isn't
@@ -27,7 +43,7 @@ EXTRAHEAD;
 		$this->load->helper('minitemplater');
 		
 		// This array get sent to the view listings_view.php
-		$data = Array ();
+		$data = Array();
 		
 		// I don't trust users to set their clocks properly
 		$data['server_dt'] = time(); 
@@ -35,33 +51,25 @@ EXTRAHEAD;
 		$data['title'] = 'Listing viewer prototype';
 		
 		// this is temporary for testing only
-		$data['days'] = array ();
-		$daycalc = array ();
+		$data['days'] = array();
+		$daycalc = array();
+		$dayofweek = date('N',time ()) - 1;
+		$monday = strtotime('-'.$dayofweek.'day', time());
 		for ($dayoffset = 0; $dayoffset < 7; $dayoffset++) {
-			$dayofweek = date('N',time ()) - 1;
+			$day_ts = strtotime('+'.$dayoffset." day",$monday);
 			
-			$monday = strtotime ('-'.$dayofweek." day",time());
-			
-			$day_ts = strtotime ('+'.$dayoffset." day",$monday);
-			
-			$data['days'][]	= date ("jS M", $day_ts);
+			$data['days'][] = date ("jS M", $day_ts);
 			$daycalc[] = date ('d#m#y',$day_ts);
 		}
 		
-		// returns the day of the week that a date falls on if
-		// that date is within the range of the current calendar
-
-		
 		// define some dummy events with a rough schema until we have access
 		// to some real data to play with
-		$data['dummies'] = array (
+		$events = array (
 			array (
 				'ref_id' => '1',
 				'name' => 'House Party',
-				'date' => '2006-12-4',
-				'day' => $this->get_dow_offset ('2006-12-4',$daycalc),
-				'starttime' => '2100',
-				'endtime' => '0000',
+				'start' => mktime(21, 0,0, 12, 4,2006),
+				'end'   => mktime( 0, 0,0, 12, 5,2006),
 				'system_update_ts' => '3',
 				'user_update_ts' => '2',
 				'blurb' => 'Bangin\' house party in my house!',
@@ -71,10 +79,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'Boring lecture about vegetables',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-8',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 8,2006),
+				'end'   => mktime(15, 0,0, 12, 8,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'this will be well good i promise',
@@ -84,10 +90,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '3',
 				'name' => 'Social Gathering',
-				'date' => '2006-12-4',
-				'day' => $this->get_dow_offset ('2006-12-4',$daycalc),
-				'starttime' => '2100',
-				'endtime' => '0000',
+				'start' => mktime(21, 0,0, 12, 4,2006),
+				'end'   => mktime( 0, 0,0, 12, 5,2006),
 				'system_update_ts' => '3',
 				'user_update_ts' => '2',
 				'blurb' => 'Bangin\' house party in my house!',
@@ -97,10 +101,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '4',
 				'name' => 'Mince Pies and Punch',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-8',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 8,2006),
+				'end'   => mktime(15, 0,0, 12, 8,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'this will be well good i promise',
@@ -110,10 +112,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '5',
 				'name' => 'International talk-like-a-pirate day',
-				'date' => '2006-12-4',
-				'day' => $this->get_dow_offset ('2006-12-5',$daycalc),
-				'starttime' => '2100',
-				'endtime' => '0000',
+				'start' => mktime(21, 0,0, 12, 5,2006),
+				'end'   => mktime( 0, 0,0, 12, 6,2006),
 				'system_update_ts' => '3',
 				'user_update_ts' => '2',
 				'blurb' => 'Bangin\' house party in my house!',
@@ -123,10 +123,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'boring lecture about vegetables',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-8',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 8,2006),
+				'end'   => mktime(15, 0,0, 12, 8,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'this will be well good i promise',
@@ -136,10 +134,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '1',
 				'name' => 'House Party',
-				'date' => '2006-12-4',
-				'day' => $this->get_dow_offset ('2006-12-4',$daycalc),
-				'starttime' => '2100',
-				'endtime' => '0000',
+				'start' => mktime(21, 0,0, 12, 4,2006),
+				'end'   => mktime( 0, 0,0, 12, 5,2006),
 				'system_update_ts' => '3',
 				'user_update_ts' => '2',
 				'blurb' => 'Bangin\' house party in my house!',
@@ -149,10 +145,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '1',
 				'name' => 'House Party',
-				'date' => '2006-12-4',
-				'day' => $this->get_dow_offset ('2006-12-5',$daycalc),
-				'starttime' => '2100',
-				'endtime' => '0000',
+				'start' => mktime(21, 0,0, 12, 5,2006),
+				'end'   => mktime( 6, 0,0, 12, 6,2006),
 				'system_update_ts' => '3',
 				'user_update_ts' => '2',
 				'blurb' => 'Bangin\' house party in my house!',
@@ -162,10 +156,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'boring lecture about vegetables',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-6',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 6,2006),
+				'end'   => mktime(15, 0,0, 12, 6,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'this will be well good i promise',
@@ -175,10 +167,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'MARATHON Noodle eating contest',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-6',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 6,2006),
+				'end'   => mktime(15, 0,0, 12, 6,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'Noodleishous!',
@@ -188,10 +178,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'Regional \'Pong\' championships, semi final',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-6',$daycalc),
-				'starttime' => '1245',
-				'endtime' => '1500',
+				'start' => mktime(12,45,0, 12, 6,2006),
+				'end'   => mktime(15, 0,0, 12, 6,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => '|&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;|<br />
@@ -207,10 +195,8 @@ EXTRAHEAD;
 			array (
 				'ref_id' => '2',
 				'name' => 'Better than vegetables',
-				'date' => '2006-12-8',
-				'day' => $this->get_dow_offset ('2006-12-7',$daycalc),
-				'starttime' => '1800',
-				'endtime' => '2000',
+				'start' => mktime(18, 0,0, 12, 7,2006),
+				'end'   => mktime(20, 0,0, 12, 7,2006),
 				'system_update_ts' => '1',
 				'user_update_ts' => '1',
 				'blurb' => 'Just a few pints',
@@ -219,7 +205,7 @@ EXTRAHEAD;
 			)
 		);
 		
-		
+		$data['dummies'] = $this->_ProcessEvents($events, $daycalc);
 		
 		$pass_data['subdata'] = $data;
 		$pass_data['extra_head'] = $extra_head;
@@ -230,16 +216,68 @@ EXTRAHEAD;
 		//$this->load->view('listings_view',$data);
 	}
 	
-	function get_dow_offset ($date,$daycalci) {
-		$ts = strtotime ($date);
-		foreach ($daycalci as $os => $date) {
-			if (date ('d#m#y',$ts) == $date) {
-				return $os;
-				break;
+	/**
+	 * @brief Process events ready for the view.
+	 * @param array $Occurrences Array of event occurrences (using timestamps).
+	 * @param array $DayCalc Visible calendar information.
+	 * @return array similar to @a $Occurrences with the following extra fields:
+	 *	- 'date' (the date of the start of the slice formatted as 'Y-m-d').
+	 *	- 'day' (the day index on the visible calendar (using @a $DayCalc).
+	 *	- 'starttime' (the start time of the occurrence using user preferences to format).
+	 *	- 'endtime' (the end time of the occurrence using user preferences to format).
+	 */
+	function _ProcessEvents($Occurrences, $DayCalc)
+	{
+		// Slice up the events
+		$event_occurrences = $this->event_manager->SliceOccurrences($Occurrences, 4*60);
+		// Perform date formatting
+		$return_array = array();
+		foreach ($event_occurrences as $event_index => $event_data) {
+			// Convert timestamps to Academic_times so its easier to extract
+			// date and time in certain formats
+			$event_data['start']       = new Academic_time($event_data['start']);
+			$event_data['end']         = new Academic_time($event_data['end']);
+			$event_data['slice_start'] = new Academic_time($event_data['slice_start']);
+			$event_data['slice_end']   = new Academic_time($event_data['slice_end']);
+			
+			// Produce and new 'date' field with string date
+			$event_data['date'] = $event_data['slice_start']->Format('Y-m-d');
+			
+			// Which day of the current view?
+			$event_data['day'] = $this->_GetDowOffset(
+					$event_data['slice_start']->Timestamp(),
+					$DayCalc);
+			
+			// Start and end time (use slice start/end for the moment)
+			// It should be made obvious when a slice is only part of an event
+			$event_data['starttime'] = $event_data['start']->Time();
+			$event_data['endtime'] = $event_data['end']->Time();
+			
+			// Add to the return array
+			$return_array[] = $event_data;
+		}
+		return $return_array;
+	}
+	
+	/**
+	 * @brief Find which day of the visible calendar a timestamp lies in.
+	 * @param timestamp $Timestamp Timestamp to find.
+	 * @param array $DayCalc Visible calendar information.
+	 * @return Array index:
+	 *	- Index of a day in @a $DayCalc that matches @a $Timestamp.
+	 *	- -1 if no matches are found.
+	 *
+	 * Returns the day of the week that a date falls on if
+	 *	that date is within the range of the current calendar
+	 */
+	function _GetDowOffset($Timestamp, $DayCalc) {
+		$formatted_time = date('d#m#y',$Timestamp);
+		foreach ($DayCalc as $offset => $calendar_date) {
+			if ($formatted_time == $calendar_date) {
+				return $offset;
 			}
 		}
 		return -1;
-		break;
 	}
 }
 ?>
