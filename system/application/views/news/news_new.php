8cf2efe0de718fafdc889d84288048f6f7de18c5
Matthew Tole <mdt502@york.ac.uk>
2006-12-05 02:10:35 +0000

    New view for news homepage.

---
+++ system/application/views/news/news_new.php
@@ -0,0 +1,113 @@
+	<div id='newsnav'>
+		<ul id='newsnavlist'>
+		<li><a href='<?php echo site_url('news/'); ?>' id='current'><img src='/images/prototype/news/uk.png' alt='News' title='News' /> News</a></li>
+		<li><a href='<?php echo site_url('news/national/'); ?>'><img src='/images/prototype/news/earth.png' alt='National' title='National' /> National</a></li>
+		<li><a href='<?php echo site_url('news/features/'); ?>'><img src='/images/prototype/news/feature.gif' alt='Feature' title='Feature' /> Features</a></li>
+		<li><a href='<?php echo site_url('news/lifestyle/'); ?>'><img src='/images/prototype/news/feature.gif' alt='Lifestyle' title='Lifestyle' /> Lifestyle</a></li>
+		<li><a href='<?php echo site_url('news/archive/'); ?>'><img src='/images/prototype/news/archive.png' alt='Archive' title='Archive' /> Archive</a></li>
+		</ul>
+	</div>
+	
+	<div class='NewsPreview' style='border-bottom: 1px solid #93969a;'>
+		<img src='/images/prototype/news/thumb1.jpg' alt='Soldier about to get run over by a tank' title='Soldier about to get run over by a tank' />
+		<h1>Israel vows ceasefire 'patience'</h1>
+		<p class='Writer'>
+			<a href='/directory/view/1.htm'>Matthew Tole</a>
+		</p>
+		<p class='Date'>5th December 2006</p>
+		<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+
+        <p>
+		 	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc
+		    elementum arcu non risus. Vestibulum arcu enim, placerat nec,
+		    malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.
+
+		</p>
+		<br style='clear: both;'/>
+	</div>
+
+	<div class='NewsPreview' style='width: 47%; float: left; '>
+		<img src='/images/prototype/news/thumb2.jpg' alt='Tony Blair' title='Tony Blair' />
+ 		<h3>Blair 'sorrow' over slave trade</h3>
+ 		<p class='Writer'>
+			<a href='/directory/view/1.htm'>Jo Shelley</a>
+		</p>
+		<p class='Date'>4th December 2006</p>
+ 		<p class='More'><?php echo anchor('news/article/2', 'Read more...'); ?></p>
+        <p>
+			Prime Minister Tony Blair has said he feels "deep sorrow" for
+			Britain's role in the slave trade. In an article for the New Nation
+			newspaper, the prime minister said it had been "profoundly shameful".
+ 		</p>
+	</div>
+
+	<div class='NewsPreview' style='width: 47%; float: right;'>
+		<img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' />
+ 		<h3>Advice sought after ex-spy death</h3>
+ 		<p class='Writer'>
+			<a href='/directory/view/1.htm'>Dan Ashby</a>
+		</p>
+		<p class='Date'>3rd December 2006</p>
+ 		<p class='More'><?php echo anchor('news/article/3', 'Read more...'); ?></p>
+        <p>
+		Hundreds of people have called the NHS Direct hotline following the
+		death of Russian ex-spy Alexander Litvinenko.
+ 		</p>
+	</div>
+
+	<div class='NewsOther'>
+		<h3>Other Articles</h3>
+		<p>
+			<a href='/news/article/3'>
+				<img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' style='float: left; '/>
+			</a>
+			<p class='Headline'>
+				<a href='/news/article/3'>Ex-spy death inquiry stepped up.</a>
+			</p>
+			<p class='Writer'>
+				<a href='/directory/view/1.htm'>Jo Shelley</a>
+			</p>
+			<p class='Date'>2nd December 2006</p>
+			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		</p>
+
+		<p>
+			<a href='/news/article/2'>
+				<img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' style='float: left; '/>
+			</a>
+			<p class='Headline'>
+				<a href='/news/article/3'>Blair 'sorrow' over slave trade.</a>
+			</p>
+			<p class='Writer'>
+				<a href='/directory/view/1.htm'>Jo Shelley</a>
+			</p>
+			<p class='Date'>1st December 2006</p>
+			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		</p>
+		
+		<p>
+			<img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' style='float: left; '/>
+			<p class='Headline'>Israel vows ceasefire 'patience'.</p>
+			<p class='Writer'>Owen Jones</p>
+			<p class='Date'>30th November 2006</p>
+			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		</p>
+
+		<p>
+			<img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' style='float: left; '/>
+			<p class='Headline'>Ex-spy death inquiry stepped up.</p>
+			<p class='Writer'>Owen Jones</p>
+			<p class='Date'>29th November 2006</p>
+			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		</p>
+
+		<p>
+			<img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' style='float: left; '/>
+			<p class='Headline'>Ex-spy death inquiry stepped up.</p>
+			<p class='Writer'>Owen Jones</p>
+			<p class='Date'>28th November 2006</p>
+			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		</p>
+		</div>
+	</div>
+	<div class='Advert'><img src='/images/adverts/box.jpg' /></div>
