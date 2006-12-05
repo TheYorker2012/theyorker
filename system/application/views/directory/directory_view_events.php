d71dc858d26011a7ad58a6106da82a02e0f1efe3
Owen Jones <oj502@york.ac.uk>
2006-12-05 23:35:36 +0000

    Split directory into four pages at nicks request controler will not access the new pages yet.

--- system/application/views/directory/directory_view_events.php
+++ system/application/views/directory/directory_view_events.php
@@ -18,24 +18,16 @@
 </div>
 <div id='newsnav'>
 	<ul id='newsnavlist'>
-	<li><a href='/directory/fragsoc/' id='current'><img src='/images/prototype/news/uk.png' alt='Frag Soc' title='Frag Soc' /> Frag Soc</a></li>
-	<li><a href='/directory/fragsoc/events/'><img src='/images/prototype/news/feature.gif' alt='Events' title='Events' /> Events</a></li>
+	<li><a href='/directory/fragsoc/'><img src='/images/prototype/news/uk.png' alt='Frag Soc' title='Frag Soc' /> Frag Soc</a></li>
+	<li><a href='/directory/fragsoc/events/' id='current'><img src='/images/prototype/news/feature.gif' alt='Events' title='Events' /> Events</a></li>
 	<li><a href='/directory/fragsoc/members/'><img src='/images/prototype/news/feature.gif' alt='Members' title='Members' /> Members</a></li>
 	<li><a href='/directory/fragsoc/reviews/'><img src='/images/prototype/news/feature.gif' alt='Reviews' title='Reviews' /> Reviews</a></li>
 	</ul>
 </div>
-<h2>FragSoc</h2>
-<div class='columnText'>
-	<h3>Society</h3>
-	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla lorem magna, tincidunt sed, feugiat nec, consectetuer vitae, nisl. Vestibulum gravida ipsum non justo. Vivamus sem. Quisque ut sem vitae elit luctus lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-	<h5>Website:</h5>
-	<a href='http://www.fragsoc.com'>www.fragsoc.com</a>
-	<h5>Location:</h5>
-	Goodricke College
-	<h5>Opening Times:</h5>
-	Every Other Weekend
-</div>
-<div class='columnPhoto'>
-	<a href='#'><img src='/images/prototype/reviews/reviews_07.jpg' /></a>
-	<a href='#'><h5>Image Information</h5></a>
+<h2>Upcoming events:</h2>
+<div>
+	<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+	<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+	<p><a href='/listings/'>Lan Party</a> for all members 1/1/1970 18:30, At <a href='#'>L/049</a>
+	<h5><a href='#'>Watch this stream of events</a></h5>
 </div>
---
+++ system/application/views/directory/directory_view_members.php
@@ -0,0 +1,49 @@
+<div id='pageheader' style='background-image: url(/images/subheadericons/pageicon_pagename.gif);'>
+	<div id='titleheader'>
+		<h1>The Yorker Directory</h1>
+	</div>
+	<div style='margin-left: 10px;'>
+		<form id='form1' name='form1' action='/directory/' method='POST'>
+			<strong>Show:</strong> 
+			Venues<input type='checkbox' name='searchrange' value="venues" checked>
+			Societies<input type='checkbox' name='searchrange' value="socs" checked>
+			Athletics Union<input type='checkbox' name='searchrange' value="au" checked>
+			Organisation<input type='checkbox' name='searchrange' value="org" checked>
+			College &#038; Campus<input type='checkbox' name='searchrange' value="campus" checked>
+			<br />
+			<input type='text' name='search'>
+			<input type='submit' name='Submit' value='Search'>
+		</form>
+	</div>
+</div>
+<div id='newsnav'>
+	<ul id='newsnavlist'>
+	<li><a href='/directory/fragsoc/'><img src='/images/prototype/news/uk.png' alt='Frag Soc' title='Frag Soc' /> Frag Soc</a></li>
+	<li><a href='/directory/fragsoc/events/'><img src='/images/prototype/news/feature.gif' alt='Events' title='Events' /> Events</a></li>
+	<li><a href='/directory/fragsoc/members/' id='current'><img src='/images/prototype/news/feature.gif' alt='Members' title='Members' /> Members</a></li>
+	<li><a href='/directory/fragsoc/reviews/'><img src='/images/prototype/news/feature.gif' alt='Reviews' title='Reviews' /> Reviews</a></li>
+	</ul>
+</div>
+<h2>Members:</h2>
+<div class='Memberbox'>
+	<div class='MemberboxText'>
+		<h3>Editor - Matthew Tole</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
+	</div>
+	<div class='MemberboxPhoto'>
+		<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+		<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
+	</div>
+	<div class="clear">&nbsp;</div>
+</div>
+<div class='Memberbox'>
+	<div class='MemberboxText'>
+		<h3>Editor - Matthew Tole</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
+	</div>
+	<div class='MemberboxPhoto'>
+		<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+		<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
+	</div>
+	<div class="clear">&nbsp;</div>
+</div>
