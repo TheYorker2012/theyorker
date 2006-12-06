bc23f3a900bd8a85e3387f2e55641ca2ac0460e9
Frazer Barnsley <frazzle@gmail.com>
2006-12-06 14:08:47 +0000

    Slightly modified layout

--- system/application/views/search/search2.php
+++ system/application/views/search/search2.php
@@ -1,7 +1,7 @@
 
 <!-- a_search_form defined twice, remember to recreate into class !! :) //-->
 
-<div id='minibox'>
+<div id='minibox' style="float:right;margin-right:5px">
     <div id='title'>Categorys</div>
     <a href='#'><b>All results</b> <i>(112)</i></a>
     <a href='#'>News <i>(41)</i></a>
@@ -11,7 +11,7 @@
     <a href='#'>Events <i>(12)</i></a>
 </div>
 
-<div id='searchresults'>
+<div id='searchresults' style="padding:0px 150px 0px 0px">
     <form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
         <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
         <input type='submit' name='submit' id='submit' value='Search' /></p>
