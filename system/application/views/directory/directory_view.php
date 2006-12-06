90141421ca0b18e41fedd0978641b0c83249d516
Nick Evans <nse500@york.ac.uk>
2006-12-06 00:48:40 +0000

    Preview without search bar

--- system/application/views/directory/directory_view.php
+++ system/application/views/directory/directory_view.php
@@ -1,21 +1,4 @@
-<div id='pageheader' style='background-image: url(/images/subheadericons/pageicon_pagename.gif);'>
-	<div id='titleheader'>
-		<h1>The Yorker Directory</h1>
-	</div>
-	<div style='margin-left: 10px;'>
-		<form id='form1' name='form1' action='/directory/' method='POST'>
-			<strong>Show:</strong> 
-			Venues<input type='checkbox' name='searchrange' value="venues" checked>
-			Societies<input type='checkbox' name='searchrange' value="socs" checked>
-			Athletics Union<input type='checkbox' name='searchrange' value="au" checked>
-			Organisation<input type='checkbox' name='searchrange' value="org" checked>
-			College &#038; Campus<input type='checkbox' name='searchrange' value="campus" checked>
-			<br />
-			<input type='text' name='search'>
-			<input type='submit' name='Submit' value='Search'>
-		</form>
-	</div>
-</div>
+<br>
 <div id='newsnav'>
 	<ul id='newsnavlist'>
 	<li><a href='/directory/fragsoc/' id='current'><img src='/images/prototype/news/uk.png' alt='Frag Soc' title='Frag Soc' /> Frag Soc</a></li>
