1d38dab54b6c9e28f6a0e3ab0fa5f7557d982b9c
Frazer Barnsley <frazzle@gmail.com>
2006-12-06 13:59:10 +0000

    Just simple modifications

--- system/application/views/howdoi/howdoi.php
+++ system/application/views/howdoi/howdoi.php
@@ -1,34 +1,28 @@
-    <div id='pageheader' style='background-image: url(/images/subheadericons/pageicon_pagename.gif);'>
-        <div id='titleheader'>
-            <h1>How Do I?</h1>
-	  </div>
-        <p>&nbsp;&nbsp;Related Pages :</p>
-        <div id='pagelinks'>
-            <ul>
-                <li><a href="/faq">FAQ</a></li>
-                <li>How Do I?</li>
-            </ul>
-        </div>
+	<div id='newsnav'>
+		<ul id='newsnavlist'>
+			<li><a href='/howdoi/' id='current'><img src='/images/prototype/news/uk.png' alt='How Do I?' title='How Do I?' /> How Do I?</a></li>
+		</ul>
+	</div>
+    <div class="padding">
+        <h2>How Do I?</h2>
+        Some Catagory
+        <ul>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+        </ul>
+        Some Catagory
+        <ul>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+        </ul>
+        Some Catagory
+        <ul>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+            <li><a href='/howdoi/view/1'>Something here</a></li>
+        </ul>
+        <br /><br />
     </div>
-    <h2>How Do I?</h2>
-    Some Catagory
-    <ul>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-    </ul>
-    Some Catagory
-    <ul>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-    </ul>
-    Some Catagory
-    <ul>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-        <li><a href='/faq/howdoi/1'>Something here</a></li>
-    </ul>
-
-    <br />
