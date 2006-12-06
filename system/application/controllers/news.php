1cbc198f4491d447dd4e502b9711ec1e2546155d
Neil Brehon <nb525@york.ac.uk>
2006-12-06 22:44:11 +0000

    The Campus News page view now pulls its data from the controller, as it ultimately should - I hope the view team isn't angry at me for invading their turf!

--- system/application/controllers/news.php
+++ system/application/controllers/news.php
@@ -1,15 +1,101 @@
 <?php
-
+/**
+ * This is the controller for the news section.
+ *
+ * @author Chris Travis, Neil Brehon
+ */
 class News extends Controller {
-
+    
+    /// The Campus News section (default).
 	function index()
 	{
+    	
+    	/// test "news preview" data for use until we can use the database
+        $preview_data = array(
+            array(
+                'id' => '8',
+                'image' => '/images/prototype/news/thumb1.jpg',
+                'image_description' => 'Soldier about to get run over by a tank',
+                'headline' => 'Israel vows ceasefire \'patience\'',
+                'writer_id' => '2',
+                'writer' => 'Matthew Tole',
+                'date' => '5th December 2006',
+                'blurb' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.'
+            ),
+            array(
+                'id' => '7',
+                'image' => '/images/prototype/news/thumb2.jpg',
+                'image_description' => 'Tony Blair',
+                'headline' => 'Blair \'sorrow\' over slave trade',
+                'writer' => 'Jo Shelley',
+                'date' => '4th December 2006',
+                'blurb' => 'Prime Minister Tony Blair has said he feels "deep sorrow" for Britain\'s role in the slave trade. In an article for the New Nation newspaper, the prime minister said it had been "profoundly shameful".'
+            ),
+            array(
+                'id' => '6',
+                'image' => '/images/prototype/news/thumb3.jpg',
+                'image_description' => 'Some Spy',
+                'headline' => 'Advice sought after ex-spy death',
+                'writer' => 'Dan Ashby',
+                'date' => '3rd December 2006',
+                'blurb' => 'Hundreds of people have called the NHS Direct hotline following the death of Russian ex-spy Alexander Litvinenko.'
+            )
+        );
+        
+        /// test "other news" data for use until we can use the database
+        $other_data = array(
+            array(
+                'id' => '5',
+                'image' => '/images/prototype/news/thumb3.jpg',
+                'image_description' => 'Some Spy',
+                'headline' => 'Ex-spy death inquiry stepped up.',
+                'writer' => 'Jo Shelley',
+                'date' => '2nd December 2006'
+            ),
+            array(
+                'id' => '4',
+                'image' => '/images/prototype/news/thumb2.jpg',
+                'image_description' => 'Tony Blair',
+                'headline' => 'Tony Blair finds £10, keeps it.',
+                'writer' => 'Jo Shelley',
+                'date' => '1st December 2006'
+            ),
+            array(
+                'id' => '3',
+                'image' => '/images/prototype/news/thumb9.jpg',
+                'image_description' => 'Man in a wig',
+                'headline' => 'Mass panic as world ends twice in one day.',
+                'writer' => 'Neil Brehon',
+                'date' => '30th November 2006'
+            ),
+            array(
+                'id' => '2',
+                'image' => '/images/prototype/news/thumb3.jpg',
+                'image_description' => 'Some Spy',
+                'headline' => 'Ex-spy death inquiry stepped up again.',
+                'writer' => 'Owen Jones',
+                'date' => '29th November 2006'
+            ),
+            array(
+                'id' => '1',
+                'image' => '/images/prototype/news/thumb2.jpg',
+                'image_description' => 'Some Spy',
+                'headline' => 'Ex-spy death inquiry stepped up.',
+                'writer' => 'Owen Jones',
+                'date' => '28th November 2006'
+            ),
+        );
+        
+    	/// The data passed to the view will come from the database once it is available.
 		$data = array(
-			'content_view' => 'news/news.php'
+			'content_view' => 'news/news.php',
+			'news_previews' => $preview_data,
+			'news_others' => $other_data
 		);
 		$this->load->view('frames/student_frame',$data);
 	}
 
+	/// The National News section.
 	function national()
 	{
 		$data = array(
@@ -18,14 +104,13 @@ class News extends Controller {
 		$this->load->view('frames/student_frame',$data);
 	}
 
+	/// The Features section.
 	function features()
 	{
-		$data = array(
-			'content_view' => 'news/news.php'
-		);
-		$this->load->view('frames/student_frame',$data);
+		$this->index();
 	}
 
+	/// The Lifestyle section.
 	function lifestyle()
 	{
 		$data = array(
@@ -34,6 +119,7 @@ class News extends Controller {
 		$this->load->view('frames/student_frame',$data);
 	}
 
+	/// General controller for all news articles.
 	function article()
 	{
 		$data = array(
@@ -42,18 +128,11 @@ class News extends Controller {
 		$this->load->view('frames/student_frame',$data);
 	}
 	
+	/// The Archive section.
 	function archive()
 	{
 		$data = array(
 			  'content_view' => 'news/archive.php'
-		);
-		$this->load->view('frames/student_frame',$data);
-	}
-
-	function test()
-	{
-		$data = array(
-			  'content_view' => 'news/test.php'
 		);
 		$this->load->view('frames/student_frame',$data);
 	}
--- system/application/views/news/news.php
+++ system/application/views/news/news.php
@@ -8,92 +8,69 @@
 		</ul>
 	</div>
 
-	<div class='NewsPreview' style='border-bottom: 1px solid #93969a;'>
-		<a href='/news/article/1'><img src='/images/prototype/news/thumb1.jpg' alt='Soldier about to get run over by a tank' title='Soldier about to get run over by a tank' /></a>
-		<h1><?php echo anchor('news/article/1', 'Israel vows ceasefire \'patience\''); ?></h1>
+    <?php
+    /**
+     * Preview #0 = top of the page
+     * Preview #1 = bottom-left
+     * Preview #2 = bottom-right
+     */
+    for ($preview_number = 0; $preview_number <= 2; $preview_number++)
+    {
+        echo '<div class=\'NewsPreview\' style=\'';
+        
+        /// the default heading size is 3 (i.e.: most headings in the loop will use <h3> for formatting)
+        $heading_size = '3';
+        
+        /// different inline styles for each news preview
+        switch ($preview_number){
+            case 0:
+                echo 'border-bottom: 1px solid #93969a;';
+                $heading_size = '1';
+                break;
+            case 1:
+                echo 'border-bottom: 1px solid #93969a; width: 47%; float: left;';
+                break;
+            case 2:
+                echo 'width: 47%; float: right;';
+                break;
+        }
+        
+        echo '\'>';
+        
+        ?>
+		<a href='/news/article/<?php echo $news_previews[$preview_number]['id']; ?>'><img src='<?php echo $news_previews[$preview_number]['image']; ?>' alt='<?php echo $news_previews[$preview_number]['image_description']; ?>' title='<?php echo $news_previews[$preview_number]['image_description']; ?>' /></a>
+		<h<?php echo $heading_size; ?>><?php echo anchor('news/article/'.$news_previews[$preview_number]['id'], $news_previews[$preview_number]['headline']); ?></h1>
 		<p class='Writer'>
-			<a href='/directory/view/1'>Matthew Tole</a>
+			<a href='/directory/view/1'><?php echo $news_previews[$preview_number]['writer']; ?></a>
 		</p>
-		<p class='Date'>5th December 2006</p>
-		<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		<p class='Date'><?php echo $news_previews[$preview_number]['date']; ?></p>
+		<p class='More'><?php echo anchor('news/article/'.$news_previews[$preview_number]['id'], 'Read more...'); ?></p>
         <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc
-		    elementum arcu non risus. Vestibulum arcu enim, placerat nec,
-		    malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.
+            <?php echo $news_previews[$preview_number]['blurb']; ?>
 		</p>
 		<br style='clear: both;'/>
 	</div>
-
-	<div class='NewsPreview' style='border-bottom: 1px solid #93969a; width: 47%; float: left; '>
-		<a href='/news/article/2'><img src='/images/prototype/news/thumb2.jpg' alt='Tony Blair' title='Tony Blair' /></a>
- 		<h3><?php echo anchor('news/article/2', 'Blair \'sorrow\' over slave trade'); ?></h3>
- 		<p class='Writer'>
-			<a href='/directory/view/1'>Jo Shelley</a>
-		</p>
-		<p class='Date'>4th December 2006</p>
- 		<p class='More'><?php echo anchor('news/article/2', 'Read more...'); ?></p>
-        <p>
-			Prime Minister Tony Blair has said he feels "deep sorrow" for
-			Britain's role in the slave trade. In an article for the New Nation
-			newspaper, the prime minister said it had been "profoundly shameful".
- 		</p>
-	</div>
-
-	<div class='NewsPreview' style='width: 47%; float: right;'>
-		<a href='/news/article/3'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
- 		<h3><?php echo anchor('news/article/3', 'Advice sought after ex-spy death'); ?></h3>
- 		<p class='Writer'>
-			<a href='/directory/view/1'>Dan Ashby</a>
-		</p>
-		<p class='Date'>3rd December 2006</p>
- 		<p class='More'><?php echo anchor('news/article/3', 'Read more...'); ?></p>
-        <p>
-			Hundreds of people have called the NHS Direct hotline following the
-			death of Russian ex-spy Alexander Litvinenko.
- 		</p>
-	</div>
-
-	<div class='NewsOther' style='width: 47%;'>
-		<h3></h3>
+	
+    <?php } ?>
+    
+    <div class='NewsOther' style='width: 47%;'>
+    <h3></h3>
+    
+    <?php
+    foreach ($news_others as $other)
+    {
+        
+        ?>
 		<p>
-			<a href='/news/article/1'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
-			<p class='Headline'><a href='/news/article/1'>Ex-spy death inquiry stepped up.</a></p>
-			<p class='Writer'><a href='/directory/view/1'>Jo Shelley</a></p>
-			<p class='Date'>2nd December 2006</p>
-			<p class='More'><?php echo anchor('news/article/1', 'Read more...'); ?></p>
+		    <a href='/news/article/<?php echo $other['id']; ?>'><img src='<?php echo $other['image']; ?>' alt='<?php echo $other['image_description']; ?>' title='<?php echo $other['image_description']; ?>' /></a>
+		    <p class='Headline'><a href='/news/article/<?php echo $other['id']; ?>'><?php echo $other['headline']; ?></a></p>
+			<p class='Writer'><a href='/directory/view/1'><?php echo $other['writer']; ?></a></p>
+			<p class='Date'><?php echo $other['date']; ?></p>
+			<p class='More'><?php echo anchor('news/article/'.$other['id'], 'Read more...'); ?></p>
 		</p>
+	
+    <?php } ?>
 
-		<p>
-			<a href='/news/article/2'><img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' /></a>
-			<p class='Headline'><a href='/news/article/2'>Blair 'sorrow' over slave trade.</a></p>
-			<p class='Writer'><a href='/directory/view/1'>Jo Shelley</a></p>
-			<p class='Date'>1st December 2006</p>
-			<p class='More'><?php echo anchor('news/article/2', 'Read more...'); ?></p>
-		</p>
-		
-		<p>
-			<a href='/news/article/3'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
-			<p class='Headline'><a href='/news/article/3'>Israel vows ceasefire 'patience'.</a></p>
-			<p class='Writer'><a href='/directory/view/1'>Owen Jones</a></p>
-			<p class='Date'>30th November 2006</p>
-			<p class='More'><?php echo anchor('news/article/3', 'Read more...'); ?></p>
-		</p>
-
-		<p>
-			<a href='/news/article/4'><img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' /></a>
-			<p class='Headline'><a href='/news/article/4'>Ex-spy death inquiry stepped up.</a></p>
-			<p class='Writer'><a href='/directory/view/1'>Owen Jones</a></p>
-			<p class='Date'>29th November 2006</p>
-			<p class='More'><?php echo anchor('news/article/4', 'Read more...'); ?></p>
-		</p>
-
-		<p>
-			<a href='/news/article/5'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
-			<p class='Headline'><a href='/news/article/5'>Ex-spy death inquiry stepped up.</a></p>
-			<p class='Writer'><a href='/directory/view/1'>Owen Jones</a></p>
-			<p class='Date'>28th November 2006</p>
-			<p class='More'><?php echo anchor('news/article/5', 'Read more...'); ?></p>
-		</p>
 		<p class='More' style='text-align:right;'>
 			<a href='/news/archive'>View News Archive...</a>
 		</p>
