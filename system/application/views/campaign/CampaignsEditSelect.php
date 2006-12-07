34045cb9393921bf2308b400a5acac349ef082c7
James Hogan <james_hogan@theyorker.co.uk>
2006-12-07 17:49:14 +0000

    CampaignsEditSelect view: correct dead links

--- system/application/views/campaign/CampaignsEditSelect.php
+++ system/application/views/campaign/CampaignsEditSelect.php
@@ -3,10 +3,10 @@
 	<div class="title">Edit Campaign</div>
 	<div class="subsubtitle">Please select a campaign to edit.</div>
 		<ul>
-			<a href="<?php echo site_url('Campaign/edit/Swimming Pool'); ?>"><li style="font-weight: bold;">SWIMMING POOL</li></a>
-			<a href="<?php echo site_url('Campaign/edit/Anti-Arms'); ?>"><li style="font-weight: bold;">ANTI-ARMS</li></a>
-			<a href="<?php echo site_url('Campaign/edit/Kill All Ants'); ?>"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
-			<a href="<?php echo site_url('Campaign/edit/Buddhist Campus'); ?>"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
+			<a href="<?php echo site_url('campaign/edit/Swimming Pool'); ?>"><li style="font-weight: bold;">SWIMMING POOL</li></a>
+			<a href="<?php echo site_url('campaign/edit/Anti-Arms'); ?>"><li style="font-weight: bold;">ANTI-ARMS</li></a>
+			<a href="<?php echo site_url('campaign/edit/Kill All Ants'); ?>"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
+			<a href="<?php echo site_url('campaign/edit/Buddhist Campus'); ?>"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
 		</ul>
 		<input type="submit" value="New Campaign" style="width: 20%;">
 	</div>
