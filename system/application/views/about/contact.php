3ac900e43c67a8f426c235d229008ec62640b24e
Owen Jones <oj502@york.ac.uk>
2006-12-09 11:18:02 +0000

    Split pages to include policy page, contact page still exists will be redirected to directory later on.

--- system/application/views/about/contact.php
+++ system/application/views/about/contact.php
@@ -1,6 +1,7 @@
 <div id='newsnav'>
 	<ul id='newsnavlist'>
 	<li><a href='/about/'><img src='/images/prototype/news/uk.png' alt='About Us' title='About Us' /> About Us</a></li>
+	<li><a href='/policy/'><img src='/images/prototype/news/feature.gif' alt='Our Policy' title='Our Policy' /> Our Policy</a></li>
 	<li><a href='/contact/' id='current'><img src='/images/prototype/news/feature.gif' alt='Contact Us' title='Contact Us' /> Contact Us</a></li>
 	<li><a href='/charity/'><img src='/images/prototype/news/earth.png' alt='Sponsored Charity' title='Sponsored Charity' /> Sponsored Charity</a></li>
 	</ul>
--- system/application/views/about/policy.php
+++ system/application/views/about/policy.php
@@ -1,12 +1,13 @@
 <div id='newsnav'>
 	<ul id='newsnavlist'>
-	<li><a href='/about/' id='current'><img src='/images/prototype/news/uk.png' alt='About Us' title='About Us' /> About Us</a></li>
-	<li><a href='/policy/'><img src='/images/prototype/news/feature.gif' alt='Our Policy' title='Our Policy' /> Our Policy</a></li>
+	<li><a href='/about/'><img src='/images/prototype/news/uk.png' alt='About Us' title='About Us' /> About Us</a></li>
+	<li><a href='/policy/' id='current'><img src='/images/prototype/news/feature.gif' alt='Our Policy' title='Our Policy' /> Our Policy</a></li>
 	<li><a href='/contact/'><img src='/images/prototype/news/feature.gif' alt='Contact Us' title='Contact Us' /> Contact Us</a></li>
 	<li><a href='/charity/'><img src='/images/prototype/news/earth.png' alt='Sponsored Charity' title='Sponsored Charity' /> Sponsored Charity</a></li>
 	</ul>
 </div>
-<h2>The Website</h2>
+<div>
+<h2>Statement of Policy</h2>
 <div class='columnText'>
 	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus enim leo, varius eu, sodales non, egestas nec, urna. Etiam lacus orci, molestie ac, malesuada at, ullamcorper eu, tortor. Phasellus fermentum, mauris a ullamcorper porta, dui erat tincidunt arcu, ac lacinia mauris lorem ac est. Sed nunc justo, feugiat sed, ultricies nec, vehicula eget, massa. Nam eget massa ut elit pretium tincidunt. Fusce sollicitudin vulputate tellus. Aliquam erat volutpat. Pellentesque turpis risus, bibendum vel, lobortis eget, facilisis non, risus.</p>
 </div>
@@ -15,7 +16,7 @@
 	<a href='#'><h5>Image Information</h5></a>
 </div>
 <div class="clear">&nbsp;</div>
-<h2>Our Aims</h2>
+<h2>Privacy Policy</h2>
 <div class='columnText'>
 	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus enim leo, varius eu, sodales non, egestas nec, urna. Etiam lacus orci, molestie ac, malesuada at, ullamcorper eu, tortor. Phasellus fermentum, mauris a ullamcorper porta, dui erat tincidunt arcu, ac lacinia mauris lorem ac est. Sed nunc justo, feugiat sed, ultricies nec, vehicula eget, massa. Nam eget massa ut elit pretium tincidunt. Fusce sollicitudin vulputate tellus. Aliquam erat volutpat. Pellentesque turpis risus, bibendum vel, lobortis eget, facilisis non, risus.</p>
 </div>
