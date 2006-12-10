518bd15a846c1d15a80051b3f1b55a1d6c7448dc
Owen Jones <oj502@york.ac.uk>
2006-12-10 21:15:57 +0000

    Updated the contact cards to look a bit nicer, still need some work any suggestions?

--- system/application/views/about/contact.php
+++ system/application/views/about/contact.php
@@ -9,39 +9,22 @@
 <div>
 	<h2>Who to contact</h2>
 	<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus enim leo, varius eu, sodales non, egestas nec, urna. Etiam lacus orci, molestie ac, malesuada at, ullamcorper eu, tortor. Phasellus fermentum, mauris a ullamcorper porta, dui erat tincidunt arcu, ac lacinia mauris lorem ac est.</p>
-	<div class='Infobox'>
-		<div class='InfoboxText'>
-			<h2>Nick Evans</h2>
-			<h3>Technical Director</h3>
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus enim leo, varius eu, sodales non, egestas nec, urna.</p>
-			<ul>
-				<li>Email : example@hello.world</li>
-				<li>Phone : 0123456789</li>
-			</ul>
+<div align='center'>
+	<div class='Memberbox'>
+		<div class='MemberboxPhoto'>
+			<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+			<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
 		</div>
-		<div class='columnPhoto'>
-			<a href='#'><img src='/images/prototype/reviews/reviews_07.jpg' /></a>
-			<a href='#'><h5>Image Information</h5></a>
-		</div>
-		<div class="clear">&nbsp;</div>
-		&nbsp;
+		<h3>Editor - Matthew Tole</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
 	</div>
-	<div class='Infobox'>
-		<div class='InfoboxText'>
-			<h2>Matthew Tole</h2>
-			<h3>Functional Specifier</h3>
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus enim leo, varius eu, sodales non, egestas nec, urna.</p>
-			<ul>
-				<li>Email : example@hello.world</li>
-				<li>Phone : 0123456789</li>
-			</ul>
+	<div class='Memberbox'>
+		<div class='MemberboxPhoto'>
+			<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+			<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
 		</div>
-		<div class='columnPhoto'>
-			<a href='#'><img src='/images/prototype/reviews/reviews_07.jpg' /></a>
-			<a href='#'><h5>Image Information</h5></a>
-		</div>
-		<div class="clear">&nbsp;</div>
-		&nbsp;
+		<h3>Editor - Matthew Tole</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
 	</div>
 </div>
-<p>&nbsp;</p>
+</div>
--- system/application/views/directory/directory_view_members.php
+++ system/application/views/directory/directory_view_members.php
@@ -8,27 +8,22 @@
 	</ul>
 </div>
 <h2>FragSoc</h2>
-<div class='Memberbox'>
-	<div class='MemberboxText'>
+<div align='center'>
+	<div class='Memberbox'>
+		<div class='MemberboxPhoto'>
+			<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+			<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
+		</div>
 		<h3>Editor - Matthew Tole</h3>
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
 	</div>
-	<div class='MemberboxPhoto'>
-		<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
-		<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
-	</div>
-	<div class="clear">&nbsp;</div>
-</div>
-<div class='Memberbox'>
-	<div class='MemberboxText'>
+	<div class='Memberbox'>
+		<div class='MemberboxPhoto'>
+			<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
+			<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
+		</div>
 		<h3>Editor - Matthew Tole</h3>
 		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos.</p>
 	</div>
-	<div class='MemberboxPhoto'>
-		<img src='/images/prototype/reviews/reviews_07.jpg' width='150'/>
-		<h5>Studying: Computer Science<br>Phone number : 07823483483<br>E-mail : <a href='mailto:editor@theyorker.co.uk'>editor@theyorker.co.uk</a></h5>
-	</div>
-	<div class="clear">&nbsp;</div>
 </div>
-<div class="clear">&nbsp;</div>
 <a href='/directory/'>Back to the directory</a>
