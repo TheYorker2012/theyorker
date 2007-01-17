26469cca68a44df3d23b06d9ff56d6be96f4d67c
Owen Jones <oj502@york.ac.uk>
2007-01-17 20:43:57 +0000

    Updated the Main directory page to remove the old blue bar, looks a little nicer now

--- system/application/views/directory/admin_directory_view.php
+++ system/application/views/directory/admin_directory_view.php
@@ -1,21 +1,3 @@
-<div id='pageheader' style='background-image: url(/images/subheadericons/pageicon_pagename.gif);'>
-	<div id='titleheader'>
-		<h1>The Yorker Directory</h1>
-	</div>
-	<div style='margin-left: 10px;'>
-		<form id='form1' name='form1' action='/directory/' method='POST'>
-			<strong>Show:</strong> 
-			Venues<input type='checkbox' name='searchrange' value='venues' checked>
-			Societies<input type='checkbox' name='searchrange' value='socs' checked>
-			Athletics Union<input type='checkbox' name='searchrange' value='au' checked>
-			Organisation<input type='checkbox' name='searchrange' value='org' checked>
-			College &#038; Campus<input type='checkbox' name='searchrange' value='campus' checked>
-			<br />
-			<input type='text' name='search'>
-			<input type='submit' name='Submit' value='Search'>
-		</form>
-	</div>
-</div>
 <h2>Organisation Information</h2>
 Organisation name : <strong>The Yorker</strong><br />
 Organisation type : <strong>Society</strong>
--- system/application/views/directory/directory.php
+++ system/application/views/directory/directory.php
@@ -1,21 +1,3 @@
-<div id='pageheader' style='background-image: url(/images/subheadericons/pageicon_pagename.gif);'>
-	<div id='titleheader'>
-		<h1>The Yorker Directory</h1>
-	</div>
-	<div style='margin-left: 10px;'>
-		<form id='form1' name='form1' action='/directory/' method='POST'>
-			<strong>Show:</strong> 
-			Venues<input type='checkbox' name='searchrange' value="venues" checked>
-			Societies<input type='checkbox' name='searchrange' value="socs" checked>
-			Athletics Union<input type='checkbox' name='searchrange' value="au" checked>
-			Organisation<input type='checkbox' name='searchrange' value="org" checked>
-			College &#038; Campus<input type='checkbox' name='searchrange' value="campus" checked>
-			<br />
-			<input type='text' name='search'>
-			<input type='submit' name='Submit' value='Search'>
-		</form>
-	</div>
-</div>
 <p>The Directory contains many different organisations. Every page has useful information about each organisation such as its contact details, reviews, events and members</p>
 <div>
 <div style='width: 90%;margin-left: auto;margin-right: auto;'>
@@ -34,7 +16,33 @@
 		</tr>
 		<?php } ?>
 	</table>
-	<div align='center'>
-		<h5>Showing 50 results, containing Venues, Societies, Athletics Union, Organisation and College &#038; Campus</h5>
-	</div>
 </div>
+<div align='center'>
+	<h5>Showing 50 results, containing Venues, Societies, Athletics Union, Organisation and College &#038; Campus</h5>
+</div>
+<form id='form1' name='form1' action='/directory/' method='POST' class='form'>
+	<fieldset>
+		<legend>Search</legend>
+		<ul>
+			<li>
+				Venues <input type='checkbox' name='searchrange' id="chkbox1" value="venues" checked>
+			</li>
+			<li>
+				Societies <input type='checkbox' name='searchrange' id="chkbox2" value="socs" checked>
+			</li>
+			<li>
+				Athletics Union <input type='checkbox' name='searchrange' id="chkbox3" value="au" checked>
+			</li>
+			<li>
+				Organisation <input type='checkbox' name='searchrange' id="chkbox4" value="org" checked>
+			</li>
+			<li>
+			College &#038; Campus <input type='checkbox' name='searchrange' id="chkbox5" value="campus" checked>
+			</li>
+		</ul>
+		<input type='text' name='search'>
+		<input type='submit' name='Submit' value='Search'>
+	</fieldset>
+	<fieldset>
+	</fieldset>
+</form>
