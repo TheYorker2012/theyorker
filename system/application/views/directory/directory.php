9d0e64a499aa43d2f5ea5cfc6b0e2905780efcad
Nick Evans <nse500@york.ac.uk>
2007-01-19 22:44:40 +0000

    Implemented AtoZ

--- system/application/views/directory/directory.php
+++ system/application/views/directory/directory.php
@@ -1,20 +1,7 @@
+
+
 <p>The Directory contains many different organisations. Every page has useful information about each organisation such as its contact details, reviews, events and members</p>
-<div>
-<div id='searchresults' style="padding:0px 0px 0px 0px">
-    <ul>
-	<?php
-	foreach ($organisations as $organisation) { ?>
-		<li>
-			<?php echo '<a href=\'/directory/' . $organisation['shortname'] . '\' style="display: inline;">' . $organisation['name']; ?></a>
-			<span style='font-size: 12px'>(<?php echo $organisation['type']; ?>)</span><br />
-			<?php echo $organisation['description']; ?>
-		</li>
-	<?php } ?>
-	</ul>
-</div>
-<div align='center'>
-	<h5>Showing <?php echo count($organisations); ?> results.</h5>
-</div>
+
 <form name='search_directory' action='/directory/' method='POST' class='form'>
 	<fieldset>
 		<legend>Search</legend>
@@ -35,9 +22,119 @@
 			College &#038; Campus <input type='checkbox' name='college_campus' value="checked">
 			</li>
 		</ul>
-		<input type='text' name='search'>
+		<input id="searchText" nameonKeyUp="searchPage('searchText','Letter');">
 		<input type='submit' name='Submit' value='Search'>
 	</fieldset>
 	<fieldset>
 	</fieldset>
 </form>
+
+<div align='center'>
+	<h5>Searching <?php echo count($organisations); ?> results.</h5>
+</div>
+
+<div id='searchresults' style="padding:0px 0px 0px 0px">
+
+<script language="javascript">
+
+insertJumpers('Jumper','Anchor');
+
+function onLoad() {
+	searchPage('searchText','Letter');
+}
+
+</script>
+
+<div id="NotFound" style="display: none;">
+<center><br>
+<b> No match was found	 </b>
+</center>
+</div>
+
+<div>
+<table width="780" border="0" cellspacing="0" cellpadding="0">
+  <tr height="14">
+	<td rowspan="2" class="AZTop">
+	<div>
+
+	<?php
+
+	$last_letter = "";
+
+	$current_letter_index = 0;
+
+	foreach ($organisations as $organisation) {
+
+		$current_letter_index ++;
+
+		$entry_name = $organisation['name'];
+
+		$current_letter = strtoupper($entry_name{0});
+
+		if($this->character_lib->isalpha($current_letter)) {
+			if ($current_letter!=$last_letter) {
+				$current_letter_index = 1;
+			?>
+			</div>
+			</td>
+		  </tr>
+		  <tr>
+			<td colspan="2">&nbsp;</td>
+		  </tr>
+		</table>
+		</div>
+		<a name="Anchor<?php echo $current_letter ?>">
+		<div id="Letter<?php echo $current_letter ?>">
+		<table width="780" border="0" cellspacing="0" cellpadding="0">
+		  <tr height="14">
+			<td width="20">&nbsp;</td>
+			<td width="338" height="14" valign="top"><div class="AZLeft"><?php echo $current_letter ?></div></td>
+			<td rowspan="2" valign="top">
+			<div class="AZTop">
+		<?php
+		}
+		$last_letter = $current_letter;
+	} else {
+		if ($last_letter != "0") {
+			$last_letter = "0";
+		?>
+			</div>
+			</td>
+		  </tr>
+		  <tr>
+			<td colspan="2">&nbsp;</td>
+		  </tr>
+		</table>
+		<div id="Letter0">
+			<table width="780" border="0" cellspacing="0" cellpadding="0">
+			  <tr height="14">
+				<td width="20">&nbsp;</td>
+				<td width="338" height="14" valign="top"><div class="AZLeft">&nbsp;</div></td>
+				<td rowspan="2" valign="top">
+				<div class="AZTop">
+
+		<?php
+		}
+		$last_letter = "0";
+	}
+	?>
+<div id="Letter<?php echo $last_letter.$current_letter_index ?>" class="AZEntry" name="<?php echo $entry_name ?>">
+
+	<?php echo '<a href=\'/directory/' . $organisation['shortname'] . '\' style="display: inline;">' . $organisation['name']; ?></a>
+	<span style='font-size: 12px'>(<?php echo $organisation['type']; ?>)</span><br />
+	<?php echo $organisation['description']; ?>
+
+</div>
+<?php
+
+
+}
+?>
+
+	</div>
+	</td>
+  </tr>
+</table>
+
+</div>
+
