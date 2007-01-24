0ac9de44d63a435e6e893daaa6f8f72894b8f791
Mark Goodall <mg512@york.ac.uk>
2007-01-24 23:14:26 +0000

    Updated the search view no CSS usage, because it is ripped from news

--- system/application/views/search/search.php
+++ system/application/views/search/search.php
@@ -1,73 +1,71 @@
 
 <!-- a_search_form defined twice, remember to recreate into class !! :) //-->
+<!-- Why is no one using CSS!!! //-->
 
-<div id='minibox'>
-    <div id='title'>Show</div>
-    <a href='#'><b>All results</b> <i>(112)</i></a>
-    <a href='#'>News <i>(41)</i></a>
-    <a href='#'>Reviews <i>(31)</i></a>
-    <a href='#'>Lifestyle <i>(12)</i></a>
-    <a href='#'>Yorkipedia <i>(28)</i></a>
-    <a href='#'>Events <i>(12)</i></a>
-    <div id='title'>Sort by</div>
-    <a href='#'><b>Relevancy</b></a>
-    <a href='#'>Date</a>
-    <a href='#'>...</a>
-</div>
-
-<div id='searchresults'>
-    <form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
-        <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
-        <input type='submit' name='submit' id='submit' value='Search' /></p>
-    </form>
-    <ol>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-        <li>
-            <a href="#">Some search results</a>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
-            adipiscing nisi.
-        </li>
-    </ol>
-    <div id="numbers">
-        <a href="#">1</a>
-        <a href="#">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">-></a>
-    </div>
-    <form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
-        <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
-        <input type='submit' name='submit' id='submit' value='Search' /></p>
-    </form>
+<div style="float: right; width: 630px; margin-left: 5px; background-color: rgb(255, 255, 255);">
+	<div style="border-style: solid none solid solid; border-color: rgb(147, 150, 154) -moz-use-text-color rgb(147, 150, 154) rgb(147, 150, 154); border-width: 1px 0pt 1px 1px; padding: 5px; width: 380px; float: left;">
+		<div class="ArticleColumn">
+			<form name='a_search_form' id='a_search_form' action='<?php echo site_url('search/search'); ?>' method='post'>
+		        <p>Search: <input type='text' name='search' id='search' value='' class='searchfield' />
+		        <input type='submit' name='submit' id='submit' value='Search' /></p>
+		    </form>
+		    <ol>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		        <li>
+		            <a href="#">Some search results</a>
+		            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse at nibh quis purus laoreet imperdiet. Pellentesque feugiat metus 
+		            adipiscing nisi.
+		        </li>
+		    </ol>
+		<p>[page numbers]</p>
+		<div style="width: 239px; float: right;">
+			<div style="border-left: 1px solid rgb(147, 150, 154); padding-left: 5px; padding-bottom: 10px;">
+				<div style="margin: 0pt; padding: 3px 3px 3px 5px; background-color: rgb(148, 151, 155); color: rgb(255, 255, 255); font-size: 12px; font-weight: bold;">
+					Show
+				</div>
+				<ul>
+					<li>All results <i>(117)</i></li>
+					<li>News <i>(42)</i></li>
+					<li>Reviews <i>(24)</i></li>
+					<li>Features <i>(42)</i></li>
+					<li>Events <i>(24)</i></li>
+					<li>How do I <i>(42)</i></li>
+					<li>Yorkipedia <i>(24)</i></li>
+				</ul>
+			</div>
+			<div style="border-style: solid solid solid none; border-color: rgb(147, 150, 154) rgb(147, 150, 154) rgb(147, 150, 154) -moz-use-text-color; border-width: 1px 1px 1px 0pt; clear: both; padding-left: 10px;">
+				<p>Sort by Relevancy</p>
+				<p>Sort by Date</p>
+			</div>
+		</div>
+	</div>
 </div>
