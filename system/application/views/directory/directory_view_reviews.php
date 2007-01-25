f298ee1b3e31730032cccd7c325b359402dc07c8
Owen Jones <oj502@york.ac.uk>
2007-01-25 20:30:45 +0000

    Updated the review in directory a bit, but really need some test data to test it with. I cant work the db lol :-(

--- system/application/views/directory/directory_view_reviews.php
+++ system/application/views/directory/directory_view_reviews.php
@@ -1,44 +1,14 @@
+<div class="clear">&nbsp;</div>
 <?php
-/*
- * Input
- * $organisation [
- *	'reviews_untyped' [review1, review2....]
- *	'reviews_by_type' [
- *		type1name => [review1, review2...],
- *		type2name => [review1, review2...]
- *	]
- * ]
- */
-if (empty($organisation['reviews_by_type'])) {
-} else {
-	foreach ($organisation['reviews_by_type'] as $review_type_name => $reviews) {
-		// $review_type_name: name of review type e.g. food, drink...
-		foreach ($reviews as $review) {
-			/*
-			 * $review is made up of:
-			 *	type - same as $review_type_name
-			 *	publish_date
-			 *	content - as parsed wikitext (html)
-			 *	link - where to link (not sure where this is supposed to link
-			 *	authors - array of authors, each with:
-			 */
-			foreach ($authors as $author) {
-				/* $author is made up of:
-				 *	name
-				 *	email
-				 */
-				
-			}
-		}
-	}
-}
-if (empty($organisation['reviews_untyped'])) {
+if (empty($organisation['reviews_untyped']) && empty($organisation['reviews_by_type'])) {
 ?>
-	<div align="center" style='padding: 100px 0px 50px 0px;'>
+	<div align='center'>
 		<b>This organisation has not been reviewed yet.</b>
 	</div>
 <?php
-} else {
+}else{}
+if (empty($organisation['reviews_untyped'])) {
+}else{
 	foreach ($organisation['reviews_untyped'] as $review) {
 		$author_links = array();
 		foreach ($review['authors'] as $author) {
@@ -49,7 +19,7 @@ if (empty($organisation['reviews_untyped'])) {
 		<div class="WhyNotTry">
 			<div class="ArticleColumn" style="width: 100%;">
 				<div style='background-color: #DDDDDD;'>
-					<div id='Byline' style="background-image: url('/images/prototype/news/benest.png');">
+					<div id='Byline'>
 						<div class='RoundBoxBL'>
 							<div class='RoundBoxBR'>
 								<div class='RoundBoxTL'>
@@ -72,6 +42,43 @@ if (empty($organisation['reviews_untyped'])) {
 			<?php echo $review['content']; ?><br />
 		</div>
 <?php
+	}
+}
+/*
+ * Input
+ * $organisation [
+ *	'reviews_untyped' [review1, review2....]
+ *	'reviews_by_type' [
+ *		type1name => [review1, review2...],
+ *		type2name => [review1, review2...]
+ *	]
+ * ]
+ */
+if (empty($organisation['reviews_by_type'])) {
+} else {
+	foreach ($organisation['reviews_by_type'] as $review_type_name => $reviews) {
+		echo "<h2>".$review_type_name." reviews:</h2>
+		<ul>";
+		// $review_type_name: name of review type e.g. food, drink...
+		foreach ($reviews as $review) {
+			/*
+			 * $review is made up of:
+			 *	type - same as $review_type_name
+			 *	publish_date
+			 *	content - as parsed wikitext (html)
+			 *	link - where to link (not sure where this is supposed to link
+			 *	authors - array of authors, each with:
+			 */
+			echo "<li><a href='".$link."'> Reviewed by ";
+			foreach ($authors as $author) {
+				/* $author is made up of:
+				 *	name
+				 *	email
+				 */
+				echo $author.", ";
+			}
+			echo "on ".$publish_date.".</a></li></ul>";
+		}
 	}
 }
 ?>
