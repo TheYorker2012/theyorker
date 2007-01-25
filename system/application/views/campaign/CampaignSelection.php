fb68789e9cc4535f9ea89611ab8ab987d85518ee
Richard Ingle <ri504@york.ac.uk>
2007-01-25 00:31:14 +0000

    Campaign details now gets some data from the model.

--- system/application/views/campaign/CampaignSelection.php
+++ system/application/views/campaign/CampaignSelection.php
@@ -7,19 +7,25 @@
 	<div class="subsubtitle">CLICK ON THE CAMPAIGN TO FIND OUT MORE</div><Br>
 	<div class="Container">
 	<div class="HalfBox">
-<b>			<a href="<?php echo site_url('campaign/details/Swimming Pool'); ?>">> SWIMMING POOL</a><Br>
-			<a href="<?php echo site_url('campaign/details/Anti-Arms'); ?>">> ANTI-ARMS</a><Br>
-			<a href="<?php echo site_url('campaign/details/Kill All Ants'); ?>">> KILL ALL ANTS</a><Br>
-			<a href="<?php echo site_url('campaign/details/Buddhist Campus'); ?>">> BUDDHIST CAMPUS</a></b>
+		<b>
+			<?php
+                        foreach ($data['vars']['Campaign_List'] as $campaigns)
+                        {
+				echo '<a href="'.site_url('campaign/details/').'/'.$campaigns['id'].'">'.$campaigns['name'].'</a><br/>';
+			}
+			?>
+		</b>
 	</div>
 	<div class="HalfBox">
 		<div style="text-align:center;font-weight: bold;font-size: 18px;">OR VOTE NOW</div>
 		<div style="padding-left:80px;">
 		<form style="display: inline;">
-		<input type="radio" value="1" name="vote"> SWIMMING POOL<br>
-		<input type="radio" value="2" name="vote"> ANTI-ARMS<br>
-		<input type="radio" value="3" name="vote"> KILL ALL ANTS<br>
-		<input type="radio" value="4" name="vote"> BUDDHIST CAMPUS<br>
+		<?php
+                foreach ($data['vars']['Campaign_List'] as $campaigns)
+                {
+			echo '<input type="radio" value="'.$campaigns['id'].'" name="vote">'.$campaigns['name'].'<br/>';
+		}
+		?>
 		<input style="width: 75%;" type="submit" value="Vote">
 		</form>
 	</div></div>
@@ -28,12 +34,23 @@
 	<div class="subtitle">CURRENT RESULTS</div>
 	<div class="subsubtitle">CHOISE DEADLINE FOR STUDENTS IS <?php echo $DeadLine; ?></div><br>
 	<div style="float: left;">
-<b>			<a href="<?php echo site_url('campaign/details/Swimming Pool'); ?>">> SWIMMING POOL</a><Br>
-			<a href="<?php echo site_url('campaign/details/Anti-Arms'); ?>">> ANTI-ARMS</a><Br>
-			<a href="<?php echo site_url('campaign/details/Kill All Ants'); ?>">> KILL ALL ANTS</a><Br>
-			<a href="<?php echo site_url('campaign/details/Buddhist Campus'); ?>">> BUDDHIST CAMPUS</a></b>
+		<b>
+			<?php
+                        foreach ($data['vars']['Campaign_List'] as $campaigns)
+                        {
+				echo '<a href="'.site_url('campaign/details/').'/'.$campaigns['id'].'">'.$campaigns['name'].'</a><br/>';
+			}
+			?>
+		</b>
 	</div>
-	<div style="float: left; padding-left: 30px;"><div class="votebar" style="width: 50px;"></div>92<br><div class="votebar" style="width: 198px;"></div>198<br><div class="votebar" style="width: 213px;"></div>213<br><div class="votebar" style="width: 92px;"></div>92</div>
+	<div style="float: left; padding-left: 30px;">
+		<?php
+                foreach ($data['vars']['Campaign_List'] as $campaigns)
+                {
+			echo '<div class="votebar" style="width: 50px;"></div>'.$campaigns['votes'].'<br/>';
+		}
+		?>
+	</div>
 	<div class="breaker"></div> <!-- Remove This Hack Sometime -->
 	</div>
 	</div>
