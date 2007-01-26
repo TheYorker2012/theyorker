9620951b310a3e9fa8c86ed6b1c6a8a1ea4a8fb6
Owen Jones <oj502@york.ac.uk>
2007-01-26 21:38:02 +0000

    Updated looks a little nicer

--- system/application/views/directory/directory_view.php
+++ system/application/views/directory/directory_view.php
@@ -1,10 +1,11 @@
+<div class="clear">&nbsp;</div>
 <div class='RightToolbar'>
 	<div class='RightToolbarHeader'>
 	Information
 	</div>
-	<p style='text-align:center;'>
-	<img width='220' src='/images/prototype/directory/about/178327854723856.jpg' />
-	</p>
+	<div style='text-align:center; padding: 10px 5px 10px 5px;'>
+	<img width='200' src='/images/prototype/directory/about/178327854723856.jpg' />
+	</div>
 	<p>
 		<?php if (!empty($organisation['website'])) {
 			echo '<img alt="Website" name="Website" src="/images/prototype/directory/link.gif" /> <a href="'.
@@ -25,22 +26,30 @@
 	Reviews
 	</div>
 	<ul>
-		<li><a href=''>Static Data1</a></li>
-		<li><a href=''>Static Data2</a></li>
-		<li><a href=''>Static Data3</a></li>
-		<li><a href=''>Static Data4</a></li>
+		<li><a href='#'>Static Data1</a></li>
+		<li><a href='#'>Static Data2</a></li>
+		<li><a href='#'>Static Data3</a></li>
+		<li><a href='#'>Static Data4</a></li>
 	</ul>
 	<div class='RightToolbarHeader'>
 	Related Articles
 	</div>
 	<ul>
-		<li><a href=''>Static Data1</a></li>
-		<li><a href=''>Static Data2</a></li>
-		<li><a href=''>Static Data3</a></li>
-		<li><a href=''>Static Data4</a></li>
+		<li><a href='#'>Static Data1</a></li>
+		<li><a href='#'>Static Data2</a></li>
+		<li><a href='#'>Static Data3</a></li>
+		<li><a href='#'>Static Data4</a></li>
 	</ul>
 </div>
-<h2>About Us</h2>
-<p><?php echo $organisation['description']; ?></p>
-<h2>Finding Us</h2>
-<img width='400' src='/images/prototype/directory/about/gmapwhereamI.png' />
+<div style="width: 400px; margin: 0px; padding-right: 3px; ">
+	<div style="border: 1px solid #2DC6D7; padding: 5px; font-size: small; margin-bottom: 4px; ">
+		<span style="font-size: x-large;  color: #2DC6D7; ">About Us</span>
+		<p><?php echo $organisation['description']; ?></p>
+	</div>
+	<div style="border: 1px solid #BBBBBB; padding: 5px; font-size: small; margin-bottom: 4px; ">
+		<span style="font-size: x-large;  color: #BBBBBB; ">Finding Us</span>
+		<p>
+		<img width='390' src='/images/prototype/directory/about/gmapwhereamI.png' />
+		</p>
+	</div>
+</div>
