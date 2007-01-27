2cdc66d392d356c3d123ff6548920fadddc1ba43
Mark Goodall <mg512@york.ac.uk>
2007-01-27 18:57:24 +0000

    Uploader - still looking for the relative path

--- system/application/controllers/upload.php
+++ system/application/controllers/upload.php
@@ -30,7 +30,7 @@ class Upload extends Controller {
 		$config['max_height']  = '768';
 		
 		$data = array();
-		echo realpath("./public_html/tmp/uploads/");
+		$data[] = realpath("./public_html/tmp/uploads/");
 		$this->load->library('upload', $config);
 		for ($x = 1; $x <= $this->input->post('destination'); $x++) {
 			if ( ! $this->upload->do_upload('userfile'.$x)) {
