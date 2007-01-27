1242d39f5425338a0a4773932ba135208d4243ac
David Huscroft <dgh500@york.ac.uk>
2007-01-27 00:50:53 +0000

    simple test of my model

---
+++ system/application/controllers/test/davemodeltest.php
@@ -0,0 +1,35 @@
+<?php
+
+class DaveModelTest extends Controller {
+	
+	/**
+	 * @brief Default constructor.
+	 */
+	function __construct()
+	{
+		parent::Controller();
+	
+		// Load the public frame
+		$this->load->library('frame_public');
+	}
+	
+	/**
+	 * @brief ModelTest test page.
+	 */
+	function index()
+	{
+		$this->load->model('review_model','review');
+		
+		//Load data from model
+		$reviews = $this->review->GetReview('boxing_club','lifestyle');
+		
+		// Set up the public frame
+		$this->frame_public->SetTitle('FOR FUCKS SAKE');
+		$this->frame_public->SetContentSimple('test/davemodeltest',$reviews);
+		
+		// Load the public frame view (which will load the content view)
+		$this->frame_public->Load();
+		
+	}
+}
+?>
