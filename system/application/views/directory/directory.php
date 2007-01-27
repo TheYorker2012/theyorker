4d4e1785d5683a96e5dd612c2fdae91ccc6d0f1c
Owen Jones <oj502@york.ac.uk>
2007-01-27 11:45:51 +0000

    Minor change of loading animation position. New position will not get covered by search suggestion lists, or change the height of the page.

--- system/application/views/directory/directory.php
+++ system/application/views/directory/directory.php
@@ -5,7 +5,8 @@
 		<div style='padding: 10px 5px 10px 5px; font-size:small;'>
 			<form name='search_directory' action='' method='POST' class='form'>
 				Enter a keyword below:<br />
-				<div style='padding: 5px; width: 100%;'><input id="searchText" width='300' name="search" onKeyUp="searchPage('searchText','Letter','filterCheck');"><div id="loadingdiv" style="display: none;"> <image src="/images/prototype/prefs/loading.gif" width="16" height="16" />  </div> </div>
+				<div id="loadingdiv"style="padding: 5px; display: none; float:right;"> <image src="/images/prototype/prefs/loading.gif" width="16" height="16" /></div>
+				<div style='padding: 5px;'><input id="searchText" width='300' name="search" onKeyUp="searchPage('searchText','Letter','filterCheck');"> </div>
 				e.g. football
 
 			</form>
