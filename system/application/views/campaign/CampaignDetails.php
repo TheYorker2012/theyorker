dceb39b787d7b75426b6500dd7a572cf59cc4328
Chris Travis <ctravis@gmail.com>
2007-01-28 17:51:27 +0000

    Inline CSS = bad - See http://real.theyorker.co.uk/wiki/CSS Altered new Campaign pages to use new CSS declarations

--- system/application/views/campaign/CampaignDetails.php
+++ system/application/views/campaign/CampaignDetails.php
@@ -1,52 +1,28 @@
-	<div style="width: 220px; margin: 0; padding-left: 3px; float: right; ">
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	Sign
-	</div>
+<div class='RightToolbar'>
+	<h4>Sign</h4>
 	<p style="margin-top: 0px; padding: 8px;">I, <b>John Smith</b>, think that this The Yorker should campaign for this cause.<br><br>#TODO</p>
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	Other Campaigns
-	</div>
+	<h4>Other Campaigns</h4>
 	<p style="margin-top: 0px; padding: 8px;">#TODO</p>
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	Find out more
-	</div>
+	<h4>Find out more</h4>
 	<p style="margin-top: 0px; padding: 8px;">#TODO</p>
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	Related Stories
-	</div>
+	<h4>Related Stories</h4>
 	<p style="margin-top: 0px; padding: 8px;">#TODO</p>
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	External Links
-	</div>
+	<h4>External Links</h4>
 	<p style="margin-top: 0px; padding: 8px;">#TODO</p>
-	
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	Comments
-	</div>
+    <h4>Comments</h4>
 	<p style="margin-top: 0px; padding: 8px;">#TODO</p>
-	</div>
+</div>
 
-	 
-	
-<div style="width: 420px; margin: 0px; padding-right: 3px; ">
+<div class='grey_box'>
+	<h2>why ban cake?</h2>
+	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+</div>
 
-	<div style="border: 1px solid #BBBBBB; padding: 6px; font-size: small; margin-bottom: 4px; ">
-	<span style="font-size: x-large;  color: #BBBBBB; ">why ban cake?</span><br /><br />
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-	</div>
-
-	<div style="border: 1px solid #2DC6D7; padding: 6px; font-size: small; margin-bottom: 4px; ">
-	<span style="font-size: x-large;  color: #2DC6D7; ">facts</span><br />
-		<ul style="padding-left: 15px; margin-top: 5px; margin-bottom: 0px;">
+<div class='blue_box'>
+	<h2>facts</h2>
+	<ul style="padding-left: 15px; margin-top: 5px; margin-bottom: 0px;">
 		<li style="padding-bottom: 15px;">Counting only the economic value of fisheries, tourism, and shoreline protection, the costs of destroying 1km of coral reef ranges between US$137,000-1,200,000 over a 25-year period.</li>
 		<li style="padding-bottom: 15px;">Properly managed coral reefs can yield an average of 15 tonnes of fish and other seafood per square kilometre each year.</li>
 		<li style="padding-bottom: 15px;">Cake is a made up drug.</li>
-		</ul>
-	</div>
-	
+	</ul>
 </div>
--- system/application/views/campaign/CampaignSelection.php
+++ system/application/views/campaign/CampaignSelection.php
@@ -1,71 +1,60 @@
-<div style="width: 220px; margin: 0; padding-left: 3px; float: right; ">
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	About Campaigns
+<div class='RightToolbar'>
+	<h4>About Campaigns</h4>
+	<div class='Entry'>
+		<a href='/news/article/2'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
+		<h5>What are these about?</h5>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
 	</div>
-	<p>
-		<div style="float: right; ">
-		<a href='/news/oarticle/2'><img src='/images/prototype/news/thumb3.jpg' alt='Some Spy' title='Some Spy' /></a>
-		</div>
-		<p style="color: #FF9933; font-weight: bold; margin-bottom: 0px;">What are these about?</p>
-		<p style="margin-top: 0px; font-size: x-small; ">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc elementum arcu non risus. Vestibulum arcu enim, placerat nec, malesuada eget, pharetra at, mi. Nullam rhoncus porttitor nunc.Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-	</p>
-	<div style="padding: 5px; background-color: #999; color: #fff; font-size: small; font-weight: bold; ">
-	What now?
+	<h4>What now?</h4>
+	<div class='Entry'>
+		<a href='/news/article/2'><img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' /></a>
+		<h5>What do I do now?</h5>
+		<p>Well my friend, you wait and wait then see the results. This page will change to something else because you smell.</p>
 	</div>
-	<p>
-		<div style="float: right; ">
-		<a href='/news/oarticle/2'><img src='/images/prototype/news/thumb2.jpg' alt='Some Spy' title='Some Spy' /></a>
-		</div>
-		<p style="color: #FF9933; font-weight: bold; margin-bottom: 0px;">What do I do now?</p>
-		<p style="margin-top: 0px; font-size: x-small; ">Well my friend, you wait and wait then see the results. This page will change to something else because you smell.</p>
-	</p>
 </div>
 
-<div style="width: 420px; margin: 0px; padding-right: 3px; ">
-	<div style="border: 1px solid #2DC6D7; padding: 6px; font-size: small; margin-bottom: 4px; ">
-	<span style="font-size: x-large;  color: #2DC6D7; "><?php echo $sections['current_campaigns']['title']; ?></span><br />
-		<?php echo $sections['current_campaigns']['blurb']; ?><br /><br />
-		<table width="100%">
-			<?php
-			$total_votes = 0;
-                        foreach ($data['vars']['Campaign_List'] as $campaigns)
-			{
-				$total_votes += $campaigns['votes'];
-			}
-                        foreach ($data['vars']['Campaign_List'] as $campaigns)
-			{
-				$percentage = $campaigns['votes']/$total_votes*100;
-				echo '<tr>
-					<td>
-					<b><a href="'.site_url('campaign/details/').'/'.$campaigns['id'].'">'.$campaigns['name'].'</a></b>
-					</td><td style="width:40%; border: thin solid teal;">
-					<div style="float: left; width: '.$percentage.'%; background-color: teal;">&nbsp</div>
-					<div stlye="float: right;">&nbsp;'.round($percentage).'%</div>
-					</td>
-					</tr>';
-			}
-			?>
-		</table>
-		<br />
-		<?php echo $sections['current_campaigns']['deadline_text']; ?>
-
-	</div>
-
-	<div style="border: 1px solid #BBBBBB; padding: 6px; font-size: small; margin-bottom: 4px; ">
-	<span style="font-size: x-large;  color: #BBBBBB; "><?php echo $sections['vote_campaigns']['title']; ?></span><br />
-		<?php echo $sections['vote_campaigns']['blurb']; ?>
-		<form id='form1' name='form1' action='#' method='POST'>
-		</form>
-		<form id='form1' name='form1' action='#' method='POST' class='form'>
-		<table width="80%">
+<div class='blue_box'>
+	<h2><?php echo $sections['current_campaigns']['title']; ?></h2>
+	<?php echo $sections['current_campaigns']['blurb']; ?><br /><br />
+	<table width="100%">
 		<?php
-                foreach ($data['vars']['Campaign_List'] as $campaigns)
-                {
-			echo '<tr><td style="text-align: right;">'.$campaigns['name'];
-			echo '</td><td><fieldset style="display: inline;"><input type="submit" name="addorgform_addbutton" value="Vote" class="button" /></fieldset><br /></td></tr>';
+		$total_votes = 0;
+                        foreach ($data['vars']['Campaign_List'] as $campaigns)
+		{
+			$total_votes += $campaigns['votes'];
+		}
+                        foreach ($data['vars']['Campaign_List'] as $campaigns)
+		{
+			$percentage = $campaigns['votes']/$total_votes*100;
+			echo '<tr>
+				<td>
+				<b><a href="'.site_url('campaign/details/').'/'.$campaigns['id'].'">'.$campaigns['name'].'</a></b>
+				</td><td style="width:40%; border: thin solid teal;">
+				<div style="float: left; width: '.$percentage.'%; background-color: teal;">&nbsp</div>
+				<div stlye="float: right;">&nbsp;'.round($percentage).'%</div>
+				</td>
+				</tr>';
 		}
 		?>
-		</table>
-		</form>
-	</div>
+	</table>
+	<br />
+	<?php echo $sections['current_campaigns']['deadline_text']; ?>
+</div>
+
+<div class='grey_box'>
+	<h2><?php echo $sections['vote_campaigns']['title']; ?></h2>
+	<?php echo $sections['vote_campaigns']['blurb']; ?>
+	<form id='form1' name='form1' action='#' method='POST'>
+	</form>
+	<form id='form1' name='form1' action='#' method='POST' class='form'>
+	<table width="80%">
+	<?php
+                foreach ($data['vars']['Campaign_List'] as $campaigns)
+                {
+		echo '<tr><td style="text-align: right;">'.$campaigns['name'];
+		echo '</td><td><fieldset style="display: inline;"><input type="submit" name="addorgform_addbutton" value="Vote" class="button" /></fieldset><br /></td></tr>';
+	}
+	?>
+	</table>
+	</form>
 </div>
