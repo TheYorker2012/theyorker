ac4ea1a91683e3ee5dab4c73cba86262f586d8c5
Frank Burton <frankablu@gmail.com>
2007-01-28 01:53:56 +0000

    Missed a file read comment below

--- system/application/models/review_model.php
+++ system/application/models/review_model.php
@@ -7,13 +7,7 @@
 **  Does: Gets the data for review page, and leagues
 **  Todo: Make the 'return' values properly formatted (not just result arrays)
 **        Do the /table/x/y function. NOTE: where does the 'star' 'price' and 'user' ratings come from - price is a tag group? /confused!
-**		  : Should probably be something like GetTagGroup($tag_group_id,$order_by (tag_group_id, 0 = user rating ?),$order_direction)
-**
-**		  : If you want to be really clever, you could:
-**			 - join the tag_group to the content_type determine content_type
-			 - find all other tag groups in content_type
-
-
+**		  : Should probably be something like GetTagGroup($tag_group_id,$order_by (enum 'star','price','user'?],$order_direction)
 **		  : If tag_group_ordered is 1 for a tag group then order by tag_order, otherwise by tag_name
 **		  : 'star' and 'price' are names of tags, ordered by tag_order and tag_name respectively
 **		  : user rating comes from a join like this (left join in case the the organisation is unrated):
@@ -32,7 +26,8 @@ class Review_model extends Model {
 		parent::Model();
 	}
 
-	//Gets comments from database, frb501
+// Notice Commit Clash - Not sure how to handle it properly.... we seem to have done the same work but I'm indexing from comments.comment_page_id and you are indexing from the organisations table. My system is currently in a working state however your's is probility the better way to do it... so I commented out yours temporatly, sorry frb501
+/*	//Gets comments from database, frb501
 	function GetComments($organisation_directory_entry_name, $content_type_codename)
 	{
 		$sql = "SELECT comment_text, comment_timestamp, comment_rating
@@ -64,7 +59,7 @@ class Review_model extends Model {
 		return $nocomments;
 		}
 	}
-
+*/
 	function GetReview($organisation_directory_entry_name,$content_type_codename) {
 
 	#dgh500
@@ -161,59 +156,78 @@ class Review_model extends Model {
 	return $league;
 	}
 
+	//Gets comments from database, frb501
+	function GetComments($page_no, $subject)
+	{
+		$sql = "SELECT comment_text, comment_timestamp, comment_rating FROM comments WHERE comment_content_type_id = ? AND comment_subject_id = ?";
+		$query = $this->db->query($sql,array($page_no,$subject));
 
+		if ($query->num_rows() > 0)
+		{
+			$commentno = 0;
+			foreach ($query->result() as $row)
+			{
+			$comments['comment_author'][$commentno] = 'nothing';
+			$comments['comment_rating'][$commentno] = $row->comment_rating;
+			$comments['comment_date'][$commentno] = $row->comment_timestamp;
+			$comments['comment_content'][$commentno] = $row->comment_text;
+			$commentno++;
+			}
 
-// ** frb501 - Whole Section Below is outdated, Messy and needs to be redone to the new database *****
-//   ** Just delete it probaility unless you see something you can use
+			return $comments;
+		}
+		else
+		{
+		$nocomments = 'empty';
+		return $nocomments;
+		}
+	}
 
-//Section - Main Page
+	//Adds a comment to the database, frb501
+	function SetComment($post_data)
+	{
+		$comment['comment_content_type_id'] = $post_data['comment_page_id'];
+		$comment['comment_subject_id'] = $post_data['comment_subject_id'];
+		$comment['comment_user_entity_id'] = $post_data['comment_user_entity_id'];
+		$comment['comment_text'] = $post_data['comment_text'];
+		$this->db->insert('comments',$comment); //Add users comment to database
+	}
 
-	//Gets data which does not change often
-//	function getStatic($thingname)
-//	{
-//		$sql = "SELECT page_property_text FROM page_properties WHERE page_property_label=?";
-//		$query = $this->db->query($sql,$thingname);
-//		return $query->result_array();
-//	}
-
-	//Retreives a random set of 5 try ideas from the given type string
-	//This should be it... I hope....
-//	function getTryArray($type)
-//	{
-//		$sql = "SELECT /organisations.organisation_name,review_type_organisations.rto_organisation_entity_id,review_type_organisations.rto_review_type_id  FROM organisations,review_type_organisations,review_types WHERE (organisations.  	organisation_organisation_entity_id_parent = review_type_organisations.rto_organisation_entity_id AND review_type_organisations.rto_review_type_id = review_types.review_type_id AND review_types.review_type_name=?) LIMIT 0, 5";
-
-//		$query = $this->db->query($sql,array($type));
-	//	return $query->result_array();
-
-//	}
-
-// Review Types Page
-
-	//Returns the blub for a query
-//	function getReviewBlurb($reviewname)
-//	{
-//		$sql = "SELECT review_type_blurb FROM review_types WHERE review_type_name=?";
-//		$query = $this->db->query($sql,$reviewname);
-//		return $query->result_array();
-//	}
-
-//	function getReviewData($reviewid)
-//	{
-//		$sql = "SELECT * FROM rto_content WHERE rto_content_id = ?";
-//		$query = $this->db->query($sql,$reviewid);
-//		$reviewdata = $query->row();
-
-//		//Get data which can be directly extracted
-//		$returndata['price'] = $reviewdata->rto_content_price;
-//		$returndata['article_content'] = $reviewdata->rto_content_blurb;
-//		$returndata['yorker_recommendation'] = $reviewdata->rto_content_recommend;
-//		$returndata['website_booking'] = $reviewdata->rto_content_book_online;
-
-		//Get data about the organisation
-//		$sql = "SELECT organisations.organisation_name,organisations_address,organisations.organisation_postcode,organisations.organisation_url,organisations.organisation_opening_hours FROM organisations, rto_content WHERE organisation_entity_id = ? AND (organisations.organisation_entity_id = rto_content.rto_content_organisation_entity_id)";
-//		$query = $this->db->query($sql,$reviewdata->rto_content_organisation_entity_id);
-//
-//	}
+	//Mirrored from GetReview - frb501 - This should return all the rows in a given type
+	function TableReview($content_type_codename) {
+	
+	#dgh500
+	# need organisation type?
+	# need organisation fileas - what IS this?? all null in DB
+	$sql = '
+			SELECT 
+			organisations.organisation_name,
+			organisations.organisation_url,
+			organisations.organisation_opening_hours,
+			organisations.organisation_directory_entry_name,
+			review_context_contents.review_context_content_blurb,
+			review_context_contents.review_context_content_recommend_item_price,
+			review_context_contents.review_context_content_recommend_item,
+			review_context_contents.review_context_content_average_price_upper,
+			review_context_contents.review_context_content_average_price_lower,
+			review_context_contents.review_context_content_rating,
+			review_context_contents.review_context_content_directions,
+			review_context_contents.review_context_content_book_online
+			  FROM content_types 
+			  INNER JOIN review_context_contents
+			  ON content_types.content_type_id = review_context_contents.review_context_content_content_type_id 
+			  INNER JOIN organisations
+			  ON review_context_contents.review_context_content_organisation_entity_id = organisations.organisation_entity_id
+			  WHERE content_types.content_type_codename = "'.$content_type_codename.'"';
+	
+	$result = $query = $this->db->query($sql);
+	
+	$reviews = $query->result_array();
+	$reviews['item_count'] = $query->num_rows();
+	
+	return $reviews;
+	
+	}
 
 }
 
