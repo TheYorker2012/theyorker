c5c2d52507abb394faf1d3d82b345d2ddaeaf513
Nick Evans <nse500@york.ac.uk>
2007-01-28 17:38:52 +0000

    Fixed db settings

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://www.your-site.com/
 |
 */
-$config['base_url']	= "http://ado.is-a-geek.net:81/";
+$config['base_url']	= "/";
 
 /*
 |--------------------------------------------------------------------------
