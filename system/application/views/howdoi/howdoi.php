773849ce362bad936e5698ba66c95568af6854e0
Frazer Barnsley <frazzle@gmail.com>
2007-01-31 20:55:31 +0000

    Put link to other page

--- system/application/views/howdoi/howdoi.php
+++ system/application/views/howdoi/howdoi.php
@@ -11,16 +11,17 @@
   </form>
   <br />
   <h4>Question Categories</h4>
-  <a href="#">Opening Times</a><br />
-  <a href="#">Numbers</a><br />
-  <a href="#">Essentials</a><br />
-  <a href="#">Other Info</a><br />
-  <a href="#">The Nearest ...</a><br />
+  <a href="view">Opening Times</a><br />
+  <a href="view">Numbers</a><br />
+  <a href="view">Essentials</a><br />
+  <a href="view">Other Info</a><br />
+  <a href="view">The Nearest ...</a><br />
 </div>
 
 <div class="grey_box">
   <h2>How do I?</h2>
-  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+  <b>Click any of the links to see the other page :)</b>
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
 Nunc elementum arcu non risus. Vestibulum arcu enim,
 placerat nec, malesuada eget, pharetra at, mi. Nullam
 rhoncus porttitor nunc. Lorem ipsum dolor sit amet,
@@ -36,35 +37,35 @@ porttitor nunc.
 <div class="blue_box">
   <h2>Question Categories</h2>
   <h5>Opening Times</h5><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
   <br /><h5>Numbers</h5><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
   <br /><h5>Essentials</h5><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
   <br /><h5>Other Info</h5><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
-  <a href="#">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
+  <a href="view">Costcutter</a><br />
 </div>
