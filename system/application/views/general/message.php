235b7a5b7faf5aebe9a6e96400d6e60ee930ce63
Owen Jones <oj502@york.ac.uk>
2007-02-02 23:18:17 +0000

    Fixed the message thing, now uses div and css.

--- system/application/views/general/message.php
+++ system/application/views/general/message.php
@@ -1,20 +1,16 @@
-<?php /*
-This is very much just a bodge to get something working
-(it looks awful and uses a table)
-*/ ?>
-<TABLE borderwidth="0" width="100%"><TR><TD bgcolor="
-	<?php
-		if ($class === 'error')
-			echo '#FF0000';
-		elseif ($class === 'warning')
-			echo '#FF8000';
-		elseif ($class === 'information')
-			echo '#FFFF00';
-		elseif ($class === 'success')
-			echo '#00FF00';
-		else
-			echo '#808080';
-	?>
-">
-<?php echo $text; ?>
-</TD></TR></TABLE>
+<div style="float: right; width: 100%; text-align: center; font-size: 12px; background-color: 
+		<?php
+			if ($class === 'error')
+				echo '#FF0000';
+			elseif ($class === 'warning')
+				echo '#FF8000';
+			elseif ($class === 'information')
+				echo '#FFFF00';
+			elseif ($class === 'success')
+				echo '#00FF00';
+			else
+				echo '#808080';
+		?>
+	;">
+	<?php echo $text ?>
+</div>
