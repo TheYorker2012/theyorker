cccc55b374e474012709605cbbebf9110da127cf
Nick Evans <nse500@york.ac.uk>
2007-02-03 15:18:25 +0000

    Added vip login

--- system/application/views/viparea/index.php
+++ system/application/views/viparea/index.php
@@ -1,31 +1,26 @@
-<div class='RightToolbar'>
-<h4>Suggestions</h4>
-<h5>Advertising</h5>
-<p>
-Use theyorker to advertise your events, its cheap and it helps charity what is there to lose you tight bastards.
-</p>
-<h4>Reminders</h4>
-Your next event starts in 2 hours<br />
-1 event has been canceled <a href='#'>Relist?</a><br />
+<div class="RightToolbar">
+	<h4>Register</h4>
+	<div class="Entry">
+		To find out more about getting your organisation on theyorker, <a href="/register/">Click here</a>.
+	</div>
 </div>
-<div class='grey_box'>
-	<h2>welcome</h2>
-	<p>
-		<?php echo $main_text; ?>
-	</p>
-</div>
-<div class='blue_box'>
-	<h2>My Tasks</h2>
-	<ul>
-		<li>Edit a directory entry</li>
-		<ul>
-			<li><a href='/viparea/directory/theyorker/'>The Yorker</a></li>
-			<li><a href='/viparea/directory/fragsoc/'>FragSoc</a></li>
-			<li><a href='/viparea/directory/evil_eye_lounge/'>Evil Eye Lounge</a></li>
-		</ul>
-		<li><a href='/viparea/calendar/'>Manage events</a></li>
-		<li><a href='/viparea/members/'>Manage members</a></li>
-		<li><a href='/viparea/advertising/'>Advertising</a></li>
-		<li><a href='/viparea/account/'>My Account</a></li>
-	</ul>
+<div class="blue_box">
+	<h2>log in</h2>
+	Good afternoon, John. Welcome to the VIP area. Please select the organisation that you wish to represent, and then reenter your university password.
+	<form name='login_form' action='/viparea/main' method='POST' class='form'>
+		<fieldset>
+			<label for='organisation'>Choose Your Organisation:</label>
+			<select name="organisation">
+				<option value="theyorker">The Yorker</option>
+			</select>
+			<br />
+			<label for='password'>Reenter Your Password:</label>
+			<input type='password' name='password'>
+			<br />
+			<label for='login_button'></label>
+			<input type='submit' class='button' name='login_button' value='Enter'>
+		</fieldset>
+		<fieldset>
+		</fieldset>
+	</form>
 </div>
