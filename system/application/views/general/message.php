47abb62321fcc7b7367e55eba4133a10c8e684cb
Richard Rout <rr512@york.ac.uk>
2007-02-03 16:13:33 +0000

    message images updates

--- system/application/views/general/message.php
+++ system/application/views/general/message.php
@@ -1,15 +1,15 @@
 <div style="margin-top: 5px;" class=" 
 		<?php
 			if ($class === 'error')
-				echo 'warning_box"><img src="/images/prototype/homepage/warning.png" /';
+				echo 'warning_box"><img src="/images/prototype/homepage/error.png" /';
 			elseif ($class === 'warning')
 				echo 'warning_box"><img src="/images/prototype/homepage/warning.png" /';
 			elseif ($class === 'information')
-				echo 'information_box"><img src="/images/prototype/homepage/infomark.png" /';
+				echo 'information_box"><img src="/images/prototype/homepage/information.png" /';
 			elseif ($class === 'success')
-				echo 'information_box"><img src="/images/prototype/homepage/infomark.png" /';
+				echo 'information_box"><img src="/images/prototype/homepage/sucess.png" /';
 			else
-				echo 'warning_box"><img src="/images/prototype/homepage/questionmark.png" /';
+				echo 'information_box"><img src="/images/prototype/homepage/questionmark.png" /';
 		?>
 	;">
 	<?php echo $text ?>
