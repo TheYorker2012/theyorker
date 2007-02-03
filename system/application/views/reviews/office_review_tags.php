63c0176304db208101ef52d16e4d439b89bc3255
Nick Evans <nse500@york.ac.uk>
2007-02-03 18:15:53 +0000

    tags view

---
+++ system/application/views/reviews/office_review_tags.php
@@ -0,0 +1,34 @@
+<div class='RightToolbar'>
+	<h4>Areas for Attention</h4>
+	<div class="Entry">
+		<div class="information_box">
+			The following reviews are waiting to be published:
+			<ul>
+				<li><a href='#'>Dan Ashby 02/02/2007</a></li>
+				<li><a href='#'>Charlotte Chung 02/02/2007</a></li>
+			</ul>
+		</div>
+		<div class="information_box">
+			<img src="/images/prototype/homepage/infomark.png" />
+			<a href='#'>Information</a> has been updated and is waiting to be published.
+		</div>
+		<div class="information_box">
+			There are <a href='#'>Comments</a> that have been reported for abuse.
+		</div>
+	</div>
+<h4>What's this?</h4>
+	<p>
+		<?php echo 'whats_this'; ?>
+	</p>
+<h4>Other tasks</h4>
+<ul>
+	<li><a href='#'>Maintain my account</a></li>
+	<li><a href='#'>Remove this directory entry</a></li>
+</ul>
+</div>
+
+<form class='form'>
+<div class="blue_box">
+	<h2>list boxes for each tag group</h2>
+</div>
+</form>
