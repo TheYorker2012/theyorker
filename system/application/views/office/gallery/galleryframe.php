d882fc0b2fd15f8ca38dbbb267a54c3a9d5f4573
Richard Rout <rr512@york.ac.uk>
2007-02-04 15:34:30 +0000

    intuitive

--- system/application/views/office/gallery/galleryframe.php
+++ system/application/views/office/gallery/galleryframe.php
@@ -7,27 +7,29 @@
 			<input type="radio" name="searchcriteria" />Photographer<br /><br />
 			Search Criteria:<input type="text" />
 			<input type="submit" class="buttom" value="Search" />
+		</form>
 	</div>
 	<h4>Advanced</h4>
 	<div class="Entry">
+		<form id="form">
 			Order by:<br />
 			<input type="radio" name="order" />Title<br />
 			<input type="radio" name="order" />Date<br />
-			<input type="radio" name="order" />Photographer<br />
-			<input type="submit" class="buttom" value="Display" /><br /><br />
+			<input type="radio" name="order" />Photographer<br /><br />
 			Show only tags:<br />
 			<select name="tag">
 				<option value="pope">Pope</option>
 				<option value="pope">Students</option>
 				<option value="pope">Bob</option>
 				<option value="pope">Gary</option>
-			<input type="submit" class="buttom" value="Display" /><br /><br />
+			</select><br /><br />
 			Show only photographers:<br />
 			<select name="tag">
 				<option value="pope">Pope</option>
 				<option value="pope">Students</option>
 				<option selected value="pope">Bob</option>
 				<option value="pope">Gary</option>
+			</select><br /><br />
 			<input type="submit" class="buttom" value="Display" />
 		</form>
 	</div>
