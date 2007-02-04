03cf15e1c5790cebf63fbc9f6a9307d4e1ffdf68
James Hogan <james_hogan@theyorker.co.uk>
2007-02-04 14:53:11 +0000

    Moved some stuff from admin menu to office menu, and removed test/phpinfo controller as its already accessible from /admin/dev

--- system/application/views/admin/admin.php
+++ system/application/views/admin/admin.php
@@ -2,14 +2,6 @@
 This is just a placeholder page at the moment, this will be the menu for all logged in root admins.
 This will be used by everyone for the charley launch, but is only inteneded for root admins in the long run.
 </p>
-<a href="/admin/faq/add">Add faq</a><br />
-<a href="/admin/howdoi/add">Add to how do I</a><br />
-<a href="/admin/faq/edit">Edit FAQ entry</a><br />
-<br />
-<a href="/admin/news/">(Editor/Writer/Photographer) News Home</a><br />
-<a href="/admin/news/request/add/">Add News Article Request</a><br />
-<a href="/admin/news/request/view/">View Article Request</a><br />
-<br />
 <a href="/admin/useradmin/edit">Edit User Information</a><br />
 <br />
 <a href="/admin/pages">Page properties, custom pages, etc.</a><br />
--- system/application/views/office/index.php
+++ system/application/views/office/index.php
@@ -27,3 +27,13 @@
 		</fieldset>
 	</form>
 </div>
+
+<a href="/admin/faq/add">Add faq</a><br />
+<a href="/admin/howdoi/add">Add to how do I</a><br />
+<a href="/admin/faq/edit">Edit FAQ entry</a><br />
+<br />
+<a href="/admin/news/">(Editor/Writer/Photographer) News Home</a><br />
+<a href="/admin/news/request/add/">Add News Article Request</a><br />
+<a href="/admin/news/request/view/">View Article Request</a><br />
+<br />
+<a href="/admin/pages">Page properties, custom pages, etc.</a><br />
