a7e76a8061e07c14f2845dbb4665187d7bdf6fc2
Richard Rout <rr512@york.ac.uk>
2007-02-04 14:57:08 +0000

    gallery side bar

--- system/application/views/office/gallery/galleryframe.php
+++ system/application/views/office/gallery/galleryframe.php
@@ -1,11 +1,35 @@
 <div class="RightToolbar">
-	<h4>Search by Tag</h4>
+	<h4>Search by...</h4>
 	<div class="Entry">
-		If you have forgotten your password, get in contact with your editor, and he will reset it.
+		<form id="form">
+			<input type="radio" name="searchcriteria" />Title<br />
+			<input type="radio" name="searchcriteria" />Tag<br />
+			<input type="radio" name="searchcriteria" />Photographer<br /><br />
+			Search Criteria:<input type="text" />
+			<input type="submit" class="buttom" value="Search" />
 	</div>
-	<h4>Get Involved</h4>
+	<h4>Advanced</h4>
 	<div class="Entry">
-		If you would like to get involved in writing for the yorker, click <a href='/office/register/'>here</a>.
+			Order by:<br />
+			<input type="radio" name="order" />Title<br />
+			<input type="radio" name="order" />Date<br />
+			<input type="radio" name="order" />Photographer<br />
+			<input type="submit" class="buttom" value="Display" /><br /><br />
+			Show only tags:<br />
+			<select name="tag">
+				<option value="pope">Pope</option>
+				<option value="pope">Students</option>
+				<option value="pope">Bob</option>
+				<option value="pope">Gary</option>
+			<input type="submit" class="buttom" value="Display" /><br /><br />
+			Show only photographers:<br />
+			<select name="tag">
+				<option value="pope">Pope</option>
+				<option value="pope">Students</option>
+				<option selected value="pope">Bob</option>
+				<option value="pope">Gary</option>
+			<input type="submit" class="buttom" value="Display" />
+		</form>
 	</div>
 </div>
 <div class="blue_box">
