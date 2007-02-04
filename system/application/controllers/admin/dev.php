b465e5d6b023a58f41afb2575edd577a59677005
James Hogan <james_hogan@theyorker.co.uk>
2007-02-04 09:26:38 +0000

    Moved dev page into admin folder + added permissions

---
+++ system/application/controllers/admin/dev.php
@@ -0,0 +1,29 @@
+<?php
+
+class Dev extends Controller {
+
+	function index()
+	{
+		SetupMainFrame('admin');
+		if (CheckPermissions(array('student','admin'))) {
+			$op  = '<a href="/admin/dev/phpinfo">PHP information</a><br />';
+			$op .= 'If you think this is wrong then email mg512<br />';
+			$op .= 'Info dumps follow:<br /><pre>';
+			exec('svn info', $ops);
+			$op .= implode("\n",$ops);
+			$op .= '<pre />';
+			
+			$this->main_frame->SetContent(new SimpleView($op));
+		}
+		$this->main_frame->SetTitle('Devr\'s Status page');
+		$this->main_frame->Load();
+	}
+	
+	function phpinfo()
+	{
+		if (CheckPermissions(array('student','admin'))) {
+			phpinfo();
+		}
+	}
+}
+?>
