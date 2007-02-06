6f0445f4b3567759fb0bc18fbffecec16c64b6b5
Richard Rout <rr512@york.ac.uk>
2007-02-06 21:17:10 +0000

    campaign edit pages

--- system/application/views/campaign/CampaignsEditDetails.php
+++ system/application/views/campaign/CampaignsEditDetails.php
@@ -1,12 +1,32 @@
-	<br>
-	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
-		<div class="title">Edit Campaign Details: <?php echo $CampaignTitle; ?></div>
-		Title: <br/>
-		<input type="text" style="width: 100%;" value="<?php echo $CampaignTitle; ?>" />
-		Details: <br/>
-		<textarea style="width: 100%; height: 500px;"></textarea>
-		Author: <input type="text" style="width: 40%;" /> Society: <input type="text" style="width: 40%;" /><br/>
-		External Links: <input type="text" style="width: 80%;" /><br/><br/>
-		<input type="submit" value="Save Changes" style="width: 20%;"> ¦ <input type="submit" value="Delete Campaign" style="width: 20%;">
-	</div>
-	<br>
+<div class="RightToolbar">
+	<h4>Fat</h4>
+	I don't know...
+</div>
+
+<div class="blue_box">
+	<h2>edit campaign</h2>
+	<form class="form">
+		<fieldset>
+			<label for="title">Title:</label>
+			<input type="text" name="title" />
+			<label for="blurb">Blurb:</label>
+			<textarea cols=35 rows=10></textarea>
+			<label for="title">Publish Date:</label>
+			<input type="text" name="title" /><span class="grey"> (dd/mm/yyyy)</span>
+			<label for="facts">Facts (each fact on a new line):</label>
+			<textarea cols=35 rows=5></textarea>
+			<label for="title">Find out more (give instructions on how to find out more):</label>
+			<textarea cols=35 rows=5></textarea>
+			<label for="blurb">Society:</label>
+			<select>
+				<option>RoboSoc</option>
+				<option>SantaSoc</option>
+				<option>BabySoc</option>
+				<option>WuvSoc</option>
+			</select>
+			<label for="title">External Links (each link on a new line):</label>
+			<textarea cols=35 rows=5></textarea>
+			<input type="submit" class="button" value="Create" />
+		</fieldset>
+	</form>
+</div>
--- system/application/views/campaign/CampaignsEditSelect.php
+++ system/application/views/campaign/CampaignsEditSelect.php
@@ -1,12 +1,30 @@
-	<br>
-	<div class="wholepage2"> <!-- Remove This Hack Sometime -->
-	<div class="title">Edit Campaign</div>
-	<div class="subsubtitle">Please select a campaign to edit.</div>
-		<ul>
-			<a href="<?php echo site_url('campaign/edit/Swimming Pool'); ?>"><li style="font-weight: bold;">SWIMMING POOL</li></a>
-			<a href="<?php echo site_url('campaign/edit/Anti-Arms'); ?>"><li style="font-weight: bold;">ANTI-ARMS</li></a>
-			<a href="<?php echo site_url('campaign/edit/Kill All Ants'); ?>"><li style="font-weight: bold;">KILL ALL ANTS</li></a>
-			<a href="<?php echo site_url('campaign/edit/Buddhist Campus'); ?>"><li style="font-weight: bold;">BUDDHIST CAMPUS</li></a>
-		</ul>
-		<input type="submit" value="New Campaign" style="width: 20%;">
-	</div>
+<div class="RightToolbar">
+	<h4>Fat</h4>
+	I don't know...
+</div>
+
+<div class="grey_box">
+	<h2>edit campaigns</h2>
+	<h3 style="display: inline;">ADO on facebook</h3>: 32 votes<br />
+	<a href="/campaign/edit/ADO_On_Facebook">[edit]</a> <a href="#">[remove]</a><br /><br />
+	<h3 style="display: inline;">Dr. Cox</h3>: 12 votes<br />
+	<a href="/campaign/edit/ADO_On_Facebook">[edit]</a> <a href="#">[remove]</a><br /><br />
+	<h3 style="display: inline;">Spam</h3>: 8 votes<br />
+	<a href="/campaign/edit/ADO_On_Facebook">[edit]</a> <a href="#">[remove]</a><br /><br />
+	<h3 style="display: inline;">Pool</h3>: 2242 votes<br />
+	<a href="/campaign/edit/ADO_On_Facebook">[edit]</a> <a href="#">[remove]</a><br /><br />
+</div>
+<div class="blue_box">
+	<h2>create campaign</h2>
+	<form class="form">
+		<fieldset>
+			<label for="title">Title:</label>
+			<input type="text" name="title" />
+			<label for="blurb">Blurb:</label>
+			<textarea cols=35 rows=10></textarea>
+			<label for="date">Publish Date:</label>
+			<input type="text" name="date" />
+			<input type="submit" class="button" value="Create" />
+		</fieldset>
+	</form>
+</div>
