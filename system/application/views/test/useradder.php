ad0c33660250d4901f1933224274c350c621bbdd
James Hogan <james_hogan@theyorker.co.uk>
2007-02-06 21:11:34 +0000

    Added temporary admin user adder (see source code for safety password)

---
+++ system/application/views/test/useradder.php
@@ -0,0 +1,15 @@
+<FORM CLASS="form" METHOD="POST" ACTION="/test/useradder/add">
+<FIELDSET>
+<label for='firstname'>Firstname:</label><input name='firstname'><br />
+<label for='surname'>Surname:</label><input name='surname'><br />
+<label for='email'>Email:</label><input name='email'><br />
+<label for='nickname'>Nickname:</label><input name='nickname'><br />
+
+<label for='safety'>Backdoor key:</label><input type='password' name='safety'><br />
+<label for='username'>Username:</label><input name='username'><br />
+<label for='password'>Password:</label><input type='password' name='password'><br />
+<label for='officepassword'>Office Password:</label><input type='password' name='officepassword'><br />
+
+<input type='submit' CLASS="button" name='login_button' value='Add'>
+</FIELDSET>
+</FORM>
