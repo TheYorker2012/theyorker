3bb9d068952c1ee1359013a9dc8b3bc5bbfb20d8
Mark Goodall <mg512@york.ac.uk>
2007-02-07 13:01:32 +0000

    image admin -- object doing wierd stuff

--- system/application/controllers/search.php
+++ system/application/controllers/search.php
@@ -5,6 +5,19 @@ class Search extends Controller {
 	function __construct()
 	{
 		parent::Controller();
+		define("ORDER_RELEVANCE", 	1);
+		define("ORDER_EARLY", 		2);
+		define("ORDER_LATE", 		3);
+
+		define("FILTER_ALL", 		0b1111111111111111);
+		define("FILTER_NEWS", 		0b0000000000000001);
+		define("FILTER_FEATURES", 	0b0000000000000010);
+		define("FILTER_LIFESTYLE", 	0b0000000000000100);
+		define("FILTER_FOOD",		0b0000000000001000);
+		define("FILTER_DRINK", 		0b0000000000010000);
+		define("FILTER_CULTURE",	0b0000000000100000);
+		define("FILTER_YORK",		0b0000000001000000);
+		$this->load->model('search');
 	}
 	
 	function index()
@@ -57,15 +70,17 @@ class Search extends Controller {
 		                   'title' => 'TODO',
 		                   'blurb' => 'TODO');
 		
+		
 		$data = array('search_form' => 'TODO Searchform again',
 		              'search_numbering' => '101',
-		              'search_all' => '101',
-		              'search_news' => '101',
-		              'search_reviews' => '101',
-		              'search_features' => '101',
-		              'search_events' => '101',
-		              'search_how' => '101',
-		              'search_york' => '101',
+		              'search_all' => 		'1111111111111111',
+		              'search_news' => 		'0000000000000001',
+		              'search_features' => 	'0000000000000010',
+		              'search_lifestyle' =>	'0000000000000100',
+		              'search_food' =>	 	'0000000000001000',
+		              'search_drink' =>		'0000000000010000',
+		              'search_culture' =>	'0000000000100000',
+		              'search_york' => 		'0000000001000000',
 		              'search_results' => $results);
 		
 		$this->main_frame->SetContentSimple('search/results', $data);
