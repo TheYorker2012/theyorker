7deedccfc7d4a21c0d2340f9e24d1fd64980db14
Mark Goodall <mg512@york.ac.uk>
2007-02-07 12:27:52 +0000

    image admin -- object doing wierd stuff

--- system/application/views/admin/images_index.php
+++ system/application/views/admin/images_index.php
@@ -3,15 +3,15 @@ TODO Gallery system to view and edit photos and associated images
 <p>
 <?php
 $column = 0;
-echo var_dump($shownPhotos);
-//if ($shownPhotos) foreach($shownPhotos->result() as $photo) {
-//	if ($column = PHOTOS_PERROW) {
-//		$column = 0;
-//		echo '</p><p>';
-//	}
-//	echo '<a href="id/'.$photo->photo_id.'"><img src="'.imageLocation($photo->photo_id, $imageType->image_type_id).'" /></a>';
-//	$column++;
-//}
+//echo var_dump($shownPhotos);
+if ($shownPhotos) foreach($shownPhotos->row() as $photo) {
+	if ($column = PHOTOS_PERROW) {
+		$column = 0;
+		echo '</p><p>';
+	}
+	echo '<a href="id/'.$photo->photo_id.'"><img src="'.imageLocation($photo->photo_id, $imageType->image_type_id).'" /></a>';
+	$column++;
+}
 ?>
 </p>
 <?=$pages?>
