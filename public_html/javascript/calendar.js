aeab98d1ceb811216f5771d7d80131ab91bf3bab
David Harker <dhh500@york.ac.uk>
2007-02-08 20:52:49 +0000

    timing spacers styled up

--- public_html/javascript/calendar.js
+++ public_html/javascript/calendar.js
@@ -55,11 +55,39 @@ function collapseEvent (arrid) {
 	new Effect.Fade ('ev_es_'+arrid,{duration:0.2});	
 }
 
-function sortByStartTime(a, b) {
-    var w = a.starttime.split(':')[0]
-    var x = b.starttime.split(':')[0]
-    var y = a.starttime.split(':')[1]
-    var z = b.starttime.split(':')[1]
+function sortByStartTime(dayOfEvents) {
+	var jev = '';
+	for (i in dayOfEvents) {
+		(jev.length > 0) ? (jev += ',') : 0;
+		jev += i;
+	}
+	jev = eval ('['+jev+']');
+	
+	var i = 0;
+	var tres = 0;
+	var temp;
+	while (i < jev.length) {
+		if (i != (jev.length - 1)) {
+			tres = compareDayStartTimes (dayOfEvents[jev[i]], dayOfEvents[jev[i+1]]);
+			//alert('COMP:'+dayOfEvents[jev[i]].starttime+' with '+dayOfEvents[jev[i+1]].starttime+' = '+tres);
+			if (tres == 1) {
+				temp = dayOfEvents[jev[i+1]];
+				dayOfEvents[jev[i+1]] = dayOfEvents[jev[i]];
+				dayOfEvents[jev[i]] = temp;
+				i = 0;
+			}
+		}
+		i++;
+	}
+
+	return dayOfEvents;
+}
+
+function compareDayStartTimes (a,b) {
+    var w = parseInt (a.starttime.split(':')[0]);
+    var x = parseInt (b.starttime.split(':')[0]);
+    var y = parseInt (a.starttime.split(':')[1]);
+    var z = parseInt (b.starttime.split(':')[1]);
     return ((w < x) ? -1 : ((w > x) ? 1 : ((y < z) ? -1 : (y > z) ? 1 : 0) ));
 }
 
@@ -72,9 +100,7 @@ function draw_calendar (events) {
 	for (day in events) {
 		
 		// sort the events into chronological order
-//		events[day] = events[day].sort(sortByStartTime);
-
-
+		events[day] = sortByStartTime(events[day]);
 	
 	}
 	
@@ -90,17 +116,82 @@ function draw_calendar (events) {
 		for (event_index in events[day]) {
 			
 			debug += ' event_index='+event_index+'\n';
+			debug += '  start time='+events[day][event_index]['starttime']+'\n';
 			
-			//
-			if (i > 100)
-				break;
-			else
-				i++;
+			draw_calendar_event (events[day][event_index]);
+
 		}
 	}
 	
-	alert (debug);
+	//alert (debug);
 	
+}
+
+// takes the name of the day and an object with a single event in it
+// appends the relevant code into the relevant day div
+function draw_calendar_event (indEvent) {
+	
+	var daysDiv = ['calviewMonday','calviewTuesday','calviewWednesday','calviewThursday','calviewFriday','calviewSaturday'];
+	var outHtml = '\n';
+	
+	outHtml += '					<div id=\"ev_%%refid%%\" class=\"calviewIndEventBox\">\n';
+	outHtml += '					<div>\n';
+	outHtml += '						<div id=\"calviewIECtrlButtonBLBound\">\n';
+	outHtml += '						\n';
+	outHtml += '							<div class=\"calviewCloseButton\" onclick=\"removeEvent(%%refid%%);hideEventMenu()\">\n';
+	outHtml += '								<a href=\"#\" onclick=\"return false\"\n';
+	outHtml += '								style=\"text-decoration: none; color: #ffffff;\">X</a>\n';
+	outHtml += '							</div>\n';
+	outHtml += '							\n'; 
+	outHtml += '							<div class=\"calviewExpandButton\"\n';
+	outHtml += '								onmouseover=\"expandEvent(%%refid%%)\"\n';
+	outHtml += '								onmouseout=\"collapseEvent(%%refid%%)\"\n';
+	outHtml += '								style=\"cursor: pointer\">\n';
+	outHtml += '								V\n';
+	outHtml += '							</div>\n';
+	outHtml += '							\n';
+	outHtml += '						</div>\n';
+	outHtml += '						\n';
+	outHtml += '						<strong>%%name%%</strong>\n';
+	outHtml += '						\n';
+	outHtml += '						<div class=\"calviewExpandedSmall\" id=\"ev_es_%%refid%%\" style=\"display: none\">\n';
+	outHtml += '						<div>\n';
+	outHtml += '							%%starttime%% to %%endtime%%<br />At: %%shortloc%%<br /><i>%%blurb%%</i>\n';
+	outHtml += '						</div>\n';
+	outHtml += '						</div>\n';
+	outHtml += '						\n';
+	outHtml += '					</div>\n';
+	outHtml += '					</div>\n\n';
+	
+	// replace the strings as would be done server side
+	
+	outHtml = outHtml.replace (/%%refid%%/g,indEvent[0]);
+	outHtml = outHtml.replace (/%%name%%/g,indEvent.name);
+	outHtml = outHtml.replace (/%%starttime%%/g,indEvent.starttime);
+	outHtml = outHtml.replace (/%%endtime%%/g,indEvent.endtime);
+	outHtml = outHtml.replace (/%%shortloc%%/g,indEvent.shortloc);
+	outHtml = outHtml.replace (/%%blurb%%/g,indEvent.blurb);
+
+	$(daysDiv[parseInt (indEvent.day)]).innerHTML += outHtml;	
+	draw_calendar_timeSpacer (daysDiv[parseInt (indEvent.day)],'123124','G');
+
+}
+
+function draw_calendar_timeSpacer (dayDivName,text,goodbad) {
+
+	var outHtml = '\n';
+
+	outHtml += '					<div class=\"calviewTimeSpacer%%tsc%%\">\n';
+	outHtml += '					<div>\n';
+	outHtml += '						%%tsText%%\n';
+	outHtml += '					</div>\n';
+	outHtml += '					</div>\n';
+	
+	outHtml = outHtml.replace (/%%tsc%%/g,goodbad);
+	outHtml = outHtml.replace (/%%tsText%%/g,text);
+
+	$(dayDivName).innerHTML += outHtml;	
+
 }
 
 
