f1cff47375da7b6fcb5c1d119c2590c07a616687
Mark Goodall <mg512@york.ac.uk>
2007-02-08 16:34:53 +0000

    bugfix -- linkfix and back to gd2

--- system/application/views/uploader/admin_upload_cropper.php
+++ system/application/views/uploader/admin_upload_cropper.php
@@ -124,4 +124,4 @@ foreach ($ThumbDetails->result() as $Single) {
 		<input type="hidden" name="width" id="width" />
 		<input type="hidden" name="height" id="height" />
 </form>
-<p><?=anchor('admin/images', 'Return')?></p>
+<p><?=anchor('admin/image', 'Return')?></p>
