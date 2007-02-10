43beac4a60c0c7aad1e441cd0eace355f28e7703
Chris Travis <ctravis@gmail.com>
2007-02-10 18:31:52 +0000

    Changes to login view for XHTML comformance. Also some, imho, much needed javascript so I don't have to reach for the mouse every time I want to login :p

--- system/application/views/login/login.php
+++ system/application/views/login/login.php
@@ -13,35 +13,42 @@
 			echo '<P>'.$login_message.'</P>';
 		}
 	?>
-	<form name='login_form' action='<?php echo $target; ?>' method='POST' class='form'>
+	<form name='login_form' action='<?php echo $target; ?>' method='post' class='form'>
 		<fieldset>
 			<?php if (isset($login_id)) { ?>
-				<input type='hidden' name='login_id' value='<?php echo $login_id; ?>'>
+				<input type='hidden' name='login_id' value='<?php echo $login_id; ?>' />
 			<?php } if (isset($usernames)) { ?>
 				<label for='username'>Select organisation:</label>
-				<SELECT name="username">
+				<select name="username">
 				<?php foreach ($usernames as $id => $name) { ?>
-					<OPTION VALUE="<?php echo $id; ?>"><?php echo $name; ?></OPTION>
+					<option value="<?php echo $id; ?>"><?php echo $name; ?></option>
 				<?php } ?>
-				</SELECT>
+				</select>
 				<br />
 			<?php } elseif (isset($username)) { ?>
 				<label for='username'>Username:</label>
-				<input name='username' value='<?php echo $username; ?>'>
+				<input name='username' id='username' value='<?php echo $username; ?>' />
 			<br />
 			<?php } ?>
 			<label for='password'>Password:</label>
-			<input type='password' name='password'>
+			<input type='password' id='password' name='password' />
 			<br />
 			<?php if (isset($keep_login)) { ?>
 				<label for='keep_login'>Remember me</label>
-				<input type='checkbox' name='keep_login' id="keep_login" value="<?php echo $keep_login; ?>">
+				<input type='checkbox' name='keep_login' id='keep_login' value="<?php echo $keep_login; ?>" />
 				<br />
 			<?php } ?>
-			<label for='login_button'></label>
-			<input type='submit' class='button' name='login_button' value='Login'>
-		</fieldset>
-		<fieldset>
+			<input type='submit' class='button' name='login_button' value='Login' />
 		</fieldset>
 	</form>
+	<script type='text/javascript'>
+	var element = document.getElementById('password');
+	if (element) {
+		element.focus();
+	}
+	element = document.getElementById('username');
+	if (element) {
+		element.focus();
+	}
+	</script>
 </div>
