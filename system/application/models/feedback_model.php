2ce356f1c87972df208fe2a0e34e143db6e107c7
Chris Travis <ctravis@gmail.com>
2007-02-16 23:08:36 +0000

    Can now hide/delete feedback entries and view hidden/deleted entries.

--- system/application/models/feedback_model.php
+++ system/application/models/feedback_model.php
@@ -31,17 +31,18 @@ class Feedback_model extends Model
 	}
 
 	/// Get number of outstanding feedback comments
-	function GetFeedbackCount ()
+	function GetFeedbackCount ($deleted = 0)
 	{
 		$sql = 'SELECT COUNT(*) AS feedback_count
-				FROM feedback_entries';
-		$query = $this->db->query($sql);
+				FROM feedback_entries
+				WHERE feedback_entry_deleted = ?';
+		$query = $this->db->query($sql,array($deleted));
 		$result = $query->row_array();
 		return $result['feedback_count'];
 	}
 
 	/// Get all feedback entries for display in admin area for review
-	function GetAllFeedback ()
+	function GetAllFeedback ($deleted = 0)
 	{
 		$sql = 'SELECT feedback_entry_id AS id,
 				 feedback_entry_page_name AS page,
@@ -51,12 +52,22 @@ class Feedback_model extends Model
 				 feedback_entry_comment AS comment,
 				 DATE_FORMAT(feedback_entry_timestamp, \'%a, %D %b %y @ %H:%i\') AS time
 				FROM feedback_entries
+				WHERE feedback_entry_deleted = ?
 				ORDER BY feedback_entry_timestamp DESC';
-		$query = $this->db->query($sql);
+		$query = $this->db->query($sql, array($deleted));
 		$result = array();
 		foreach ($query->result_array() as $row) {
 			$result[] = $row;
 		}
 		return $result;
+	}
+
+	function DeleteFeedback ($entry_id)
+	{
+		$sql = 'UPDATE feedback_entries
+				SET feedback_entry_deleted = 1
+				WHERE feedback_entry_id = ?';
+		$query = $this->db->query($sql, array($entry_id));
+		return $this->db->affected_rows();
 	}
 }
--- system/application/views/admin/feedback.php
+++ system/application/views/admin/feedback.php
@@ -1,6 +1,13 @@
 	<script type='text/javascript' src='/javascript/prototype.js'></script>
-	<script type='text/javascript' src='/javascript/scriptaculous.js'></script>
+	<script type='text/javascript' src='/javascript/scriptaculous.js?load=effects'></script>
 	<script type='text/javascript'>
+	<?php if ($editable) { ?>
+	function deleteEntry (id) {
+		document.getElementById('feedback' + id).innerHTML = "<div class='ajax_loading'><img src='/images/prototype/prefs/loading.gif' alt='Deleting' title='Deleting' /> Deleting Entry</div>";
+		xajax_deleteEntry(id);
+		return false;
+	}
+	<?php } ?>
 	function showEntry (id) {
 		if (document.getElementById('feedback' + id).style.display == 'none') {
 			Effect.BlindDown('feedback' + id);
@@ -12,6 +19,12 @@
 		return false;
 	}
 	</script>
+
+	<div class='blue_box' style='width: auto;'>
+		<h2><?php if (!$editable) { echo 'Deleted '; } ?>Feedback Entries</h2>
+		<a href='/admin/feedback'><span id='new_entries'><?php echo $new_entries; ?></span> new feedback entries</a><br />
+		<a href='/admin/feedback/deleted'><span id='deleted_entries'><?php echo $deleted_entries; ?></span> deleted feedback entries</a>
+	</div>
 
 	<?php foreach ($entries as $entry) { ?>
 	<div id='container<?php echo $entry['id']; ?>' class='feedback'>
@@ -32,6 +45,12 @@
 		</div>
 		<div id='feedback<?php echo $entry['id']; ?>' class='main'>
 			<?php echo nl2br($entry['comment']); ?>
+			<div style='text-align: center;'><br />
+				<?php if ($editable) { ?>
+				<a href='/admin/feedback' onclick="return deleteEntry('<?php echo $entry['id']; ?>');"><span class='blue' style='float:right'>[ Delete Entry ]</span></a>
+				<?php } ?>
+				<span class='form'><button class='button' style='float:none;' onclick="return showEntry('<?php echo $entry['id']; ?>');">Close</button></span>
+			</div>
 		</div>
 	</div>
 	<script type='text/javascript'>document.getElementById('feedback<?php echo $entry['id']; ?>').style.display = 'none';</script>
