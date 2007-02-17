a38577e6ffc7c45a8d692f06563d04852c424648
Mark Goodall <mg512@york.ac.uk>
2007-02-17 20:50:23 +0000

    Gallery

--- system/application/controllers/office/gallery.php
+++ system/application/controllers/office/gallery.php
@@ -236,13 +236,13 @@ define('BASE_DIR', '/home/theyorker/public_html');
 			return $objResponse;
 		}
 		
-		$config['image_library'] = 'gd2';
+		$config['image_library'] = 'gd';
 //		$config['library_path'] = '/usr/bin/';
 		$config['source_image'] = $selectedThumb[0];
 		$config['width'] = $formData['width'];
 		$config['height'] = $formData['height'];
 		$config['maintain_ratio'] = FALSE;
-		$config['new_image'] = imageLocation($selectedThumb[4], $selectedThumb[3], null, TRUE);
+		$config['new_image'] = BASE_DIR.imageLocation($selectedThumb[4], $selectedThumb[3], null, TRUE);
 		$config['x_axis'] = $formData['x1'];
 		$config['y_axis'] = $formData['y1'];
 		
