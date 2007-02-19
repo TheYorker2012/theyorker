c1a23f64f48c960f8773e615edb621b16d136304
Richard Ingle <ri504@york.ac.uk>
2007-02-19 16:14:38 +0000

    charity views rename

--- system/application/views/charity/charity.php
+++ system/application/views/charity/charity.php
@@ -1,13 +1,12 @@
 <div class='RightToolbar'>
-	<h4><?php echo $sections['sidebar_goal']['title']; ?></h4>
+	<h4><?php echo $sections['sidebar_about']['title']; ?></h4>
 	<div class='Entry'>
-		<h5><?php echo $sections['sidebar_goal']['subtitle']; ?></h5>
+		<h5><?php echo $sections['sidebar_about']['subtitle']; ?></h5>
 		<?php echo $sections['charity']['target_text']; ?>
 	</div>
-	
-	<h4><?php echo $sections['sidebar_funding']['title']; ?></h4>
+	<h4><?php echo $sections['sidebar_help']['title']; ?></h4>
 	<div class='Entry'>
-		<?php echo $sections['sidebar_funding']['text']; ?>
+		<?php echo $sections['sidebar_help']['text']; ?>
 	</div>
 	
 	<h4><?php echo $sections['sidebar_related']['title']; ?></h4><!--Next 2 sections basically the same with different data and links-->
@@ -31,13 +30,18 @@
 	</div>
 </div>
 
-
-
-
+<img src="/images/prototype/homepage/rowing.jpg" alt="" title="" width="420" height="93" />
 
 <div class='grey_box'>
 	<h2><?php echo $sections['article']['heading']; ?></h2>
 	<span class="black"><?php echo $sections['article']['text']; ?></span>
+</div>
+
+<div class='blue_box'>
+	<h2><?php echo $sections['funding']['title']; ?></h2>
+	<div class='Entry'>
+		<?php echo $sections['funding']['text']; ?>
+	</div>
 </div>
 
 
@@ -45,7 +49,7 @@
 
 if (isset($sections['progress_reports']['entries']))
 {
-	echo '<div class="blue_box">';
+	echo '<div class="grey_box">';
 	echo '<h2>'.$sections['progress_reports']['title'].'</h2>';
 	foreach ($sections['progress_reports']['entries'] as $pr_entry)
 	{
@@ -57,15 +61,10 @@ if (isset($sections['progress_reports']['entries']))
 
 ?>
 
-<div class='grey_box'>
-	<h2><?php echo $sections['section_help']['title']; ?></h2>
-	<?php echo $sections['section_help']['text']; ?>
-</div>
-
 <?php
-/*
+
 echo '<pre>';
 echo print_r($data);
 echo '</pre>';
-*/
+
 ?>
