cb79b15cfc894f405e869248d1fdad60e1b52756
Mark Goodall <mg512@york.ac.uk>
2007-02-26 12:22:34 +0000

    Odd error, one of the <br /> is being dropped...

--- system/application/views/directory/viparea_directory_photos.php
+++ system/application/views/directory/viparea_directory_photos.php
@@ -21,14 +21,18 @@
 <div class='blue_box'>
 <h2> photo upload </h2>
 <div id="source" style="display:none">
-	<label for="title">Photo Title:</label><input type="text" name="title" size="30" /></br />
-	<label for="userfile">Photo File:</label><input type="file" name="userfile" size="30" /><br />
+	<label for="title">Photo Title:</label><input type="text" name="title" size="30" />
+	</br />
+	<label for="userfile">Photo File:</label><input type="file" name="userfile" size="30" />
+	<br />
 </div>
 <?=form_open_multipart('viparea/'.$organisation['shortname'].'/directory/photos/upload'); ?>
 <p>Photo's should be in jpg format. The upload size limit is 2mb(?).</p><br />
 <div>
-	<label for="title1">Photo Title:</label><input type="text" name="title1" size="30" /><br />
-	<label for="userfile1">Photo File:</label><input type="file" name="userfile1" size="30" /><br />
+	<label for="title1">Photo Title:</label><input type="text" name="title1" size="30" />
+	<br />
+	<label for="userfile1">Photo File:</label><input type="file" name="userfile1" size="30" />
+	<br />
 </div>
 <input type="hidden" name="destination" id="destination" value="1" />
 
