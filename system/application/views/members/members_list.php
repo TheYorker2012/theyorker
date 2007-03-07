03fc0a082b88c53cdd02d42319b6930d7d6b8c62
James Hogan <james_hogan@theyorker.co.uk>
2007-03-07 00:45:37 +0000

    #127 Members stuff, more support for teams

--- system/application/views/members/members_list.php
+++ system/application/views/members/members_list.php
@@ -60,7 +60,7 @@ function FilterLinkBool($filter, $field, $value)
 ?>
 
 <div class='blue_box'>
-	<h2>members</h2>
+	<h2>Members of <?php echo $organisation['name']; ?></h2>
 	<form class="form" method="post" action="<?php echo $target; ?>" name="member_select_form" id="member_select_form">
 		
 		<table style="border: 1px solid #ccc;" cellspacing="0" cellpadding="2">
@@ -76,6 +76,7 @@ function FilterLinkBool($filter, $field, $value)
 				<?php BoolFilterLinks($filter, 'paid'); ?></th>
 			<th><?php SortLink($filter, $sort_fields, 'vip','VIP'); ?><br />
 				<?php BoolFilterLinks($filter, 'vip'); ?></th>
+			<th>Subteams</th>
 			<th>Edit</th>
 		</tr>
 		<?php foreach ($members as $membership) {?>
@@ -86,15 +87,28 @@ function FilterLinkBool($filter, $field, $value)
 					id="user<?php echo $membership['user_id']; ?>" /></td>
 			<td><label for="user<?php echo $membership['user_id']; ?>"><?php echo $membership['firstname']; ?></label></td>
 			<td><label for="user<?php echo $membership['user_id']; ?>"><?php echo $membership['surname']; ?></label></td>
-			<?php if (NULL !== $membership['email']) { ?>
-				<td><a href='mailto:<?php echo $membership['email'];?>'><?php echo $membership['email']; ?></a></td>
+			<td><?php if (NULL !== $membership['email']) { ?>
+				<a href='mailto:<?php echo $membership['email'];?>'>email</a>
 			<?php } else {?>
-				<td><IMG SRC="/images/prototype/members/nomail.png" ALT="not available" /></td>
-			<?php } ?>
-			<td><?php FilterLinkBool($filter, 'confirmed', $membership['confirmed']); ?></td><?php /*
-			<td><?php FilterLinkBool($filter, 'mailable', $membership['on_mailing_list']); ?></td>*/ ?>
-			<td><?php FilterLinkBool($filter, 'paid', $membership['paid']); ?></td>
-			<td><?php FilterLinkBool($filter, 'vip', $membership['vip']); ?></td>
+				<IMG SRC="/images/prototype/members/nomail.png" ALT="not available" />
+			<?php } ?></td>
+			<td><?php if (isset($membership['confirmed'])) FilterLinkBool($filter, 'confirmed', $membership['confirmed']); ?></td><?php /*
+			<td><?php if (isset($membership['on_mailing_list'])) FilterLinkBool($filter, 'mailable', $membership['on_mailing_list']); ?></td>*/ ?>
+			<td><?php if (isset($membership['paid'])) FilterLinkBool($filter, 'paid', $membership['paid']); ?></td>
+			<td><?php if (isset($membership['vip'])) FilterLinkBool($filter, 'vip', $membership['vip']); ?></td>
+			<td><small><?php
+				foreach ($membership['teams'] as $team) {
+					if ($team['confirmed']) {
+						echo '<IMG SRC="/images/prototype/members/yes9.png" ALT="confirmed" />';
+					} else {
+						echo '<IMG SRC="/images/prototype/members/no9.png" ALT="not confirmed" />';
+					}
+					//echo '<A HREF="'.vip_url($filter['base'] .'/team/'.$team['team_id']).'">';
+					echo '<A HREF="'.vip_url($filter['base'] .'/team/'.$team['team_id']).'">';
+					echo $team['team']['name'];
+					echo '</A><br />';
+				}
+			?></small></td>
 			<td><a href='<?php echo vip_url('members/info/'.$membership['user_id']); ?>'>Edit</a></td>
 		</tr>
 		<?php } ?>
@@ -112,7 +126,7 @@ function FilterLinkBool($filter, $field, $value)
 		
 		<fieldset>
 			<label for="invite_team">Invite to team:</label>
-			<select name="invite_team"><?php EchoOptionTeams($organisation, 0, FALSE, -1); ?></select>
+			<select name="invite_team"><?php EchoOptionTeams($organisation, 0, TRUE, 0); ?></select>
 			<input type='submit' class='button' name='members_select_invite_button' value='Invite'>
 		</fieldset>
 	</form>
