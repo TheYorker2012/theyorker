a8acf20edd15d72e3c8dfcc38009b39fd33babbe
Chris Travis <ctravis@gmail.com>
2007-03-08 01:17:18 +0000

    attempt #2

--- system/application/controllers/office/news.php
+++ system/application/controllers/office/news.php
@@ -758,13 +758,13 @@ class News extends Controller
 		}
 		if (($data['user_level'] == 'editor') || ($this->requests_model->IsUserRequestedForArticle($article_id, $this->user_auth->entityId) == 'accepted')) {
 			if (is_numeric($revision)) {
-				$headline = htmlentities($this->input->xss_clean($headline),ENT_QUOTES);
-				$subheadline = htmlentities($this->input->xss_clean($subheadline),ENT_QUOTES);
-				$subtext = htmlentities($this->input->xss_clean($subtext),ENT_QUOTES);
-				$blurb = htmlentities($this->input->xss_clean($blurb),ENT_QUOTES);
-				$wiki = htmlentities($this->input->xss_clean($wiki),ENT_QUOTES);
-				$fact_heading = htmlentities($this->input->xss_clean($fact_heading),ENT_QUOTES);
-				$fact_text = htmlentities($this->input->xss_clean($fact_text),ENT_QUOTES);
+				$headline = htmlentities($this->input->xss_clean($headline));
+				$subheadline = htmlentities($this->input->xss_clean($subheadline));
+				$subtext = htmlentities($this->input->xss_clean($subtext));
+				$blurb = htmlentities($this->input->xss_clean($blurb));
+				$wiki = htmlentities($this->input->xss_clean($wiki));
+				$fact_heading = htmlentities($this->input->xss_clean($fact_heading));
+				$fact_text = htmlentities($this->input->xss_clean($fact_text));
 				$revision = $this->article_model->GetArticleRevisionToEdit($article_id, $this->user_auth->entityId, $revision);
 				$wiki_cache = '';
 //				if ($create_cache) {
