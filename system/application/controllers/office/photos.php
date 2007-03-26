c2af82575902aa1d42f5398d3e9ebbe3248334a0
Chris Travis <ctravis@gmail.com>
2007-03-26 16:13:36 +0000

    More photo requests mockup + changed link in office navigation bar

--- system/application/controllers/office/photos.php
+++ system/application/controllers/office/photos.php
@@ -18,9 +18,25 @@ class Photos extends Controller
 	}
 
 	/**
-	 *	@brief Load default page
+	 *	@brief Load photo request pool page
 	 */
 	function index()
+	{
+		/// Make sure users have necessary permissions to view this page
+		if (!CheckPermissions('office')) return;
+
+		$data['test'] = 'test';
+
+		/// Set up the main frame
+		$this->main_frame->SetContentSimple('office/photos/home', $data);
+		/// Set page title & load main frame with view
+		$this->main_frame->Load();
+	}
+
+	/**
+	 *	@brief Load photo request details
+	 */
+	function view()
 	{
 		/// Make sure users have necessary permissions to view this page
 		if (!CheckPermissions('office')) return;
---
+++ system/application/views/office/photos/home.php
@@ -0,0 +1,71 @@
+	<div class="RightToolbar">
+		<h4>Heading</h4>
+		<ul>
+			<li><a href="/office/news/request">Operation blah...</a></li>
+		</ul>
+	</div>
+	<div class="blue_box">
+		<h2>Unassigned</h2>
+		<div id="ArticleBox">
+			<table>
+			    <thead>
+			        <tr>
+				        <th>Request Title</th>
+				        <th>Photographer</th>
+				        <th>Status</th>
+	    		    </tr>
+			    </thead>
+	            <tbody>
+					<tr class="tr2">
+						<td><a href="/office/photos/view/">Monkey in a tree</a></td>
+						<td>Unassigned</td>
+						<td>Waiting</td>
+					</tr>
+			    </tbody>
+			</table>
+		</div>
+	</div>
+
+	<div class="blue_box">
+		<h2>Assigned</h2>
+		<div id="ArticleBox">
+			<table>
+			    <thead>
+			        <tr>
+				        <th>Request Title</th>
+				        <th>Photographer</th>
+				        <th>Status</th>
+	    		    </tr>
+			    </thead>
+	            <tbody>
+					<tr class="tr2">
+						<td><a href="/office/photos/view/">Monkey in a tree</a></td>
+						<td>Chris Travis</td>
+						<td>Accepted</td>
+					</tr>
+			    </tbody>
+			</table>
+		</div>
+	</div>
+
+	<div class="blue_box">
+		<h2>Flagged</h2>
+		<div id="ArticleBox">
+			<table>
+			    <thead>
+			        <tr>
+				        <th>Request Title</th>
+				        <th>Photographer</th>
+				        <th>Status</th>
+	    		    </tr>
+			    </thead>
+	            <tbody>
+					<tr class="tr2">
+						<td><a href="/office/photos/view/">Monkey in a tree</a></td>
+						<td>Chris Travis</td>
+						<td>Accepted</td>
+					</tr>
+			    </tbody>
+			</table>
+		</div>
+	</div>
