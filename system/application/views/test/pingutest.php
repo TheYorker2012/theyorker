262cba36ccb94c82d9d650ca5fa37b4b8faad307
Richard Ingle <ri504@york.ac.uk>
2007-03-27 00:45:15 +0000

    pr test views

--- system/application/views/test/pingutest.php
+++ system/application/views/test/pingutest.php
@@ -4,34 +4,165 @@
 		<a href="#">Wizard</a>
 	</div>
 </div>
-<div class="grey_box">
-	<h2>Unnassigned</h2>
+<div class="blue_box">
+	<h2>Summary (PR Officer)</h2>
 	<div id="ArticleBox">
 		<table>
 			<thead>
 				<tr>
-					<th>Name</th>
-					<th>PR Rep(s)</th>
-					<th>Options</th>
+					<th>PR Rep</th>
+					<th>Rating</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr class="tr1">
-					<td><a href="#">Tesco (Heslington)</a></td>
-					<td>Lance Taylor</td>
-					<td><input type="submit" value="Request"></td>
+					<td>Francesca Burton</td>
+					<td>87%</td>
 				</tr>
 				<tr class="tr2">
-					<td><a href="#">Bing Bong Bowling</a></td>
-					<td>Susan Parker<br />Rachael Rout</td>
-					<td><input type="submit" value="Withdraw"></td>
+					<td>Nancy Brehon</td>
+					<td>75%</td>
+				</tr>
+				<tr class="tr1">
+					<td>Jamie Hogan</td>
+					<td>69%</td>
+				</tr>
+				<tr class="tr2">
+					<td>Christine Travis</td>
+					<td>56%</td>
+				</tr>
+				<tr class="tr1">
+					<td>Joe Shelley</td>
+					<td>52%</td>
+				</tr>
+				<tr class="tr2">
+					<td><span class="orange">Nicola Evans</span></td>
+					<td><span class="orange">47%</span></td>
+				</tr>
+				<tr class="tr1">
+					<td><span class="orange">Daniella Ashby</span></td>
+					<td><span class="orange">30%</span></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="grey_box">
+	<h2>Summary (PR Rep)</h2>
+	<div id="ArticleBox">
+		<h5>Rep Info</h5>
+		Name: Nicola Evans<br />
+		Email: nse500<br />
+		<table>
+			<thead>
+				<tr>
+					<th>Organisation</th>
+					<th>Priority</th>
+					<th>Rating</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="tr1">
+					<td>Toffs</td>
+					<td>1</td>
+					<td>71%</td>
+				</tr>
+				<tr class="tr2">
+					<td><span class="orange">Evil Eye</span></td>
+					<td><span class="orange">1</span></td>
+					<td><span class="orange">35%</span></td>
+				</tr>
+				<tr class="tr1">
+					<td>Punch Bowl</td>
+					<td>2</td>
+					<td>67%</td>
+				</tr>
+				<tr class="tr2">
+					<td>Tesco (Clifton Moor)</td>
+					<td>3</td>
+					<td>95%</td>
+				</tr>
+				<tr class="tr1">
+					<td>Fantasy World</td>
+					<td>5</td>
+					<td>60%</td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </div>
 <div class="blue_box">
-	<h2>Unnassigned</h2>
+	<h2>Summary (Organisation)</h2>
+	<div id="ArticleBox">
+		<h5>Toffs</h5>
+		Rep: Nicola Evans<br />
+		Rep Rating: 56/66<br />
+		<table>
+			<thead>
+				<tr>
+					<th>Info</th>
+					<th>Rating<br />(Current/Expected)</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="tr1">
+					<td><span class="orange">Events</span></td>
+					<td><span class="orange">1/4</span></td>
+				</tr>
+				<tr class="tr2">
+					<td>Deals</td>
+					<td>3/2</td>
+				</tr>
+				<tr class="tr1">
+					<td><span class="orange">Directory</span></td>
+					<td><span class="orange">14/16</span></td>
+				</tr>
+				<tr class="tr2">
+					<td>Directory (Food)</td>
+					<td>6/8</td>
+				</tr>
+				<tr class="tr1">
+					<td>Directory (Drink)</td>
+					<td>8/8</td>
+				</tr>
+				<tr class="tr2">
+					<td><span class="orange">Photos</span></td>
+					<td><span class="orange">6/6</span></td>
+				</tr>
+				<tr class="tr1">
+					<td>Photos (Food)</td>
+					<td>4/3</td>
+				</tr>
+				<tr class="tr2">
+					<td><span class="orange">Photos (Drink)</span></td>
+					<td><span class="orange">2/3</span></td>
+				</tr>
+				<tr class="tr1">
+					<td>Tags</td>
+					<td>5/5</td>
+				</tr>
+				<tr class="tr2">
+					<td>Tags (Food)</td>
+					<td>2/2</td>
+				</tr>
+				<tr class="tr1">
+					<td>Tags (Drink)</td>
+					<td>3/3</td>
+				</tr>
+				<tr class="tr2">
+					<td>Business Cards</td>
+					<td>9/6</td>
+				</tr>
+				<tr class="tr1">
+					<td><span class="orange">Serving Times</span></td>
+					<td><span class="orange">0/1</span></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="grey_box">
+	<h2>Unnassigned (PR Officer)</h2>
 	<div id="ArticleBox">
 		<table>
 			<thead>
@@ -54,8 +185,34 @@
 				</tr>
 				<tr class="tr1">
 					<td colspan="3">
-						<input type="submit" value="Go">
+						<input type="submit" value="Assign">
 					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="blue_box">
+	<h2>Unnassigned (PR Rep)</h2>
+	<div id="ArticleBox">
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>PR Rep(s)</th>
+					<th>Options</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="tr1">
+					<td><a href="#">Tesco (Heslington)</a></td>
+					<td>Lance Taylor</td>
+					<td><input type="submit" value="Request"></td>
+				</tr>
+				<tr class="tr2">
+					<td><a href="#">Bing Bong Bowling</a></td>
+					<td>Susan Parker<br />Rachael Rout</td>
+					<td><input type="submit" value="Withdraw"></td>
 				</tr>
 			</tbody>
 		</table>
@@ -85,48 +242,7 @@
 	</div>
 </div>
 <div class="blue_box">
-	<h2>Suggestions</h2>
-	<div id="ArticleBox">
-		<table>
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Suggested By</th>
-					<th>Date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr class="tr1">
-					<td><a href="#">Toffs V2.0</a></td>
-					<td>Jamie Hogan</td>
-					<td>25th March 07</td>
-				</tr>
-				<tr class="tr2">
-					<td><a href="#">So Cheesy</a></td>
-					<td>Christine Travis</td>
-					<td>24th March 07</td>
-				</tr>
-				<tr class="tr1">
-					<td><a href="#">Blue Bicycle</a></td>
-					<td>Joe Shelley</td>
-					<td>24th March 07</td>
-				</tr>
-				<tr class="tr2">
-					<td><a href="#">Vue Cinema</a></td>
-					<td>Nicola Evans</td>
-					<td>23rd March 07</td>
-				</tr>
-				<tr class="tr1">
-					<td><a href="#">So Cheesy</a></td>
-					<td>Daniella Ashby</td>
-					<td>21st March 07</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-</div>
-<div class="grey_box">
-	<h2>Suggestions</h2>
+	<h2>Suggestions (PR Officer)</h2>
 	<div id="ArticleBox">
 		<table>
 			<thead>
@@ -185,7 +301,47 @@
 		</table>
 	</div>
 </div>
-
+<div class="grey_box">
+	<h2>Suggestions (PR Rep)</h2>
+	<div id="ArticleBox">
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Suggested By</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="tr1">
+					<td><a href="#">Toffs V2.0</a></td>
+					<td>Jamie Hogan</td>
+					<td>25th March 07</td>
+				</tr>
+				<tr class="tr2">
+					<td><a href="#">So Cheesy</a></td>
+					<td>Christine Travis</td>
+					<td>24th March 07</td>
+				</tr>
+				<tr class="tr1">
+					<td><a href="#">Blue Bicycle</a></td>
+					<td>Joe Shelley</td>
+					<td>24th March 07</td>
+				</tr>
+				<tr class="tr2">
+					<td><a href="#">Vue Cinema</a></td>
+					<td>Nicola Evans</td>
+					<td>23rd March 07</td>
+				</tr>
+				<tr class="tr1">
+					<td><a href="#">So Cheesy</a></td>
+					<td>Daniella Ashby</td>
+					<td>21st March 07</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
 
 <pre>
 <?php
