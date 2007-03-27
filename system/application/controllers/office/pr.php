1f1e7001f7edaa6a9f3169f204cdc1a8dbd487f5
Richard Ingle <ri504@york.ac.uk>
2007-03-27 00:02:45 +0000

    broken pr page

--- system/application/controllers/office/pr.php
+++ system/application/controllers/office/pr.php
@@ -15,22 +15,73 @@ class Pr extends controller
 	{
 		parent::controller();
 	}
-	
-	/// Index page (accessed through /office/pr)
-	function index()
+
+	/// Set up the navigation bar
+	private function _SetupNavbar()
+	{
+		$navbar = $this->main_frame->GetNavbar();
+		$navbar->AddItem('suggestions', 'Suggestions',
+				'/office/pr/suggestions');
+		$navbar->AddItem('unnassigned', 'Unnassigned',
+				'/office/pr/unnassigned');
+		$navbar->AddItem('summary', 'Summary',
+				'/office/pr/summary');
+	}
+
+	function suggestions()
 	{
 		// Not accessed through /office/pr/$organisation, not organisation
 		// specific so needs to be office permissions.
 		if (!CheckPermissions('office')) return;
-		$this->pages_model->SetPageCode('office_pr');
-		
-		// Organisations to list depends on whether editor
-		if (PermissionsSubset('editor', GetUserLevel())) {
-			// Show all organisations
-		} else {
-			// Show only pr rep organisations
-		}
-		
+
+		$this->_SetupNavbar();
+		$this->main_frame->SetPage('suggestions');
+		$this->pages_model->SetPageCode('suggestions');
+		$data = array();
+
+		// Set up the public frame
+		$the_view = $this->frames->view('office/pr/suggestions', $data);
+		$this->main_frame->SetContent($the_view);
+
+		// Load the public frame view (which will load the content view)
+		$this->main_frame->load();
+	}
+
+	function unnassigned()
+	{
+		// Not accessed through /office/pr/$organisation, not organisation
+		// specific so needs to be office permissions.
+		if (!CheckPermissions('office')) return;
+
+		$this->_SetupNavbar();
+		$this->main_frame->SetPage('unnassigned');
+		$this->pages_model->SetPageCode('unnassigned');
+		$data = array();
+
+		// Set up the public frame
+		$the_view = $this->frames->view('office/pr/unnassigned', $data);
+		$this->main_frame->SetContent($the_view);
+
+		// Load the public frame view (which will load the content view)
+		$this->main_frame->load();
+	}
+
+	function summary()
+	{
+		// Not accessed through /office/pr/$organisation, not organisation
+		// specific so needs to be office permissions.
+		if (!CheckPermissions('office')) return;
+
+		$this->_SetupNavbar();
+		$this->main_frame->SetPage('summary');
+		$this->pages_model->SetPageCode('summary');
+		$data = array();
+
+		// Set up the public frame
+		$the_view = $this->frames->view('office/pr/summary', $data);
+		$this->main_frame->SetContent($the_view);
+
+		// Load the public frame view (which will load the content view)
 		$this->main_frame->load();
 	}
 }
---
+++ system/application/views/office/pr/summary.php
@@ -0,0 +1,20 @@
+<div class="RightToolbar">
+	<h4>Make Suggestion</h4>
+	<div class="Entry">
+		<a href="#">Wizard</a>
+	</div>
+</div>
+
+
+<pre>
+<?php
+
+/*
+echo '<br />';
+echo print_r($content_types);
+echo '<br />';
+*/
+
+
+?>
+</pre>
