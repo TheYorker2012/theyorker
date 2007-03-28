3f7d114fd27ee09f00979fb26e6223c4025e9c31
Mark Goodall <mg512@york.ac.uk>
2007-03-28 12:13:07 +0000

    d'oh

--- system/application/controllers/office/gallery.php
+++ system/application/controllers/office/gallery.php
@@ -205,7 +205,7 @@ class Gallery extends Controller {
 		$reply = '';
 		if ($tagSearch->num_rows() > 0) {
 			foreach ($tagSearch->result() as $tag) {
-				$reply.='<a onClick="$(\'newtag\').value = \'.'$tag.'\'">'.$tag.'</a><br />\n';
+				$reply.='<a onClick="$(\'newtag\').value = \''.$tag.'\'">'.$tag.'</a><br />\n';
 			}
 			$objResponse->addAssign("txt_result", "style.display", 'block');
 			$objResponse->addAssign("txt_result", "innerHTML", $reply);
