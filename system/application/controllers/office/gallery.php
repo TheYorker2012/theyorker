d33cd17ec8f4713d8f2b816a7c20b429382dd8f9
Mark Goodall <mg512@york.ac.uk>
2007-03-29 15:13:16 +0000

    Changed list-style-type

--- system/application/controllers/office/gallery.php
+++ system/application/controllers/office/gallery.php
@@ -264,6 +264,7 @@ class Gallery extends Controller {
 		$this->main_frame->SetTitle('Admin\'s Photo Cropper');
 		$head = $this->xajax->getJavascript(null, '/javascript/xajax.js');
 		$head.= '<link rel="stylesheet" type="text/css" href="stylesheets/cropper.css" media="all" /><script src="javascript/prototype.js" type="text/javascript"></script><script src="javascript/scriptaculous.js?load=builder,effects,dragdrop" type="text/javascript"></script><script src="javascript/cropper.js" type="text/javascript"></script>';
+		$head.= '<style type="text/css" media="all">#ctags li {list-style-type:none;} #ntags li {list-style-type:none;}</style>';
 		$this->main_frame->SetExtraHead($head);
 		$this->main_frame->SetContentSimple('uploader/admin_upload_cropper', array('data' => $data, 'ThumbDetails' => &$query));
 		$this->main_frame->Load();
