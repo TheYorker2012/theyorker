742fd6966a2269c862355b097064e74832819c66
Andrew Oakley <ado@theyorker.co.uk>
2007-03-30 10:17:09 +0000

    Updated campaign petition and charity views

--- system/application/views/campaign/campaign_petition.php
+++ system/application/views/campaign/campaign_petition.php
@@ -1,115 +1,129 @@
 <div id="RightColumn">
-	<h2 class="first"><?php echo $sidebar_petition['title']; ?></h2>
+	<h2 class="first"><?php echo($sidebar_petition['title']); ?></h2>
 	<div class="Entry">
-		<h3><?php echo str_replace("%%count%%", $campaign['signatures'], $sidebar_petition['text']); ?></h3>
+		<?php echo(str_replace('%%count%%', $campaign['signatures'], $sidebar_petition['text'])); ?>
+
 	</div>
-	
-	<h2><?php echo $sidebar_sign['title']; ?></h2>
+
+	<h2><?php echo($sidebar_sign['title']); ?></h2>
 	<div class="Entry">
-		<?php
-		if ($user == TRUE) 
-		{
-			if ($user['vote_id'] == FALSE)
-			{
-				echo str_replace("%%name%%", $user['firstname'].' '.$user['surname'], $sidebar_sign['new_text']);
-		?>
-		<form action="/campaign/signpetition" method="post" class="form">
+<?php
+if ($user != FALSE) {
+	if ($user['vote_id'] == FALSE) {
+		echo('		');
+		echo(str_replace('%%name%%', $user['firstname'].' '.$user['surname'], $sidebar_sign['new_text']));
+		echo("\n");
+?>
+		<form name="sign" action="/campaign/signpetition" method="post">
 			<fieldset>
-				<input type="hidden" name="r_redirecturl" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
+				<input type="hidden" name="r_redirecturl" value="<?php echo($_SERVER['REQUEST_URI']); ?>" />
 				<input type="password" name="a_password" />
+			</fieldset>
+			<fieldset>
 				<input type="submit" value="Sign" class="button" name="r_sign" />
 			</fieldset>
 		</form>
-		<?php
-			}
-			else
-			{
-				echo str_replace("%%name%%", $user['firstname'].' '.$user['surname'], $sidebar_sign['withdraw_text']);
-		?>
-		<form action="/campaign/withdrawsignature" method="post" class="form">
+<?php
+	} else {
+		echo('		');
+		echo(str_replace('%%name%%', $user['firstname'].' '.$user['surname'], $sidebar_sign['withdraw_text']));
+		echo("\n");
+?>
+		<form name="sign" action="/campaign/withdrawsignature" method="post">
 			<fieldset>
-				<input type="hidden" name="r_redirecturl" value="<?php echo $_SERVER['REQUEST_URI']; ?>" />
+				<input type="hidden" name="r_redirecturl" value="<?php echo($_SERVER['REQUEST_URI']); ?>" />
+			</fieldset>
+			<fieldset>
 				<input type="submit" value="Withdraw" class="button" name="r_withdraw" />
 			</fieldset>
 		</form>
-		<?php
-			}
-		}
-		else
-			echo $sidebar_sign['not_logged_in'];
-		?>
+<?php
+	}
+} else {
+		echo('		'.$sidebar_sign['not_logged_in']."\n");
+}
+?>
 	</div>
-	
-	<h2><?php echo $sidebar_more['title']; ?></h2>
+
+	<h2><?php echo($sidebar_more['title']); ?></h2>
 	<div class="Entry">
-		<?php echo $sidebar_more['text']; ?>
+		<?php echo($sidebar_more['text']); ?>
 	</div>
-	
-	<h2><?php echo $sidebar_related['title']; ?></h2>
-	<div class="Entry">
-		<?php
-		foreach ($article['related_articles'] as $related_articles)
-		{
-			echo '<b><a href="http://www.google.com/">'.$related_articles['heading'].'</a></b><br />';
-		};
-		?>
-	</div>
-	
-	<h2><?php echo $sidebar_external['title']; ?></h2>
-	<div class="Entry">
-		<?php
- 	       foreach ($article['links'] as $links)
-		{
-			echo '<b><a href="'.$links['url'].'">'.$links['name'].'</a></b><br />';
-		};
-		?>
-	</div>
-	
-    	<h2><?php echo $sidebar_comments['title']; ?></h2>
+
+<?php
+if(count($article['related_articles']) > 0) {
+	echo('	<h2>'.$sidebar_related['title'].'/h2>'."\n");
+	echo('	<div class="Entry">'."\n");
+	echo('		<ul>'."\n");
+	foreach ($article['related_articles'] as $related_articles) {
+		echo('		');
+		echo('<li><a href="http://www.google.com/">'.$related_articles['heading'].'</a></li>'."\n");
+	}
+	echo('		</ul>'."\n");
+	echo('	</div>'."\n");
+}
+?>
+
+<?php
+if(count($article['links']) > 0) {
+	echo('	<h2>'.$sidebar_external['title'].'/h2>'."\n");
+	echo('	<div class="Entry">'."\n");
+	echo('		<ul>'."\n");
+	foreach ($article['links'] as $link) {
+		echo('		');
+		echo('<li><a href="'.$link['url'].'">'.$link['name'].'</a></li>'."\n");
+	}
+	echo('		</ul>'."\n");
+	echo('	</div>'."\n");
+}
+?>
+
+    	<h2><?php echo($sidebar_comments['title']); ?></h2>
 	<div class="Entry">
 		#TODO
 	</div>
 </div>
 
-
 <div id="MainColumn">
 	<div class="BlueBox">
-		<span style="font-size: x-large;  color: #BBBBBB; "><?php echo $our_campaign['title']; ?></span><br />
-		<?php echo $article['text']; ?>
+		<h2><?php echo($our_campaign['title']); ?></h2>
+		<?php echo($article['text']); ?>
 	</div>
 
-	<?php
-		foreach ($article['fact_boxes'] as $fact_box)
-		{
-			echo '<div class="BlueBox">';
-			echo '<h2>'.$fact_box['title'].'</h2>';
-			echo $fact_box['wikitext'];
-			echo '</div>';
-		}
-	?>
+<?php
+foreach ($article['fact_boxes'] as $fact_box) {
+	echo('	<div class="BlueBox">'."\n");
+	echo('		<h2>'.$fact_box['title'].'</h2>'."\n");
+	echo($fact_box['wikitext']);
+	echo('	</div>'."\n");
+}
 
-	<?php
-		if (isset($sections['progress_reports']['entries']))
-		{
-			echo '<div class="BlueBox">';
-			echo '<span style="font-size: x-large;  color: #BBBBBB; ">'.$progress_reports['title'].'</span><br />';
-			foreach ($sections['progress_reports']['entries'] as $pr_entry)
-			{
-				echo '<br>';
-				echo '<span style="font-size: large;  color: #BBBBBB; ">'.$pr_entry['date'].'</span><br />';
-				echo $pr_entry['text'].'<br />';
-			}
-			if ($sections['progress_reports']['totalcount'] > 3)
-				echo '<a href="/campaign/preports/">show more...</a>';
-			echo '</div>';
+if (isset($sections['progress_reports']['entries'])) {
+	echo('	<div class="BlueBox">'."\n");
+	echo('		<h2>'.$progress_reports['title'].'</h2>');
+	foreach ($sections['progress_reports']['entries'] as $pr_entry) {
+		echo('			');
+		echo('<h3>'.$pr_entry['date'].'</h3>'."\n");
+		echo('			');
+		echo($pr_entry['text']);
 	}
-	?>
+	echo('	</div>'."\n");
+}
+?>
 </div>
 
-<?php
-/*
-echo '<div class="BlueBox"><pre>';
-echo print_r($data);
-echo '</pre></div>';
-*/
+
+<?php /*
+	if (isset($progress_reports['entries']))
+	{
+		echo '<div class="grey_box">';
+		echo '<span style="font-size: x-large;  color: #BBBBBB; ">'.$progress_reports['title'].'</span><br />';
+		foreach ($progress_reports['entries'] as $pr_entry)
+		{
+			echo '<br>';
+			echo '<span style="font-size: large;  color: #BBBBBB; ">'.$pr_entry['date'].'</span><br />';
+			echo $pr_entry['text'].'<br />';
+		}
+		echo '</div>';
+} */
 ?>
--- system/application/views/charity/charity.php
+++ system/application/views/charity/charity.php
@@ -1,79 +1,77 @@
 <div id="RightColumn">
-	<h2 class="first"><?php echo $sections['sidebar_about']['title']; ?></h2>
+	<h2 class="first"><?php echo($sections['sidebar_about']['title']); ?></h2>
 	<div class="Entry">
-		<h3><?php echo $sections['sidebar_about']['subtitle']; ?></h3>
-		<?php echo $sections['charity']['target_text']; ?>
-	</div>
-	<h2><?php echo $sections['sidebar_help']['title']; ?></h2>
-	<div class="Entry">
-		<?php echo $sections['sidebar_help']['text']; ?>
-	</div>
-	
-	<h2><?php echo $sections['sidebar_related']['title']; ?></h2><!--Next 2 sections basically the same with different data and links-->
-	<div class="Entry">
-		<?php
-		if (isset($sections['article']['related_articles']))
-		{
-		        foreach ($sections['article']['related_articles'] as $related_articles)
-			{
-				echo '<b><a href="/news/uninews/'.$related_articles['id'].'">'.$related_articles['heading'].'</a></b><br />';
-			}
-		}
-		?>
+		<h3><?php echo($sections['sidebar_about']['subtitle']); ?></h3>
+		<?php echo($sections['charity']['target_text']); ?>
 	</div>
 
-	<h2><?php echo $sections['sidebar_external']['title']; ?></h2>
+	<h2><?php echo($sections['sidebar_help']['title']); ?></h2>
 	<div class="Entry">
-		<?php
-		if (isset($sections['article']['links']))
-		{
-		        foreach ($sections['article']['links'] as $links)
-			{
-				echo '<b><a href="'.$links['url'].'">'.$links['name'].'</a></b><br />';
-			}
-		}
-		?>
+		<?php echo($sections['sidebar_help']['text']); ?>
 	</div>
+
+<?php
+if (count($sections['article']['related_articles']) > 0) {
+?>
+	<h2><?php echo($sections['sidebar_related']['title']); ?></h2>
+	<div class="Entry">
+		<ul>
+<?php
+	foreach ($sections['article']['related_articles'] as $related_articles) {
+		echo('			');
+		echo('<li><a href="/news/uninews/'.$related_articles['id'].'">'.$related_articles['heading'].'</a></li>'."\n");
+	}
+?>
+		</ul>
+	</div>
+<?php
+}
+?>
+
+<?php
+if (count($sections['article']['links']) > 0) {
+?>
+	<h2><?php echo($sections['sidebar_external']['title']); ?></h2>
+	<div class="Entry">
+		<ul>
+<?php
+	foreach ($sections['article']['links'] as $link) {
+		echo('			');
+		echo('<li><a href="'.$link['url'].'">'.$link['name'].'</a></li>'."\n");
+	}
+?>
+		</ul>
+	</div>
+<?php
+}
+?>
 </div>
 
 <div id="MainColumn">
-
-	<img src="/images/prototype/homepage/rowing.jpg" alt="rowing" width="420" height="93" />
-
 	<div class="BlueBox">
-		<h2><?php echo $sections['article']['heading']; ?></h2>
-		<span class="black"><?php echo $sections['article']['text']; ?></span>
+		<h2><?php echo($sections['article']['heading']); ?></h2>
+		<?php echo($sections['article']['text']); ?>
 	</div>
 
 	<div class="BlueBox">
-		<h2><?php echo $sections['funding']['title']; ?></h2>
+		<h2><?php echo($sections['funding']['title']); ?></h2>
 		<div class="Entry">
-			<?php echo $sections['funding']['text']; ?>
+			<?php echo($sections['funding']['text']); ?>
 		</div>
 	</div>
 
-	<?php
-	if (isset($sections['progress_reports']['entries']))
-	{
-		echo '<div class="BlueBox">';
-		echo '<h2>'.$sections['progress_reports']['title'].'</h2>';
-		foreach ($sections['progress_reports']['entries'] as $pr_entry)
-		{
-			echo '<h5><span class="orange">'.$pr_entry['date'].'</span></h5>';
-			echo $pr_entry['text'].'<br /><br />';
-		}
-		if ($sections['progress_reports']['totalcount'] > 3)
-			echo '<a href="/charity/preports/">'.$sections['progress_reports']['showmore'].'</a>';
-		echo '</div>';
+<?php
+if (isset($sections['progress_reports']['entries'])) {
+	echo('	<div class="BlueBox">'."\n");
+	echo('		<h2>'.$sections['progress_reports']['title'].'</h2>'."\n");
+	foreach ($sections['progress_reports']['entries'] as $pr_entry) {
+		echo('		<h3>'.$pr_entry['date'].'</h3>'."\n");
+		echo($pr_entry['text']."\n");
 	}
-	?>
+	if ($sections['progress_reports']['totalcount'] > 3)
+		echo('<p><a href="/charity/preports/">There are older reports click here to view all progress reports.</a></p>'."\n");
+	echo '</div>';
+}
+?>
 
 </div>
-
-<?php
-/*
-echo '<div class="BlueBox"><pre>';
-echo print_r($data);
-echo '</pre></div>';
-*/
-?>
