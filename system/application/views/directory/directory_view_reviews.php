f3121ba3227d689ac25bcd85bc11b63554c52d54
Richard Ingle <ri504@york.ac.uk>
2007-03-30 12:03:21 +0000

    deals admin view, and directory reviews neating up started

--- system/application/views/directory/directory_view_reviews.php
+++ system/application/views/directory/directory_view_reviews.php
@@ -1,56 +1,65 @@
-<?php
-if (empty($organisation['reviews_untyped']) && empty($organisation['reviews_by_type'])) {
-?>
-	<div align='center'>
-		<b>This society has not been reviewed yet.</b>
-	</div>
-<?php
-}
+<div id="RightColumn">
+	<h2>sidebar</h2>
+</div>
+<div id="MainColumn">
+	<div class="BlueBox">
+		<?php
+		if (empty($organisation['reviews_untyped']) && empty($organisation['reviews_by_type']))
+			echo '<p><strong>This society has not been reviewed yet.</strong></p>';
 
-if (!empty($organisation['reviews_untyped'])) {
-	foreach ($organisation['reviews_untyped'] as $review) {
-		$author_links = array();
-		foreach ($review['authors'] as $author) {
-			$author_links[] = '<a href="mailto:'.$author['email'].'">'.$author['name'].'</a>';
-		}
-		$authors = implode(', ', $author_links);
-?>
-		<div class="WhyNotTry">
-			<div class="ArticleColumn" style="width: 100%;">
-				<div style='background-color: #DDDDDD;'>
-					<div id='Byline'>
-						<div class='RoundBoxBL'>
-							<div class='RoundBoxBR'>
-								<div class='RoundBoxTL'>
-									<div class='RoundBoxTR'>
-										<div id='BylineText'>
-											Written by<br />
-											<span class='name'><?php echo $authors; ?></span><br />
-											<?php echo $review['publish_date']; ?><br />
-											<span class='links'>
-												<?php echo anchor($review['link'], 'See more...'); ?>
-											</span>
+		if (!empty($organisation['reviews_untyped'])) {
+			foreach ($organisation['reviews_untyped'] as $review) {
+				$author_links = array();
+				foreach ($review['authors'] as $author) {
+					$author_links[] = '<a href="mailto:'.$author['email'].'">'.$author['name'].'</a>';
+				}
+				$authors = implode(', ', $author_links);
+		?>
+				<div class="WhyNotTry">
+					<div class="ArticleColumn" style="width: 100%;">
+						<div style='background-color: #DDDDDD;'>
+							<div id='Byline'>
+								<div class='RoundBoxBL'>
+									<div class='RoundBoxBR'>
+										<div class='RoundBoxTL'>
+											<div class='RoundBoxTR'>
+												<div id='BylineText'>
+													Written by<br />
+													<span class='name'><?php echo $authors; ?></span><br />
+													<?php echo $review['publish_date']; ?><br />
+													<span class='links'>
+														<?php echo anchor($review['link'], 'See more...'); ?>
+													</span>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					<?php echo $review['content']; ?><br />
 				</div>
-			</div>
-			<?php echo $review['content']; ?><br />
-		</div>
-<?php
+	<?php
+		}
 	}
-}
-/*
- * Input
- * $organisation [
- *	'reviews_untyped' [review1, review2....]
- *	'reviews_by_type' [
- *		type1name => [review1, review2...],
- *		type2name => [review1, review2...]
- *	]
- * ]
- */
-?>
+	?>
+	</div>
+	<?php
+	/*
+	 * Input
+	 * $organisation [
+	 *	'reviews_untyped' [review1, review2....]
+	 *	'reviews_by_type' [
+	 *		type1name => [review1, review2...],
+	 *		type2name => [review1, review2...]
+	 *	]
+	 * ] 
+	 */
+	 
+	 echo '<div class="BlueBox"><pre>';
+	 print_r($data);
+	 echo '</pre></div>';
+	 
+	?>
+</div>
--- system/application/views/test/pingutest.php
+++ system/application/views/test/pingutest.php
@@ -9,7 +9,7 @@
 		<h2>current deals</h2>
 		<h3>Triples for Singles</h3>
 		<div class="Date">25th March 2007 - 1st April 2007</div>
-		<p>An amazing offer on ginuea pigs, 3 for 1!!! You've left the office door open! Spies are everywhere: if you're going to stay out for a while, please shut it and lock it.</p>
+		<p>An amazing offer on guinea pigs, 3 for 1!!! You've left the office door open! Spies are everywhere: if you're going to stay out for a while, please shut it and lock it.</p>
 		<p><a href="#">[edit]</a> <a href="#">[expire]</a> <a href="#">[delete]</a></p>
 		<h3>Half Price Hay</h3>
 		<div class="Date">23th March 2007 - 30th March 2007</div>
@@ -28,7 +28,7 @@
 		<h2>expired deals</h2>
 		<h3>Triples for Singles</h3>
 		<div class="Date">12th March 2007 - 20th March 2007</div>
-		<p>An amazing offer on ginuea pigs, 3 for 1!!!</p>
+		<p>An amazing offer on guinea pigs, 3 for 1!!!</p>
 		<p><a href="#">[edit]</a> <a href="#">[delete]</a></p>
 		<h3>Half Price Hay</h3>
 		<div class="Date">5th March 2007 - 15th March 2007</div>
@@ -47,7 +47,7 @@
 				<label for="a_date_end">Date - End: </label>
 				<input type="text" name="a_date_end" id="a_date_end" style="width: 220px;" value="20/03/2007" />
 				<label for="a_description">Description: </label>
-				<textarea name="a_description" id="a_description" cols="25" rows="5">An amazing offer on ginuea pigs, 3 for 1!!!</textarea>
+				<textarea name="a_description" id="a_description" cols="25" rows="5">An amazing offer on guinea pigs, 3 for 1!!!</textarea>
 			</fieldset>
 			<fieldset>
 				<input type="submit" name="r_submit_save" value="Save" class="button" />
