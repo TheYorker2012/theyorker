edb6cdaa4e7d7b542791d6b8c3b3d2d309fd088f
Mark Goodall <mg512@york.ac.uk>
2007-03-31 11:44:24 +0000

    Typos fix

--- system/application/views/uploader/upload_single_photo.php
+++ system/application/views/uploader/upload_single_photo.php
@@ -2,7 +2,8 @@
 	<form action="<?=site_url($this->uri->uri_string())?>" method="post" enctype="multipart/form-data">
 		Generic helping text should go here, each file is limited to 2Mb.
 		<fieldset>
-			Image Filename: <input type="file" name="userfile1" size="30" />
+			Photo Title: <input type="text" name="title1" size="30" />
+			Photo Filename: <input type="file" name="userfile1" size="30" />
 			<input type="hidden" name="destination" id="destination" value="1" />
 			<input type="submit" value="upload" />
 		</fieldset>
