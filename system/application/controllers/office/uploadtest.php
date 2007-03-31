94f7ec0fa89fe64d06d87e81137f50ceddd986ce
Mark Goodall <mg512@york.ac.uk>
2007-03-31 11:38:47 +0000

    Changing test

--- system/application/controllers/office/uploadtest.php
+++ system/application/controllers/office/uploadtest.php
@@ -12,7 +12,7 @@ class Uploadtest extends Controller {
 		if (!CheckPermissions('office')) return;
 		$this->load->library('Image_upload');
 		$this->load->helper('url');
-		if ($this->image_upload->uploadForm(false, true)) { //if set to images (second to false), set type on next line
+		if ($this->image_upload->uploadForm(false, false)) { //if set to images (second to false), set type on next line
 			$this->image_upload->recieveUpload('office/uploadtest/done', null, true);
 		}
 	}
