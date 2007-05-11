cb32368be674630a5e216db33fb5310bab97fda4
Alex Fargus <alex@fargus.co.uk>
2007-05-11 20:05:57 +0000

    Homapge redesign

--- system/application/views/general/home.php
+++ system/application/views/general/home.php
@@ -1,42 +1,60 @@
 <?php
-function printsmallnewsbox($article){
-	echo('	<div class="NewsBox SmallNewsBox">'."\n");
-	echo('		<a class="NewsImg" href="/news/'.$article['article_type'].'/'.$article['id'].'">'."\n");
-	echo('			'.$article['photo_xhtml']."\n").'';
-	echo('		</a>'."\n");
-	echo('		<h3 class="Headline"><small><a href="/news/'.$article['article_type'].'/'.$article['id'].'">'."\n");
-	echo('			'.$article['heading']."\n");
-	echo('		</a></small></h3>'."\n");
-	echo('		<div style="clear: both" class="Date">'.$article['date'].'</div>'."\n");
-	echo('	</div>'."\n");
-}
+	function get_link_ref($article,$prefix){
+		return 'href="/'.$prefix.'/'.$article['article_type'].'/'.$article['id'].'"';
+	};
 
-function printmiddlebox($title,$article_array){
-		echo('  <h2>'.$title.'</h2>'."\n");
+	function print_box($primary_article,$secondary_article,$tertiary_article,$heading,$prefix){
+		echo('  <h2>'.$heading.'</h2>'."\n"); 
+		echo('  <div class="NewsBox">'."\n"); 
+		echo('          <a class="NewsImg"'.get_link_ref($primary_article,$prefix).'>'."\n");
+		echo('                  '.$primary_article['photo_xhtml']."\n").'';
+		echo('          </a>'."\n");
+		echo('          <h3 class="Headline"><a '.get_link_ref($primary_article,$prefix).'>'.$primary_article['heading'].'</a></h3>'."\n");
+		echo('          <div class="Date">'.$primary_article['date'].'</div>'."\n");
+		echo('		<p class="More">'.$primary_article['blurb'].'</p>'."\n");
+		echo('          <ul class="TitleList">'."\n");
+		echo('                  <li><a '.get_link_ref($secondary_article,$prefix).'>'.$secondary_article['heading'].'</a></li>'."\n");
+		echo('                  <li><a '.get_link_ref($tertiary_article,$prefix).'>'.$tertiary_article['heading'].'</a></li>'."\n");
+		echo('          </ul>'."\n");
+		echo('  </div>'."\n");
+	};
+
+	
+	function print_middle_box($title,$article_array){
+		echo('  <h4>'.$title.'</h4>'."\n");
 		echo('  <ul class="TitleList">'."\n");
 		foreach ($article_array as $article) {
-			echo('          <li><a class="TitleHeading" href="/news/'.$article['article_type'].'/'.$article['id'].'" >'."\n");
-			echo('			'.$article['heading']."\n");
-			echo('		</a></li>'."\n");
+			echo('          <li><a href="/news/'.$article['article_type'].'/'.$article['id'].'" >'."\n");
+			echo('                  '.$article['heading']."\n");
+			echo('          </a></li>'."\n");
 		}
 		echo('  </ul>'."\n");
-	}
+	};
 ?>
 
 <div id="RightColumn">
 	<h2 class="first">My Links</h2>
 	<div class="Entry">
-<?php if ($link->num_rows() > 0) { foreach($link->result() as $picture) {?>
-		<a href="<?=$picture->link_url?>"><?=imageLocTag($picture->link_image_id, 'link', false, $picture->link_name, null, $picture->image_file_extension, null, 'title = "'.$picture->link_name.'"')?></a>
-<?php } } else { ?>
-	<a href="http://theyorker.co.uk">You have no links :(</a>
-<?php }?>
+<?php 	if ($link->num_rows() > 0) 
+	{ 
+	foreach($link->result() as $picture){
+		echo('	<a href="'.$picture->link_url.'">'.imageLocTag($picture->link_image_id, 'link', false, $picture->link_name, null, $picture->image_file_extension, null, 'title = "'.$picture->link_name.'"').'</a>'."\n");
+		} 
+	} else { 
+		echo('	<a href="http://theyorker.co.uk">You have no links :(</a>'."\n");
+	}
+?>
 		<a class="RightColumnAction"  href="/account/links">Customise</a>
 	</div>
 
-	<h2>My Webmail</h2>
+	<h2> My Webmail </h2>
+	<a class="MailLogo" href="https://webmail.york.ac.uk">
+		<img src="/images/prototype/news/test/webmail_large.jpg" alt="Webmail Logo" />
+	</a>
 	<div class="Entry">
-		<table width="100%"><tr><td><a href="https://webmail.york.ac.uk/"><img src="/images/prototype/news/test/webmail_large.jpg"></a></td><td><a href="https://webmail.york.ac.uk/">0 Unread E-mails</a></td></tr></table>
+			<p class="MailText">
+				<a href="https://webmail.york.ac.uk/">E-mail</a>
+			</p>
 	</div>
 
 	<h2>Upcoming Events</h2>
@@ -62,51 +80,24 @@ function printmiddlebox($title,$article_array){
 
 <div id="MainColumn">
 	<div id="HomeBanner">
-		<?php
-			echo($banner);
-		?>
+		<?php echo($banner) ?>
 	</div>
 
 	<div class="BlueBox">
-		<h2><?php echo('latest news')?></h2>
-		<div class="NewsBox">
-			<?php
-				echo('	<a class="NewsImg" href="/news/'.$primary_article['article_type'].'/'.$primary_article['id'].'">'."\n");
-				echo('		'.$primary_article['photo_xhtml']."\n");
-				echo('	</a>'."\n");
-			?>
-			<h3 class="Headline"><?php echo('<a href="/news/'.$primary_article['article_type'].'/'.$primary_article['id'].'">'.$primary_article['heading'].'</a>') ?></h3>
-			<div class="Date" > <?php echo($primary_article['date']) ?></div>
-			<p class="More"> <?php echo($primary_article['blurb']) ?></p>
-		</div>
-		<?php printsmallnewsbox($secondary_article) ?>
-		<?php printsmallnewsbox($tertiary_article) ?>
-	</div>
-
-	<div class="BlueBox" style="float: left; width:48%; height:120px;">
-		<?php
-			printmiddlebox('features',$features);
-		?>
-	</div>
-	<div class="BlueBox" style="float: left; width:48%; height:120px; margin-left: 9px;">
-		<?php
-			printmiddlebox('arts',$arts);
-		?>
+		<?php print_box($primary_article,$secondary_article,$tertiary_article,'latest news','news') ?>
 	</div>
 
 	<div class="BlueBox">
-		<h2><?php echo('latest sport')?></h2>
-		<div class="NewsBox">
-			<?php
-				echo('	<a class="NewsImg" href="/news/'.$primary_sports['article_type'].'/'.$primary_sports['id'].'">'."\n");
-				echo('		'.$primary_sports['photo_xhtml']."\n");
-				echo('	</a>'."\n");
-			?>
-			<h3 class="Headline"><?php echo('<a href="/news/'.$primary_sports['article_type'].'/'.$primary_sports['id'].'">'.$primary_sports['heading'].'</a>') ?></h3>
-			<div class="Date" > <?php echo($primary_sports['date']) ?></div>
-			<p class="More"> <?php echo($primary_sports['blurb']) ?></p>
+		<h2><?php echo('and today...')?></h2>
+		<div class="LeftNewsBox NewsBox">
+			<?php print_middle_box('IN FEATURES',$features) ?>
 		</div>
-		<?php printsmallnewsbox($secondary_sports) ?>
-		<?php printsmallnewsbox($tertiary_sports) ?>
+		<div class="RightNewsBox NewsBox">
+			<?php print_middle_box('IN ARTS',$arts) ?>
+		</div>
+	</div>
+
+	<div class="BlueBox">
+		<?php print_box($primary_sports,$secondary_sports,$tertiary_sports,'latest sport','news/sport') ?>
 	</div>
 </div>
