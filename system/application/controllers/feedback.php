9d284981607172277c7e6c73305e18d577d828b3
James Hogan <james_hogan@theyorker.co.uk>
2007-05-12 13:10:47 +0000

    Fix feedback once and for all...ish

--- system/application/controllers/feedback.php
+++ system/application/controllers/feedback.php
@@ -19,13 +19,28 @@ class Feedback extends Controller {
 		
 		$this->load->model('feedback_model','feedback_model');
 
-		$this->feedback_model->AddNewFeedback($_POST['a_pagetitle'], 
-			$_POST['a_authorname'], $_POST['a_authoremail'], 
-			$_POST['a_rating'], $_POST['a_feedbacktext']);
-
-		$this->main_frame->AddMessage('success',
-			'You have sucessfully left feedback, thanks for your thoughts.');
-		redirect($_POST['r_redirecturl']);
+		$redirect_path = $this->input->post('r_redirecturl', '');
+		$page_title = $this->input->post('a_pagetitle');
+		$author_name = $this->input->post('a_authorname');
+		$author_email = $this->input->post('a_authoremail');
+		$rating = $this->input->post('a_rating');
+		$feedback_text = $this->input->post('a_feedbacktext');
+		
+		if (FALSE !== $feedback_text) {
+			$this->feedback_model->AddNewFeedback($page_title, 
+				$author_name, $author_email, 
+				$rating, $feedback_text);
+	
+			$this->main_frame->AddMessage('success',
+				'You have sucessfully left feedback, thanks for your thoughts.');
+		} else {
+			$this->main_frame->AddMessage('error',
+				'To leave feedback use the feedback form at the bottom of each page.');
+		}
+		
+		if ($redirect_path === '/')
+			$redirect_path = '';
+		redirect($redirect_path);
 	}
 }
 ?>
