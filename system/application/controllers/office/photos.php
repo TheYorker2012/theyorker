9d1ab3ae3a46368f4d66ffe831440d97a2afc255
Mark Goodall <mg512@york.ac.uk>
2007-05-12 10:58:46 +0000

    incomplete request suggestions updated:tweaks 2 and last

--- system/application/controllers/office/photos.php
+++ system/application/controllers/office/photos.php
@@ -52,6 +52,16 @@ class Photos extends Controller
 		$requestID = $this->uri->segment(4);
 		$viewer = $this->uri->segment(5);
 
+		if ($this->input->post('r_assign') == 'Suggest' and $this->input->post('imgid_number')) {
+			for ($i=0; $i<$this->input->post('imgid_number'); $i++) {
+				if ($this->input->post('imgid_'.$i.'_allow') == 'y'){
+					$this->requests_model->SuggestPhoto($requestID,
+						                                $this->input->post('imgid_'.$i.'_number'),
+						                                $this->input->post('imgid_'.$i.'_comment'));
+				}
+			}
+		}
+
 		$data['photoRequest'] = $this->requests_model->GetPhotoRequest($requestID);
 		$data['photos'] = $this->requests_model->GetAllPhotosForRequest($requestID);
 		$data['article'] = $this->article_model->GetArticleDetails($data['photoRequest']->photo_request_article_id);
@@ -67,15 +77,7 @@ class Photos extends Controller
 //			$this->messages->AddMessage('error', 'This photo request is marked as deleted.');
 //		}
 		
-		if ($this->input->post('r_assign') == 'Suggest' and $this->input->post('imgid_number')) {
-			for ($i=0; $i<$this->input->post('imgid_number'); $i++) {
-				if ($this->input->post('imgid_'.$i.'_allow') == 'y'){
-					$this->requests_model->SuggestPhoto($requestID,
-						                                $this->input->post('imgid_'.$i.'_number'),
-						                                $this->input->post('imgid_'.$i.'_comment'));
-				}
-			}
-		}
+
 
    		/// Get custom page content
 		$this->pages_model->SetPageCode('office_photos');
