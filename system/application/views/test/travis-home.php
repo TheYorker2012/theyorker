7c905fa3de66043ec5db2c5c15afd99d9c70c479
Chris Travis <ctravis@gmail.com>
2007-05-13 15:05:32 +0000

    Mock content type adder to make my job easier - will need something like this in admin in the future

--- system/application/views/test/travis-home.php
+++ system/application/views/test/travis-home.php
@@ -2,4 +2,5 @@
 <li><a href="/test/travis/calendar">Calendar</a></li>
 <li><a href="/test/travis/imap">IMAP - Check for new email count</a></li>
 <li><a href="/test/travis/campaign">Campaign Info Selecty Thing</a></li>
+<li><a href="/test/travis/contenttypes">ADMIN: Content Types Adder</a></li>
 </ul>
