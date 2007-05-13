b35eb4df54b9556fa88626d389d085259f1db40b
Nick Evans <nse500@york.ac.uk>
2007-05-13 14:07:10 +0000

    Fix reviewlist routes

--- system/application/config/routes.php
+++ system/application/config/routes.php
@@ -118,7 +118,7 @@ $route['office/pr/summary/org/([^/]*)'] = 'office/prindex/summary/org/$1'; //org
 $route['office/pr/summary'] = 'office/prindex/summary/def/0'; //default
 $route['office/pr/(.*)'] = 'office/prindex/$1';
 
-$route['office/reviewlist/('.$org_name_regex.')'] = 'office/reviewlist/showleagues/$1';
+$route['office/reviewlist/('.$org_name_regex.')'] = 'office/reviewlist/attentionlist/$1';
 $route['office/reviewlist/('.$org_name_regex.')/([a-z]+)'] = 'office/reviewlist/$2/$1';
 $route['office/reviewlist/('.$org_name_regex.')/([a-z]+)/([0-9]+)'] = 'office/reviewlist/$2/$1/$3';
 
