3ae3cfc52e6303e5c759e1f3db437b4f660c65cf
Chris Travis <ctravis@gmail.com>
2007-05-14 14:51:13 +0000

    Fixing creation of comments thread

--- system/application/models/photos_model.php
+++ system/application/models/photos_model.php
@@ -207,7 +207,8 @@ class Photos_model extends Model
 
 		/// Create new comment thread
 		$this->load->model('comments_model');
-		$this->comments_model->CreateThread(array('comment_thread_allow_anonymous_comments' => FALSE), 'photo_requests', array('photo_request_id' => $this->db->insert_id()), 'photo_request_comment_thread_id');
+		$CI = &get_instance();
+		$CI->comments_model->CreateThread(array('comment_thread_allow_anonymous_comments' => FALSE), 'photo_requests', array('photo_request_id' => $this->db->insert_id()), 'photo_request_comment_thread_id');
 	}
 
 	function SuggestPhoto($request_id,$photo_id,$comment,$user_id)
