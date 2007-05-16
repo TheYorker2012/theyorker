f6f348a47dda43019fbc5821f2ed00a39fbe1e12
James Hogan <james_hogan@theyorker.co.uk>
2007-05-16 21:40:18 +0000

    Ooops, forgot to include config file

--- system/application/config/yorker.php
+++ system/application/config/yorker.php
@@ -3,4 +3,7 @@
 $config['saved_login_duration'] = 60*60*24*30;
 $config['username_email_postfix'] = '@york.ac.uk';
 
+$config['company_organisation_id'] = 'theyorker';
+$config['company_entity_id'] = 378;
+
 ?>
