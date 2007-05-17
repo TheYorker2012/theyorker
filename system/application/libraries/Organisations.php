0ffc526da54a0f9c9d85ebeed08e12fe18d09391
Nick Evans <nse500@york.ac.uk>
2007-05-17 15:42:39 +0000

    Small fix

--- system/application/libraries/Organisations.php
+++ system/application/libraries/Organisations.php
@@ -65,7 +65,7 @@ class Organisations
 		foreach ($orgs as $org) {
 			$organisations[] = array(
 				'name' => $org['organisation_name'],
-				'shortname' => $org['organisation_directory_entry_name'],
+				'shortname' => $org['organisation_directory_entry_name'];
 				'link' => $urlpathpre.$org['organisation_directory_entry_name'].$urlpathpost;
 				'description' => $org['organisation_description'],
 				'shortdescription' => word_limiter(
