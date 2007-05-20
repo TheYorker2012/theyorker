d56e828a7a5c85b9d5aab20824ba714cb4ee8e41
Mark Goodall <mg512@york.ac.uk>
2007-05-20 18:45:16 +0000

    missing bits

--- system/application/controllers/image.php
+++ system/application/controllers/image.php
@@ -14,12 +14,18 @@ class Images extends Controller
 		$sql = 'SELECT image_mime, image_data FROM images WHERE image_id = ? LIMIT 1';
 		$result = $this->db->query($sql, array($id));
 		if ($result->num_rows() == 1){
+			header('Expires: '.date(DATE_RFC822, strtotime('+10 minutes')));
+			header('Cache-Control: no-store, cache');
+			header('Pragma: cache');
 			header('Content-Type: '.$result->first_row()->image_mime);
 			echo $result->first_row()->image_data;
 		} else {
 			$sql = 'SELECT image_type_error_mime, image_type_error_data FROM image_types WHERE image_type_codename = ? LIMIT 1';
 			$result = $this->db->query($sql, array($type));
 			if ($result->num_rows() == 1) {
+				header('Expires: '.date(DATE_RFC822, strtotime('+5 minutes')));
+				header('Cache-Control: no-store, cache');
+				header('Pragma: cache');
 				header('Content-Type: '.$result->first_row()->image_type_error_mime);
 				echo $result->first_row()->image_type_error_data;
 			}
