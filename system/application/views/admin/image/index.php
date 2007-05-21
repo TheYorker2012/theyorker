0040093f1a4c008af5268a58b8fe2ee3db2caade
Mark Goodall <mg512@york.ac.uk>
2007-05-21 16:29:28 +0000

    tweaks

--- system/application/views/admin/image/index.php
+++ system/application/views/admin/image/index.php
@@ -1,7 +1,7 @@
 <div class="blue_box" style="width:638px">
 <?php if($imageType->num_rows() > 0) foreach ($imageType->result() as $type) {?>
-	<div style="display:inline">
-		<h4><?=$type->image_type_name?></h4>
+	<div style="display:inline;width: 220px;float:left">
+		<h5><?=$type->image_type_name?></h5>
 		<?=$this->image->getImage(0, $type->image_type_codename)?><br />
 		<a href="imagecp/<?=$type->image_type_codename?>">Edit</a>, <a href="imagecp/view/<?=$type->image_type_codename?>">View All</a>, <a href="imagecp/delete/<?=$type->image_type_codename?>">Delete All</a>
 	</div>
