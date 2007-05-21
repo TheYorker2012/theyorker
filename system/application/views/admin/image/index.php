3472d9c09c50aa8f841b354bdc2c90372f2c7f97
Mark Goodall <mg512@york.ac.uk>
2007-05-21 15:51:12 +0000

    typo

--- system/application/views/admin/image/index.php
+++ system/application/views/admin/image/index.php
@@ -1,9 +1,9 @@
 <div class="blue_box" style="width:638px">
 <?php if($imageType->num_rows() > 0) foreach ($imageType->result() as $type) {?>
-	<div>
-		<h3><?=$type->image_type_name?></h3>
+	<div style="display:inline">
+		<h4><?=$type->image_type_name?></h4>
 		<?=$this->image->getImage(0, $type->image_type_codename)?><br />
-		<a href="imagecp/<?=$type->image_type_codename?>">Edit</a>, <a href="imagecp/view/<?=$type->image_type_codename?>">View All</a>
+		<a href="imagecp/<?=$type->image_type_codename?>">Edit</a>, <a href="imagescp/view/<?=$type->image_type_codename?>">View All</a>, <a href="imagecp/delete/<?=$type->image_type_codename?>">Delete All</a>
 	</div>
 <?php } else { ?>
 			<p>There are no image types :(</p>
