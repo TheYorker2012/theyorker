a3f8e11d10047809c95fc4b090854c658ba0e538
Nick Evans <nse500@york.ac.uk>
2007-05-22 22:52:46 +0000

    VIP css fix

--- system/application/views/frames/vip_frame.php
+++ system/application/views/frames/vip_frame.php
@@ -68,11 +68,10 @@ echo('<?xml version="1.0" encoding="UTF-8"?>');
 			<h1 id="TopBannerName">
 				<a href="/"><img src="/images/prototype/header/header_Layer-1.gif" width="275" height="108" alt="The Yorker"/></a>
 			</h1>
-			<div id="TopBannerPictures">
-				<div style="float: right; line-height: 19px; width: 500px; overflow: hidden; color: #FFFFFF; text-align: center; position: relative; top: 37px; height: 40px;">
+			<div id="TopBannerPictures" style="line-height: 19px; width: 500px; overflow: hidden; color: #FFFFFF; text-align: center; position: relative; top: 37px; height: 40px;">
 					<span style="font-size: 16px; font-weight:bold; "><?php echo $vipinfo['name']; ?></span><br />
 					<span style="font-size: 16px; font-weight:bold"><?php echo $vipinfo['organisation']; ?></span>
-				</div>
+
 			</div>
 		</div>
 	</div>
