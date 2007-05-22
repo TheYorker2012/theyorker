dc118879687b74a4adead4abe6a9f8d5bbe008ec
Mark Goodall <mg512@york.ac.uk>
2007-05-22 20:28:48 +0000

    image cp: bugfix

--- system/application/views/admin/image/view.php
+++ system/application/views/admin/image/view.php
@@ -2,7 +2,7 @@
 <?php if($images->num_rows() > 0) foreach ($images->result() as $image) {?>
 	<div style="display:inline;padding: 10px;float:left">
 		<h5><?=$image->image_title?></h5>
-		<?=$this->image->getImage($image->image_id, $type->image_image_type_id)?><br />
+		<?=$this->image->getImage($image->image_id, $image->image_image_type_id)?><br />
 		<a href="<?=site_url('admin/imagecp/'.$codename.'/'.$image->image_id.'/delete')?>">Delete</a>
 	</div>
 <?php } else { ?>
