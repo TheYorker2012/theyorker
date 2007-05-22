5dd9d12ed0cc2f37d2d8086e33b07de54e75319c
Nick Evans <nse500@york.ac.uk>
2007-05-22 22:54:59 +0000

    VIP css fix

--- system/application/views/frames/vip_frame.php
+++ system/application/views/frames/vip_frame.php
@@ -68,10 +68,10 @@ echo('<?xml version="1.0" encoding="UTF-8"?>');
 			<h1 id="TopBannerName">
 				<a href="/"><img src="/images/prototype/header/header_Layer-1.gif" width="275" height="108" alt="The Yorker"/></a>
 			</h1>
-			<div id="TopBannerPictures" style="line-height: 19px; width: 500px; overflow: hidden; color: #FFFFFF; text-align: center; position: relative; top: 37px; height: 40px;">
-					<span style="font-size: 16px; font-weight:bold; "><?php echo $vipinfo['name']; ?></span><br />
-					<span style="font-size: 16px; font-weight:bold"><?php echo $vipinfo['organisation']; ?></span>
-
+			<div id="TopBannerPictures">
+				<img src="/images/prototype/header/header_Layer-4.gif" alt="News" />
+				<img src="/images/prototype/header/header_Layer-3.gif" alt="Calendar" />
+				<img src="/images/prototype/header/header_Layer-2.gif" alt="Reviews" />
 			</div>
 		</div>
 	</div>
