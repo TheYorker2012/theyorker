527a9b2338c901be1ecc281bc8d72d8365632678
Mark Goodall <mg512@york.ac.uk>
2007-05-22 21:43:57 +0000

    image uploader for images bugfix

--- system/application/libraries/Image_upload.php
+++ system/application/libraries/Image_upload.php
@@ -174,7 +174,7 @@ class Image_upload {
 			$result = $result->first_row();
 			$newImage = imagecreatetruecolor($result->x, $result->y);
 			imagecopyresampled($image, $newImage, 0, 0, 0, 0, $result->x, $result->y, imagesx($image), imagesy($image));
-			$id = $this->ci->image->add($result->codename, $image, array('title' => $selectedThumb[7], 'mime' => $mime));
+			$id = $this->ci->image->add('image', $image, array('title' => $selectedThumb[7], 'mime' => $mime, 'type_id' => $selectedThumb[3]));
 			if ($id != false) {
 				foreach ($_SESSION['img'] as &$newImages) {
 					if ($selectedThumb[4] == $newImages['list'] and $selectedThumb[3] == $newImages['type']) {
