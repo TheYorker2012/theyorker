1ec0c433a4c11b88388448564345a8c2d206806c
Nick Evans <nse500@york.ac.uk>
2007-05-24 20:25:40 +0000

    Reporter link fix

--- system/application/views/general/byline.php
+++ system/application/views/general/byline.php
@@ -12,7 +12,6 @@ echo('		<div class="Date">'.$article_date.'</div>'."\n");
 
 foreach ($reporters as $id => $reporter) {
 	echo('		');
-	echo('<a href="/archive/reporter/'.$id.'">Read more articles by '.$reporter['name'].'</a>');
 }
 ?>
 	</div>
