df67ca701a3177cfd6cc78d788cf3f1a75550e41
Nick Evans <nse500@york.ac.uk>
2007-05-27 14:28:56 +0000

    Photos header fix

--- system/application/controllers/photos.php
+++ system/application/controllers/photos.php
@@ -15,10 +15,10 @@ class Photos extends Controller
 			$sql = 'SELECT photo_mime, photo_data FROM photos WHERE photo_id = ? LIMIT 1';
 			$result = $this->db->query($sql, array($id));
 			if ($result->num_rows() == 1) {
+				header('Content-type: '.$result->first_row()->photo_mime);
+				header('Cache-control: store, cache');
 				header('Expires: '.date(DATE_RFC822, strtotime('+1 month')));
-				header('Cache-Control: store, cache');
 				header('Pragma: cache');
-				header('Content-Type: '.$result->first_row()->photo_mime);
 				echo $result->first_row()->photo_data;
 			} else {
 				header('HTTP/1.0 404 Not Found');
@@ -33,19 +33,19 @@ class Photos extends Controller
 			        LIMIT 1';
 			$result = $this->db->query($sql, array($id, $type));
 			if ($result->num_rows() == 1) {
+				header('Content-type: '.$result->first_row()->photo_mime);
+				header('Cache-control: no-store, cache');
 				header('Expires: '.date(DATE_RFC822, strtotime('+10 minutes')));
-				header('Cache-Control: no-store, cache');
 				header('Pragma: cache');
-				header('Content-Type: '.$result->first_row()->photo_mime);
 				echo $result->first_row()->photo_thumbs_data;
 			} else {
 				$sql = 'SELECT image_type_error_mime, image_type_error_data FROM image_types WHERE image_type_codename = ? LIMIT 1';
 				$result = $this->db->query($sql, array($type));
 				if ($result->num_rows() == 1) {
+					header('Content-type: '.$result->first_row()->image_type_error_mime);
+					header('Cache-control: no-store, cache');
 					header('Expires: '.date(DATE_RFC822, strtotime('-10 minutes')));
-					header('Cache-Control: no-store, cache');
 					header('Pragma: cache');
-					header('Content-Type: '.$result->first_row()->image_type_error_mime);
 					echo $result->first_row()->image_type_error_data;
 				} else {
 					header('HTTP/1.0 404 Not Found');
