33e77268dbe0d97cd7f6fba4e9774e9c271388e1
Nick Evans <nse500@york.ac.uk>
2007-05-28 18:35:52 +0000

    Fix images

--- system/application/views/uploader/upload_multiple_images.php
+++ system/application/views/uploader/upload_multiple_images.php
@@ -1,19 +1,33 @@
-<div id="source" style="display:none">
-	Image Title: <input type="text" name="title" size="30" />
-	Image Filename: <input type="file" name="userfile" size="30" />
+<div id="source" style="display:none;">
+	<label for="title" style="margin-top: 20px;">Title / ALT Text: </label>
+	<input type="text" name="title" size="32" style="margin-top: 20px;" />
+	<label for="userfile">File Location: </label>
+	<input type="file" name="userfile" size="20"/>
 </div>
-<div class="blue_box">
-<form action="<?=site_url($this->uri->uri_string())?>" method="post" enctype="multipart/form-data">
-<fieldset>
-Generic helping text should go here, each file is limited to 2Mb.
-<div>
-	Image Title: <input type="text" name="title1" size="30" />
-	Image Filename: <input type="file" name="userfile1" size="30" />
+<div id="RightColumn">
+	<h2 class="first">What's this?</h2>
+	<p> The <b>image title</b> will be used as an alternative text for the photo that is uploaded, to make the site more accessible to people who have difficulty recognising images. Please try to make this title <b>concisely</b> describe what the image depicts.</p>
 </div>
-<input type="hidden" name="destination" id="destination" value="1" />
 
-<input type="button" onClick="AddClones()" value="Another"/>
-<input type="submit" value="upload" />
-</fieldset>
-</form>
+<div id="MainColumn">
+	<form action="<?=site_url($this->uri->uri_string())?>" method="post" onsubmit="return ValidateClones();" enctype="multipart/form-data">
+		<div class="BlueBox">
+			<h2>image upload</h2>
+			<p> Please choose the photos you wish up upload here. Each file is limited to 2Mb in size, and must be larger than 220x165 in resolution.</p>
+			<div>
+				<label for="title1">Title / ALT Text: </label>
+				<input type="text" name="title1" size="32" />
+				<label for="userfile1">File Location: </label>
+				<input type="file" name="userfile1" size="20" />
+			</div>
+			<input type="hidden" name="destination" id="destination" value="1" />
+			<div style="clear: both"></div>
+			<input type="button" class="button" onclick="AddClones()" value="Add Another File"/>
+		</div>
+		<div class="BlueBox">
+			<h2>next step...</h2>
+			<p>When you have finished selecting files, click the button below to proceed.</p>
+			<input type="submit" class="button" value="Upload" />
+		</div>
+	</form>
 </div>
