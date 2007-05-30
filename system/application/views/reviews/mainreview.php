3c27b20e722606d4261297d79a3b686eff4d1e52
Nick Evans <nse500@york.ac.uk>
2007-05-30 10:58:04 +0000

    View fix

--- system/application/views/reviews/mainreview.php
+++ system/application/views/reviews/mainreview.php
@@ -13,7 +13,7 @@ $address = str_replace("\r", '', $address);
 $address = str_replace("\n", ', ', $address);
 $address = htmlspecialchars($address);
 ?>
-			<?php if(strlen(trim($address)) > 0) { ?><b>Address:</b>  <?php echo($address); ?><br />
+			<?php if(strlen(trim($address)) > 0) { ?><b>Address:</b>  <?php echo($address); ?><br /><?php } ?>
 			<?php if(strlen(trim($website)) > 0) { ?><b>Website:</b>  <a href="<?php echo(htmlspecialchars($website)); ?>">Click Here</a><br /><?php } ?>
 			<?php if(strlen(trim($email)) > 0) { ?><b>Email:</b>  <a href="mailto:<?php echo(htmlspecialchars($email)); ?>"><?php echo(htmlspecialchars($email)); ?></a><br /><?php } ?>
 			<?php if(strlen(trim($telephone)) > 0) { ?><b>Telephone:</b>  <?php echo(htmlspecialchars($telephone)); ?><br /><?php } ?>
