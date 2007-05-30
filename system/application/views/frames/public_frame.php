f0729909f9985c544e47cd9bc1a45c0f1b6ad24c
Nick Evans <nse500@york.ac.uk>
2007-05-30 13:44:37 +0000

    alt text

--- system/application/views/frames/public_frame.php
+++ system/application/views/frames/public_frame.php
@@ -136,7 +136,7 @@ echo('<?xml version="1.0" encoding="UTF-8"?>');
 				?>
 			</div>
 
-			<img src="/images/adverts/bigd_webad.jpg" width="120" height="600" style="margin-top: 80px;"/>
+			<img src="/images/adverts/bigd_webad.jpg" width="120" height="600" style="margin-top: 80px;" alt="" />
 		</div>
 
 		<div id="MainBodyPane">
