8399056a5e2a925c68c15a0fcab9ad520231af4e
Nick Evans <nse500@york.ac.uk>
2007-05-31 20:25:46 +0000

    Couldn't help it!

--- system/application/views/wizard/organisation.php
+++ system/application/views/wizard/organisation.php
@@ -306,7 +306,7 @@ function addstrike($text)
 			 ?>
 
 			 <p>As you are connected with this organisation, we will automatically request VIP status for you. We will contact you to confirm your position in the organisation in the near future.</p>
-			 <? php } ?>
+			 <?php } ?>
 			 <p>Should we accept your organisation, you will be automatically subscribed to it.</p>
 		</fieldset>
 		<fieldset>
