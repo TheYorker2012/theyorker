399c973580068356653b7c97043d90ea7eef8bba
Nick Evans <nse500@york.ac.uk>
2007-05-31 11:46:35 +0000

    Link nomination

--- system/application/controllers/office/links.php
+++ system/application/controllers/office/links.php
@@ -131,10 +131,10 @@ class Links extends Controller
 
 				redirect('/office/links', 'location');
 			}
-		} elseif(!$this->input->post('lname')) {
+		} elseif($this->input->post('lurl') && !$this->input->post('lname')) {
 			$this->messages->AddMessage('error', 'Please enter a name for your link.');
 			redirect('/office/links/customlink');
-		} elseif (!$this->input->post('upload')) {
+		} elseif ($this->input->post('lurl') && !$this->input->post('upload')) {
 			$this->messages->AddMessage('error', 'Please choose an image to upload with the link.');
 			redirect('/office/links/customlink');
 		}
