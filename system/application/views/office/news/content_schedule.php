4a01a64258756ead23dd5510d5bbbdf5c33c79f3
Nick Evans <nse500@york.ac.uk>
2007-06-05 14:32:36 +0000

    Day spaces

--- system/application/views/office/news/content_schedule.php
+++ system/application/views/office/news/content_schedule.php
@@ -8,22 +8,28 @@
 	<thead>
 	<th>Deadline</th>
 	<th>Box</th>
+	<th>Story</th>
 	<th>Editor</th>
+	<th>Writer(s)</th>
 	<th>Status</th>
-	<th>Request Title</th>
-	<th>Authors</th>
 	</thead>
 	</tr>
 
 	<?php foreach($articlelist as $article) { ?>
-
+	<?php
+		$new_publish_date = date('D jS M',strtotime($article['publish_date']));
+		if (isset($publish_date) && $publish_date != $new_publish_date) {
+			echo '<tr><td colspan="6">&nbsp;</td></tr>';
+			$publish_date = $new_publish_date;
+		}
+	?>
 	<tr>
-	<td style="<?php if($article['overdue']) echo 'color: red;';?>"><?php echo date('D jS M',strtotime($article['publish_date'])); ?></td>
+	<td style="<?php if($article['overdue']) echo 'color: red;';?>"><?php echo $publish_date; ?></td>
 	<td><?php echo $article['content_type_name']; ?></td>
-	<td><?php echo $article['editor']; ?></td>
-	<td><?php echo ($article['is_accepted'] ? '<img src="/images/prototype/news/accepted.gif" title="Accepted" alt="Accepted" />' : ($article['is_requested'] ? '<img src="/images/prototype/news/requested.gif" title="Requested" alt="Requested" />' : '<img src="/images/prototype/news/unassigned.png" title="Unassigned" alt="Unassigned" />') ); ?></td>
 	<td><a href="/office/news/<?php echo $article['article_id']; ?>/" title="Edit this article"><?php echo (strlen($article['headline']) == 0 ? 'No title' : $article['headline'] ); ?></a></td>
+	<td><?php echo $article['editor']; ?></td>
 	<td><?php echo $article['authors']; ?></td>
+	<td><?php echo ($article['is_accepted'] ? '<img src="/images/prototype/news/accepted.gif" title="Accepted" alt="Accepted" />' : ($article['is_requested'] ? '<img src="/images/prototype/news/requested.gif" title="Requested" alt="Requested" />' : '<img src="/images/prototype/news/unassigned.png" title="Unassigned" alt="Unassigned" />') ); ?></td>
 	</tr>
 
 	<? } ?>
