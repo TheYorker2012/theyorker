d21b938e648598de2238b6e2d84b6ec5c9b4d7b4
Nick Evans <nse500@york.ac.uk>
2007-06-07 22:20:22 +0000

    Make news articles redirect if the article is not found, so that Google does not index duplicate content!!!!!

--- system/application/controllers/news.php
+++ system/application/controllers/news.php
@@ -91,16 +91,20 @@ class News extends Controller {
 		/// Get requested article id if submitted
 		$url_article_id = $this->uri->segment(3);
 		// Check if an article id was requested, if so check that the type of article it corresponds
-		// to is correct for the current news view else ignore it
-		if (($url_article_id !== FALSE) && (is_numeric($url_article_id)) && ($this->News_model->IdIsOfType($url_article_id,$article_type))) {
-			/// Check if requested article is already one of the IDs returned
-			$found_article = array_search($url_article_id, $latest_article_ids);
-			if ($found_article !== FALSE) {
-				/// If it is, remove it from the list
-				unset($latest_article_ids[$found_article]);
+		// to is correct for the current news view redirect to home page of article type (so that search engines do not index duplicate pages).
+		if ($url_article_id !== FALSE) {
+			if (is_numeric($url_article_id) && $this->News_model->IdIsOfType($url_article_id,$article_type)) {
+				/// Check if requested article is already one of the IDs returned
+				$found_article = array_search($url_article_id, $latest_article_ids);
+				if ($found_article !== FALSE) {
+					/// If it is, remove it from the list
+					unset($latest_article_ids[$found_article]);
+				}
+				/// Put request article id onto front of array so that it becomes the main article
+				$latest_article_ids = array_merge(array($url_article_id),$latest_article_ids);
+			} else {
+				redirect('/news/'.$article_type);
 			}
-			/// Put request article id onto front of array so that it becomes the main article
-			$latest_article_ids = array_merge(array($url_article_id),$latest_article_ids);
 		}
 
 		/// Get all of the latest article
