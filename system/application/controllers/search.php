ce67af2f9b24ec67c5c26f4438abed1d0e7e87dc
Mark Goodall <mg512@york.ac.uk>
2007-06-22 14:38:14 +0000

    new permissions check

--- system/application/controllers/search.php
+++ system/application/controllers/search.php
@@ -5,18 +5,24 @@ class Search extends Controller {
 	function __construct()
 	{
 		parent::Controller();
-		define("ORDER_RELEVANCE", 	1);
-		define("ORDER_EARLY", 		2);
-		define("ORDER_LATE", 		3);
+		define("ORDER_RELEVANCE", 		1);
+		define("ORDER_EARLY", 			2);
+		define("ORDER_LATE", 			3);
 
-		define("FILTER_ALL", 		0b1111111111111111);
-		define("FILTER_NEWS", 		0b0000000000000001);
-		define("FILTER_FEATURES", 	0b0000000000000010);
-		define("FILTER_LIFESTYLE", 	0b0000000000000100);
-		define("FILTER_FOOD",		0b0000000000001000);
-		define("FILTER_DRINK", 		0b0000000000010000);
-		define("FILTER_CULTURE",	0b0000000000100000);
-		define("FILTER_YORK",		0b0000000001000000);
+		define("FILTER_ALL", 			16777216);
+		define("FILTER_ALL_ARTICLES", 	511);
+		define("FILTER_NEWS", 			1);
+		define("FILTER_FEATURES", 		2);
+		define("FILTER_LIFESTYLE", 		4);
+		define("FILTER_FOOD",			8);
+		define("FILTER_DRINK", 			16);
+		define("FILTER_unused",			32);	//unused
+		define("FILTER_ARTS",			64);
+		define("FILTER_SPORTS",			128);
+		define("FILTER_BLOGS",			256);
+		define("FILTER_YORK",			512);
+		define("FILTER_DIR",			1024);
+		define("FILTER_EVENTS",			2048);
 		$this->load->model('search');
 	}
 	
@@ -35,59 +41,59 @@ class Search extends Controller {
 		$this->main_frame->Load();
 	}
 	
-	function reply($fd) {
-		$objResponse = new xajaxResponse();
-		if ($this->input->post('search')) {
-			//TODO mysql queries though model
-			//TODO ajax submit
-		}
-	}
-	
-//	function get() {
-//		$this->load->helper('url');
-//		$this->load->library('session');
-//		$search_keywords = $this->input->post('search');
-//		$search_category = $this->input->post('category');
-//		$url = url_title($search_keywords).'/'.url_title($search_category);
-//		$data = array('search_keywords' => $search_keywords,
-//		              'search_category' => $search_category,
-//		              'url' => $url);
-//		$this->session->set_userdata($data);
-//		redirect('/search/results/'. $url, 'location');
+//	function reply($fd) {
+//		$objResponse = new xajaxResponse();
+//		if ($this->input->post('search')) {
+//			//TODO mysql queries though model
+//			//TODO ajax submit
+//		}
 //	}
-	
-	function results() {
-		if (!CheckPermissions('public')) return;
-		
-		$this->load->library('session');
-		//TODO do mysql queries through model
-		//TODO display results
-		
-		// Set up the public frame
-		$this->main_frame->SetTitle('Search Results');
-		
-		$results[] = array('link' => 'TODO',
-		                   'title' => 'TODO',
-		                   'blurb' => 'TODO');
-		
-		
-		$data = array('search_form' => 'TODO Searchform again',
-		              'search_numbering' => '101',
-		              'search_all' => 		'1111111111111111',
-		              'search_news' => 		'0000000000000001',
-		              'search_features' => 	'0000000000000010',
-		              'search_lifestyle' =>	'0000000000000100',
-		              'search_food' =>	 	'0000000000001000',
-		              'search_drink' =>		'0000000000010000',
-		              'search_culture' =>	'0000000000100000',
-		              'search_york' => 		'0000000001000000',
-		              'search_results' => $results);
-		
-		$this->main_frame->SetContentSimple('search/results', $data);
-		
-		// Load the public frame view (which will load the content view)
-		$this->main_frame->Load();
-	}
+//	
+////	function get() {
+////		$this->load->helper('url');
+////		$this->load->library('session');
+////		$search_keywords = $this->input->post('search');
+////		$search_category = $this->input->post('category');
+////		$url = url_title($search_keywords).'/'.url_title($search_category);
+////		$data = array('search_keywords' => $search_keywords,
+////		              'search_category' => $search_category,
+////		              'url' => $url);
+////		$this->session->set_userdata($data);
+////		redirect('/search/results/'. $url, 'location');
+////	}
+//	
+//	function results() {
+//		if (!CheckPermissions('public')) return;
+//		
+//		$this->load->library('session');
+//		//TODO do mysql queries through model
+//		//TODO display results
+//		
+//		// Set up the public frame
+//		$this->main_frame->SetTitle('Search Results');
+//		
+//		$results[] = array('link' => 'TODO',
+//		                   'title' => 'TODO',
+//		                   'blurb' => 'TODO');
+//		
+//		
+//		$data = array('search_form' => 'TODO Searchform again',
+//		              'search_numbering' => '101',
+//		              'search_all' => 		'1111111111111111',
+//		              'search_news' => 		'0000000000000001',
+//		              'search_features' => 	'0000000000000010',
+//		              'search_lifestyle' =>	'0000000000000100',
+//		              'search_food' =>	 	'0000000000001000',
+//		              'search_drink' =>		'0000000000010000',
+//		              'search_culture' =>	'0000000000100000',
+//		              'search_york' => 		'0000000001000000',
+//		              'search_results' => $results);
+//		
+//		$this->main_frame->SetContentSimple('search/results', $data);
+//		
+//		// Load the public frame view (which will load the content view)
+//		$this->main_frame->Load();
+//	}
 
 }
 ?>
--- system/application/models/search.php
+++ system/application/models/search.php
@@ -10,21 +10,26 @@
 // 1. use a new bit in a define and name it well
 // 2. add a new if statement to full and ajax functions, with desired query
 
-define("ORDER_RELEVANCE", 	1);
-define("ORDER_EARLY", 		2);
-define("ORDER_LATE", 		3);
+define("ORDER_RELEVANCE", 		1);
+define("ORDER_EARLY", 			2);
+define("ORDER_LATE", 			3);
 
-define("FILTER_ALL", 		0b1111111111111111);
-define("FILTER_NEWS", 		0b0000000000000001);
-define("FILTER_FEATURES", 	0b0000000000000010);
-define("FILTER_LIFESTYLE", 	0b0000000000000100);
-define("FILTER_FOOD",		0b0000000000001000);
-define("FILTER_DRINK", 		0b0000000000010000);
-define("FILTER_CULTURE",	0b0000000000100000);
-define("FILTER_YORK",		0b0000000001000000);
+define("FILTER_ALL", 			16777216);
+define("FILTER_ALL_ARTICLES", 	511);
+define("FILTER_NEWS", 			1);
+define("FILTER_FEATURES", 		2);
+define("FILTER_LIFESTYLE", 		4);
+define("FILTER_FOOD",			8);
+define("FILTER_DRINK", 			16);
+define("FILTER_unused",			32);	//unused
+define("FILTER_ARTS",			64);
+define("FILTER_SPORTS",			128);
+define("FILTER_BLOGS",			256);
+define("FILTER_YORK",			512);
+define("FILTER_DIR",			1024);
+define("FILTER_EVENTS",			2048);
 
-class Search extends Model
-{
+class Search extends Model {
 	function Search()
 	{
 		//Call the Model Constructor
@@ -39,41 +44,128 @@ class Search extends Model
 	 * @param $ordering integer Ordering of the search, if defined.
 	 * @param $filter integer Filtering of the search, if defined.
 	 */
-	function full($string, $ordering = ORDER_RELEVANCE, $filter =FILTER_ALL) {
-		function ordering_addition($ordering, &$query) {
+	function full($string, $limit = 10, $offset = 0, $ordering = ORDER_RELEVANCE, $filter = FILTER_ALL) {
+		function ordering_addition($ordering) {
 			switch($ordering) {
-				case ORDER_RELEVANCE: return $this->db->orderby('something','desc');
+				case ORDER_RELEVANCE: return 'ORDER BY relevance DESC';
 				break;
-				case ORDER_EARLY: return $this->db->orderby('something', 'asc')
+				case ORDER_EARLY: return 'ORDER BY date ASC';
 				break;
-				case ORDER_LATE: return $this->db->orderby('something', 'desc')
+				case ORDER_LATE: return 'ORDER BY date DESC';
 				break;
-				default: return false;
-				break;
+				default: return '';
 			}
 		}
 		
 		$result = array();
-		
-		//TODO replace pseudocode
-		$query = $this->db->select('news title, text')->from('twelveteen tables')->join('table', 'conditions')
-		
-		if ($filter & FILTER_NEWS) {
-		}
-		if ($filter & FILTER_FEATURES) {
-		}
-		if ($filter & FILTER_LIFESTYLE) {
-		}
-		if ($filter & FILTER_FOOD) {
-		}
-		if ($filter & FILTER_DRINK) {
-		}
-		if ($filter & FILTER_CULTURE) {
+		if ($filter & FILTER_ALL_ARTICLES) {
+			$sql = <<<QUERY
+SELECT articles.article_id AS id,
+	UNIX_TIMESTAMP(articles.article_publish_date) AS date,
+	content_types.content_type_codename AS type_codename,
+	parent_type.content_type_codename AS parent_codename,
+	IF (content_types.content_type_parent_content_type_id IS NOT NULL,
+		CONCAT(parent_type.content_type_name, " - ", content_types.content_type_name),
+		content_types.content_type_name) AS type_name,
+	article_contents.article_content_heading AS heading,
+	article_contents.article_content_blurb AS blurb,
+	photo_requests.photo_request_chosen_photo_id AS photo_id,
+	photo_requests.photo_request_title AS photo_title
+	MATCH(article_content_heading, article_content_subheading, article_contents.article_content_blurb,article_content_wikitext_cache)
+		AGAINST(?) as score,
+FROM articles 
+LEFT JOIN photo_requests 
+	ON ( articles.article_thumbnail_photo_id = photo_requests.photo_request_relative_photo_number
+		AND articles.article_id = photo_requests.photo_request_article_id
+		AND photo_requests.photo_request_deleted = 0
+		AND photo_requests.photo_request_chosen_photo_id IS NOT NULL
+		AND photo_requests.photo_request_approved_user_entity_id IS NOT NULL ),
+	article_contents, content_types
+LEFT JOIN content_types AS parent_type
+	ON content_types.content_type_parent_content_type_id = parent_type.content_type_id
+WHERE
+	articles.article_content_type_id = content_types.content_type_id
+	AND articles.article_pulled = 0 AND articles.article_publish_date < CURRENT_TIMESTAMP()
+	AND articles.article_deleted = 0 AND articles.article_live_content_id IS NOT NULL
+	AND articles.article_live_content_id = article_contents.article_content_id
+	AND articles.article_id = article_contents.article_content_article_id
+	AND content_types.content_type_archive = 1
+	AND MATCH(article_content_heading, article_content_subheading, article_contents.article_content_blurb,article_content_wikitext_cache)
+AGAINST(?) ';
+QUERY;
+			if ($filter & FILTER_NEWS) {
+				$sql.= 'AND content_types.content_type_codename = `uninews` OR parent_type.content_type_codename = `uninews` ';
+			}
+			if ($filter & FILTER_FEATURES) {
+				$sql.= 'AND content_types.content_type_codename = `features` OR parent_type.content_type_codename = `features` ';
+			}
+			if ($filter & FILTER_LIFESTYLE) {
+				$sql.= 'AND content_types.content_type_codename = `lifestyle` OR parent_type.content_type_codename = `lifestyle` ';
+			}
+			if ($filter & FILTER_FOOD) {
+				$sql.= 'AND content_types.content_type_codename = `food` OR parent_type.content_type_codename = `food` ';
+			}
+			if ($filter & FILTER_DRINK) {
+				$sql.= 'AND content_types.content_type_codename = `drink` OR parent_type.content_type_codename = `drink` ';
+			}
+			if ($filter & FILTER_ARTS) {
+				$sql.= 'AND content_types.content_type_codename = `arts` OR parent_type.content_type_codename = `arts` ';
+			}
+			if ($filter & FILTER_SPORTS) {
+				$sql.= 'AND content_types.content_type_codename = `sport` OR parent_type.content_type_codename = `sport` ';
+			}
+			if ($filter & FILTER_BLOGS) {
+				$sql.= 'AND content_types.content_type_codename = `blogs` OR parent_type.content_type_codename = `blogs` ';
+			}
+			$sql.= ordering_addition($ordering);
+			$result['articles'] = $this->db->query($sql, array($string, $string));
 		}
 		if ($filter & FILTER_YORK) {
+			curl_init('http://yorkipedia.theyorker.co.uk/api.php?action=opensearch&search='.urlencode(utf8_decode($filter)));
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 0.2); //possibly not valid, manual says integer only, but we wil try...
+			$wikiResult = curl_exec($curl);
+			$result['wiki'] = array();
+			if ($wikiResult or $wikiResult != 1) {
+				//example result ["Tet",["Tet","Tet-off","Tet-on","TetR","Tet 1969","Tet Corporation"]]
+				preg_match_all("/[^.|\"\[\]]*/", $wikiResult, $wikiResult);
+				foreach ($wikiResult as $page) if (!empty($page) and $page != ',') $result['wiki'][] = $page;
+			}
 		}
-		$query = ordering_addition($ordering, $query);
-		...
+		if ($filter & FILTER_DIR) {
+			// This query is possibly even slower than the one above, because It has to match against
+			// two separate indexes!
+			$sql = <<<QUERY
+				SELECT
+					organisations.organisation_name AS heading,
+					organisations.organisation_directory_entry_name AS link,
+					organisation_contents.organisation_content_description AS blurb,
+					organisation_types.organisation_type_name,
+					UNIX_TIMESTAMP(organisation_timestamp) AS date,
+					SUM((MATCH(organisations.organisation_name) AGAINST(?)),
+						(MATCH(organisation_contents.organisation_content_description) AGAINST(?)))/2 AS score
+				FROM organisations
+				INNER JOIN organisation_types
+				ON organisations.organisation_organisation_type_id=organisation_types.organisation_type_id
+				LEFT JOIN organisation_contents
+				ON organisations.organisation_live_content_id = organisation_contents.organisation_content_id
+				INNER JOIN entities
+				ON entities.entity_id = organisations.organisation_entity_id
+				AND entities.entity_deleted = 0
+				WHERE organisations.organisation_directory_entry_name IS NOT NULL
+					AND organisation_types.organisation_type_directory=1
+					AND organisations.organisation_show_in_directory=1
+					AND organisations.organisation_needs_approval=0
+					AND (MATCH (organisations.organisation_name) AGAINST (?)
+					OR MATCH (organisation_contents.organisation_content_description) AGAINST (?))
+QUERY;
+			$sql.= ordering_addition($ordering);
+			$result['directory'] = $this->db->query($sql, array($string, $string, $string, $string));
+		}
+		if ($filter & FILTER_EVENTS) {
+			// TODO hook into James' code
+			$result['events'] = array();
+		}
 		return $result;
 	}
 
@@ -82,7 +174,8 @@ class Search extends Model
 	 * @param $string string Search string entered.
 	 */
 	function ajax($string) {
-		
+		// TODO improve this, we only need titles
+		return full($string, 3);
 	}
 
 ?>
