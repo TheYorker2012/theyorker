8432df3933e3100d3234536f796d2940c174dc56
Owen Jones <oj502@york.ac.uk>
2007-06-30 23:06:57 +0000

    Some improvement to the contacts card page in the directory, with a fact box. Number of subscribers still needs to be added. Better layout for pages with no data now.

--- system/application/views/directory/directory_view_members.php
+++ system/application/views/directory/directory_view_members.php
@@ -1,29 +1,52 @@
 <div id="RightColumn">
-	<h2 class="first">Groups</h2>
+	<h2 class="first">What's this?</h2>
+	<div class="Entry">
+		<?php if (isset($whats_this)) echo $whats_this; ?>
+	</div>
+	
+<?php
+	if(!empty($organisation['groups'])){
+?>
+<h2 class="first">Groups</h2>
 	<div class="Entry">
 <?php
-	foreach ($organisation['groups'] as $group) {
+		foreach ($organisation['groups'] as $group) {
 ?>
-		<a href='<?php echo(htmlspecialchars($group['href'])); ?>'>
-			<?php echo(htmlspecialchars($group['name'])); ?>
-		</a><br />
+			<a href='<?php echo(htmlspecialchars($group['href'])); ?>'>
+				<?php echo(htmlspecialchars($group['name'])); ?>
+			</a><br />
+<?php
+		}
+?>
+	</div>
 <?php
 	}
 ?>
-	</div>
 
 	<h2>Facts</h2>
 	<div class="entry">
+	<ul>
+		<li>Number of members : <?php if (isset($number_of_members)) echo $number_of_members; ?></li>
+		<li>Number of subscriptions : <?php if (isset($number_of_subscriptions)) echo $number_of_subscriptions; ?></li>
+		<li>Last member joined : <?php if (isset($last_joined)) echo $last_joined; ?></li>
+		<li>Male:Female ratio : <?php if (isset($male_female_ratio)) echo $male_female_ratio; ?></li>
+	</ul>
 	</div>
 </div>
 
 <div id="MainColumn">
 	<div class="BlueBox">
 <?php
-	if(empty($organisation['cards'])) {
+	if(empty($organisation['groups'])) {
 ?>
 		<div align="center">
-			<b>This organisation has not listed any members in this team.</b>
+			<b><?php if (isset($no_groups)) echo $no_groups; ?></b>
+		</div>
+<?php
+	} else if (empty($organisation['cards'])) {
+?>
+		<div align="center">
+			<b><?php if (isset($no_cards)) echo $no_cards; ?></b>
 		</div>
 <?php
 	} else {
