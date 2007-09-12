7a795a0359c809d452f6cef0033e5a99dc3a5f01
Andrew Oakley <ado@theyorker.co.uk>
2007-09-12 20:10:41 +0000

    Added some more google maps keys

--- system/application/views/frames/maps.php
+++ system/application/views/frames/maps.php
@@ -24,6 +24,12 @@ switch($_SERVER['SERVER_NAME']) {
 	case 'www.theyorker.co.uk':
 		$key = 'ABQIAAAAG00OtuAD_rmvNO96T7IKKRSsupC0bCgwgBIcSr9y7Z9nV9qoOBRUsXmbgvycoXlZCvBZpQ0TqmIW0A';
 		break;
+	case 'theyorker.ado.is-a-geek.net':
+		$key = 'ABQIAAAA4LuflJA4VPgM8D-gyba8yBQqxqph5xmYOwiXPhDCFStDHdEmQxStGW_JuIRgEXmatGyMz88xSyDOow';
+		break;
+	case 'trunk.theyorker.gmghosting.com':
+		$key = 'ABQIAAAA4LuflJA4VPgM8D-gyba8yBR6L4oJGHgAGz3lHpOpEmhODgCApRRRjD3jh4l746FkEpP3mk8KX1qNQQ';
+		break;
 	case 'localhost':
 		$key = 'ABQIAAAA6vFF9HQVRyZ6pmMbEW2o8hT4dMPT2p45abcp05Afs400sGBlHhRGtu7daesOnj_9G28sgfkXgxTfxQ';
 		break;
