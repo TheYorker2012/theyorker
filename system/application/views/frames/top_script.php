39ec6eb59f25f444964bffbfea20608394a39413
Andrew Oakley <ado@theyorker.co.uk>
2007-09-25 21:55:29 +0000

    Put javascript in a CDATA block

--- system/application/views/frames/top_script.php
+++ system/application/views/frames/top_script.php
@@ -4,17 +4,14 @@
 <script type="text/javascript" src="/javascript/search.js"></script>
 
 	<script type="text/javascript">
-	<!--
-	// -----------------------------------------------------------------------------
-	// Globals
+	//<![CDATA[
 	// Major version of Flash required
 	var requiredMajorVersion = 7;
 	// Minor version of Flash required
 	var requiredMinorVersion = 0;
 	// Minor version of Flash required
 	var requiredRevision = 0;
-	// -----------------------------------------------------------------------------
-	// -->
+	//]]>
 	</script>
 
 	<?php
