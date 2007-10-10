15eab9f12f1666f0911bea3729257956985b8c47
Andrew Oakley <ado@theyorker.co.uk>
2007-10-10 12:17:07 +0000

    Updated export script so it actually packages up the symlink and deletes it afterwards

--- export/doexport.sh
+++ export/doexport.sh
@@ -17,7 +17,7 @@ echo "Creating symlink"
 ln -s r${rev}/public_html new_html
 
 echo "Creating tarball..."
-tar -cf yorker.tar r${rev}/
+tar -cf yorker.tar r${rev} new_html
 
 echo "Copying tarball..."
 scp yorker.tar yorker@www.theyorker.co.uk:
@@ -25,3 +25,4 @@ scp yorker.tar yorker@www.theyorker.co.uk:
 echo "Deleting files..."
 rm -rf r${rev} > /dev/null
 rm yorker.tar > /dev/null
+rm new_html > /dev/null
