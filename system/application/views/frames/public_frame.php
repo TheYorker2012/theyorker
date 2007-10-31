1958de3a746f7ca54216aa1c0ceedc0c537a1d5a
Chris Travis <ctravis@gmail.com>
2007-10-31 15:26:38 +0000

    Setting up link for arts homepage on public frame

--- system/application/views/frames/public_frame.php
+++ system/application/views/frames/public_frame.php
@@ -111,7 +111,7 @@ if (isset($medium_type)) { ?>
 					<!--<li><a href="/news/national/">UK &amp; World News</a></li>-->
 					<li><a href="/news/features/">Features</a></li>
 					<li><a href="/lifestyle/">Lifestyle</a></li>
-					<li><a href="/news/arts/">Arts</a></li>
+					<li><a href="/arts/">Arts</a></li>
 					<li><a href="/news/blogs/">Blogs</a></li>
 					<li><a href="/reviews/food">Food</a></li>
 					<!--<li><a href="/reviews/drink">Drink</a></li>-->
