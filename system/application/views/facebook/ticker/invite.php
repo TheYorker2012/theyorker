8fc63dacb50ba69a661205d4590b759ec928aef1
Chris Travis <ctravis@gmail.com>
2007-11-03 02:50:56 +0000

    Altered the invite function on The Yorker Facebook Application so that its easier to tell who hasn't yet added the app so that invites can be used more effectively.

--- system/application/views/facebook/ticker/invite.php
+++ system/application/views/facebook/ticker/invite.php
@@ -2,5 +2,5 @@
 		method="post" invite="true" content="The Yorker provides online independent student
 		news. Why not check out the website and find out what's hot in York?
 		<fb:req-choice url='http://www.facebook.com/add.php?api_key=<?php echo($this->facebook_ticker->fb_config['ticker']['api_key']); ?>' label='Read the latest news!' />">
-			<fb:multi-friend-selector actiontext="Select the friends you wish to invite to add The Yorker application." bypass="cancel" />
+			<fb:multi-friend-selector actiontext="Your following friends haven't added the The Yorker application, why not invite them?" bypass="cancel" exclude_ids="<?php echo(implode(',', $app_users)); ?>" />
 		</fb:request-form>
