b0e4f4d41f7175078bb81364a2a0b4fefe5b4100
James Hogan <james_hogan@theyorker.co.uk>
2007-11-18 13:04:24 +0000

    Undid change to vip_frame in last merge that should not have been there. i suspect i wasn't up to date when i merged or had local changes. wierd\!

--- system/application/views/frames/vip_frame.php
+++ system/application/views/frames/vip_frame.php
@@ -93,8 +93,8 @@ echo('<?xml version="1.0" encoding="UTF-8"?>');
 				</ul>
 				<ul>
 					<li class="first"><a href="<?php echo vip_url('directory/information'); ?>">Directory Entry</a></li>
-					<li><a href="<?php echo(vip_url('calendar')); ?>">Manage Events</a></li>
 					<?php /*
+					<li><a href="<?php echo(vip_url('calendar')); ?>">Manage Events</a></li>
 					<li><a href="<?php echo vip_url('notices'); ?>">Manage Notices</a></li>
 					*/ ?>
 					<li><a href="<?php echo vip_url('members'); ?>">Manage Members</a></li>
