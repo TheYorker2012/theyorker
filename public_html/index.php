91b4bec7a6e11dcdc888aee49fe03fb77c71c14f
Andrew Oakley <ado@theyorker.co.uk>
2008-01-12 20:59:50 +0000

    Added page render time comment

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,10 @@
 <?php
 error_reporting(E_ALL);
 
+if (!isset($time)) {
+	$time = microtime();
+}
+
 define('BASEPATH', '../system/');
 define('APPPATH', '../system/application/');
 define('EXT', '.php');
@@ -8,4 +12,13 @@ define('FCPATH', __FILE__);
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
 require_once BASEPATH.'codeigniter/CodeIgniter'.EXT;
+
+/* If we are sending html, add the render time */
+$headers = headers_list();
+foreach ($headers as $header) {
+	if (strpos($header, 'Content-type: text/html') !== FALSE) {
+		echo('<!-- Rendered in '.(microtime() - $time).' seconds -->');
+		break;
+	}
+}
 ?>
