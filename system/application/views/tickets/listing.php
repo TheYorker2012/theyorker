2803e6e22bb2735f81881c88f596fd6fec869679
Richard Ingle <ri504@york.ac.uk>
2008-01-20 15:15:25 +0000

    barebones (controller, view) for tickets system

---
+++ system/application/views/tickets/listing.php
@@ -0,0 +1,20 @@
+<div id="RightColumn">
+	<h2 class="first">
+		<?php //echo($links_heading); ?>
+	</h2>
+	<div class="Entry">
+		<?php //$this->homepage_boxes->print_puffer_column($puffers); ?>
+	</div>
+</div>
+<div id="MainColumn">
+	<div id="HomeBanner">
+		<?php 
+		//$this->homepage_boxes->print_homepage_banner($banner);
+		?>
+	</div>
+	<?php
+	//$this->homepage_boxes->print_box_with_picture_list($main_articles,$latest_heading,'news');
+	//if($show_featured_puffer) $this->homepage_boxes->print_specials_box($featured_puffer_title,$featured_puffer);
+	//if(!empty($lists_of_more_articles)) $this->homepage_boxes->print_box_of_category_lists($more_heading,$more_article_types,$lists_of_more_articles);
+	?>
+</div>
