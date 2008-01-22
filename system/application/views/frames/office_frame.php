96ad2ae484446147dc006278decbc41bbb4de3de
James Hogan <james_hogan@theyorker.co.uk>
2008-01-22 22:53:11 +0000

    Re-enable polls link in 1.1.3

--- system/application/views/frames/office_frame.php
+++ system/application/views/frames/office_frame.php
@@ -151,11 +151,9 @@ echo('<?xml version="1.0" encoding="UTF-8"?>');
 		<div class='officenavigation_item'>
 			<a href='/office/advertising'>Advertising</a>
 		</div>
-		<?php /*
 		<div class='officenavigation_item'>
 			<a href='/office/polls'>Polls</a>
 		</div>
-		*/ ?>
 <?php
 	}
 ?>
