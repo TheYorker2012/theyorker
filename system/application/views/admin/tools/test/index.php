743eeb603f9bb4da5be0aaf958ee60dfdf2a9372
James Hogan <james_hogan@theyorker.co.uk>
2008-02-02 21:40:05 +0000

    Simple admin area web interface for static analyser. 	/admin/tools/test has /admin/tools/test/static which uses static analysis perl script 	added xml and plain text general views

---
+++ system/application/views/admin/tools/test/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file views/admin/tools/test/index.php
+ * @brief Release tests index page.
+ * @author James Hogan (jh559@cs.york.ac.uk)
+ */
+?>
+<div id="RightColumn">
+	<div>
+		<h2 class="first">what&apos;s this?</h2>
+		<p>Release tests should be run before updating the live site</p>
+	</div>
+</div>
+<div id="MainColumn">
+	<div class="BlueBox">
+		<h2>release tests</h2>
+		<ul>
+			<li><a href="<?php echo(site_url('admin/tools/test/static')); ?>">Static Analyser</a></li>
+		</ul>
+	</div>
+</div>
