971c7213d19da5799f55d301ac8de7ac8f4c9c52
James Hogan <james_hogan@theyorker.co.uk>
2008-02-05 22:29:14 +0000

    [wiki:branches/htmlentities]: Fix for content schedule showing br's they were actually being inserted between authors by database query. it now just adds newlines which are changed to br's after escaping in the view

--- system/application/views/office/news/content_schedule.php
+++ system/application/views/office/news/content_schedule.php
@@ -28,7 +28,7 @@
 	<td><?php echo(xml_escape($article['content_type_name'])); ?></td>
 	<td><a href="/office/news/<?php echo($article['article_id']); ?>/" title="Edit this article"><?php echo((strlen($article['headline']) == 0 ? 'No title' : xml_escape($article['headline'] ))); ?></a></td>
 	<td><?php echo(xml_escape($article['editor'])); ?></td>
-	<td><?php echo(xml_escape($article['authors'])); ?></td>
+	<td><?php echo(nl2br(xml_escape($article['authors']))); ?></td>
 	<td><?php echo(($article['is_accepted'] ? '<img src="/images/prototype/news/accepted.gif" title="Accepted" alt="Accepted" />' : ($article['is_requested'] ? '<img src="/images/prototype/news/requested.gif" title="Requested" alt="Requested" />' : '<img src="/images/prototype/news/unassigned.png" title="Unassigned" alt="Unassigned" />') )); ?></td>
 	</tr>
 
