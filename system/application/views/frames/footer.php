3a8e9ec9efe6e8e7a8e08e594f4732460d2fb52e
Chris Travis <ctravis@gmail.com>
2008-02-12 17:42:40 +0000

    Google Analytics - Updating javascript to use new tracker

--- system/application/views/frames/footer.php
+++ system/application/views/frames/footer.php
@@ -43,10 +43,12 @@
 	</small>
 </div>
 
-<script src="https://ssl.google-analytics.com/urchin.js" type="text/javascript">
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-_uacct = "UA-864229-2";
-urchinTracker();
+var pageTracker = _gat._getTracker("UA-864229-2");
+pageTracker._initData();
+pageTracker._trackPageview();
 </script>
-
