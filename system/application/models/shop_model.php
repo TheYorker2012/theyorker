89e9d432a92bc4686d43ba4e52f69053c85d773e
Tim Fletcher <tjf500@york.ac.uk>
2008-02-17 21:04:44 +0000

    Remove erroneous comma

--- system/application/models/shop_model.php
+++ system/application/models/shop_model.php
@@ -275,7 +275,7 @@ class Shop_model extends Model
 			$basket_item['customisations'] = $query2->result_array();
 
 			$sql = 'SELECT	shop_item_name as name,
-							shop_item_description as description,
+							shop_item_description as description
 					FROM	shop_items
 					WHERE	shop_item_id = ?';
 			$query3 = $this->db->query($sql, array($basket_item['item_id']));
