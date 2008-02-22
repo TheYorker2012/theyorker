55f205613d4a0afb14c89100cae0db803a28819b
Chris Travis <ctravis@gmail.com>
2008-02-22 23:30:06 +0000

    Merged [log:trunk@4229:4235] into [wiki:branches/1.1.4]

--- system/application/controllers/charity.php
+++ system/application/controllers/charity.php
@@ -58,8 +58,12 @@ class Charity extends Controller {
 		else
 		{
 			$data['sections']['progress_reports'] = array('title'=>$this->pages_model->GetPropertyText('section_progress_reports_title',TRUE),'showmore'=>$this->pages_model->GetPropertyWikitext('section_preports_showmore',FALSE));
-			$data['sections']['funding'] = array('title'=>$this->pages_model->GetPropertyText('section_funding_title',FALSE),'text'=>$this->pages_model->GetPropertyWikitext('section_funding_text',FALSE,FALSE,
-				array('%%target%%' => $data['sections']['charity']['target'])));
+			$data['sections']['funding'] = array(
+				'title'=>$this->pages_model->GetPropertyText('section_funding_title',FALSE),
+				'text'=>$this->pages_model->GetPropertyWikitext('section_funding_text',FALSE,FALSE, array(
+							'target' => array($data['sections']['charity']['target'], true),
+				)),
+			);
 			$data['sections']['sidebar_about'] = array('title'=>$this->pages_model->GetPropertyText('sidebar_about_title',FALSE),'text'=>$this->pages_model->GetPropertyWikitext('sidebar_about_text',FALSE));
 			$data['sections']['sidebar_help'] = array('title'=>$this->pages_model->GetPropertyText('sidebar_help_title',FALSE),'text'=>$this->pages_model->GetPropertyWikitext('sidebar_help_text',FALSE));
 			$data['sections']['sidebar_related'] = array('title'=>$this->pages_model->GetPropertyText('sidebar_related_title',TRUE));
--- system/application/views/office/bylines/byline.php
+++ system/application/views/office/bylines/byline.php
@@ -1,6 +1,6 @@
 	<div class="BlueBox">
 		<div style="float:right;margin:0.2em 0.5em;text-align:right">
-			<img src="<?php echo(($business_card_image_href != '') ? $business_card_image_href : 'http://www.theyorker.co.uk/images/prototype/directory/members/no_image.png'); ?>" alt="<?php echo(xml_escape($business_card_name)); ?>" title="<?php echo(xml_escape($business_card_name)); ?>" />
+			<img src="<?php echo(($business_card_image_href != '') ? $business_card_image_href : site_url('images/prototype/directory/members/no_image.png')); ?>" alt="<?php echo(xml_escape($business_card_name)); ?>" title="<?php echo(xml_escape($business_card_name)); ?>" />
 		</div>
 
 		<div style="background-color:#20c1f0;color:#fff;padding:0.2em;margin:0;font-weight:bold;font-size:medium;">
@@ -19,44 +19,44 @@
 
 		<div>
 			<?php if ($business_card_course !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/script.png" alt="" title="Course" />
+				<img src="<?php echo site_url('images/icons/script.png'); ?>" alt="Course" title="Course" />
 				<b>Course:</b>
 				<?php echo(xml_escape($business_card_course)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($business_card_email !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/email.png" alt="" title="E-Mail Address" />
+				<img src="<?php echo site_url('images/icons/email.png'); ?>" alt="E-Mail Address" title="E-Mail Address" />
 				<b>E-Mail:</b>
 				<?php echo(xml_escape($business_card_email)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($business_card_phone_internal !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/phone.png" alt="" title="Internal Phone" />
+				<img src="<?php echo site_url('images/icons/phone.png'); ?>" alt="Internal Phone" title="Internal Phone" />
 				<b>Phone (Int):</b>
 				<?php echo(xml_escape($business_card_phone_internal)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($business_card_phone_external !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/phone.png" alt="" title="External Phone" />
+				<img src="<?php echo site_url('images/icons/phone.png'); ?>" alt="External Phone" title="External Phone" />
 				<b>Phone (Ext):</b>
 				<?php echo(xml_escape($business_card_phone_external)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($business_card_mobile !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/phone.png" alt="" title="Mobile" />
+				<img src="<?php echo site_url('images/icons/phone.png'); ?>" alt="Mobile" title="Mobile" />
 				<b>Mobile:</b>
 				<?php echo(xml_escape($business_card_mobile)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($business_card_postal_address !== NULL) { ?>
-				<img src="http://www.theyorker.co.uk/images/icons/map.png" alt="" title="Address" />
+				<img src="<?php echo site_url('images/icons/map.png'); ?>" alt="Address" title="Address" />
 				<b>Address:</b>
 				<?php echo(xml_escape($business_card_postal_address)); ?>
 				<br />
 			<?php } ?>
 			<?php if ($archive_link) { ?>
-				<a href="http://www.theyorker.co.uk/news/archive/reporter/<?php echo($business_card_id); ?>/">
-					<img src="http://www.theyorker.co.uk/images/icons/book_open.png" alt="Archive" title="Archive" />
+				<a href="<?php echo site_url('news/archive/reporter/' . $business_card_id . '/'); ?>">
+					<img src="<?php echo site_url('images/icons/book_open.png'); ?>" alt="Archive" title="Archive" />
 					View articles I have written
 				</a>
 			<?php } ?>
