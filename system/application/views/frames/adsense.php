bf684c09e7d207edb48833e2646752ba356cb15e
Chris Travis <ctravis@gmail.com>
2008-02-26 22:16:12 +0000

    Moving adsense down to same position as other ads

--- system/application/views/frames/adsense.php
+++ system/application/views/frames/adsense.php
@@ -1,10 +1,12 @@
-<script type="text/javascript">
-<!--
-google_ad_client = "pub-8676956632365960";
-/* 120x600, created 26/02/08 */
-google_ad_slot = "7544347244";
-google_ad_width = 120;
-google_ad_height = 600;
-//-->
-</script>
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+<div style="margin-top: 40px;">
+	<script type="text/javascript">
+	<!--
+	google_ad_client = "pub-8676956632365960";
+	/* 120x600, created 26/02/08 */
+	google_ad_slot = "7544347244";
+	google_ad_width = 120;
+	google_ad_height = 600;
+	//-->
+	</script>
+	<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+</div>
