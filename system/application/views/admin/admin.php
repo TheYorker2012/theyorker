f967daa9d0800331a4d8e87729915d033fc6dcf3
Owen Jones <oj502@york.ac.uk>
2008-04-13 15:24:05 +0000

    Updated the office contact us, and added in link to admin page.

--- system/application/views/admin/admin.php
+++ system/application/views/admin/admin.php
@@ -4,6 +4,7 @@
 		<p>The admin section isnt used for much anymore and there probably isnt any need to see this page, someone might move everything into the office one day.</p>
 		<ul>
 			<li><a href="/admin/useradmin/edit">Edit User Information</a></li>
+			<li><a href="/office/contactus">Edit contact us address'</a></li>
 			<li><a href="/admin/feedback">Feedback (<?php echo $feedback_count; ?> entries)</a></li>
 			<li><a href="/admin/pages">Page properties, custom pages, etc.</a></li>
 			<li><a href="/admin/dev">Webserver information</a></li>
