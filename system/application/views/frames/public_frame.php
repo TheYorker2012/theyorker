06e018ee962d5d3fbba2d133300542553b862bda
Chris Travis <ctravis@gmail.com>
2008-11-24 23:48:22 +0000

    Changing menu link to food section

--- system/application/views/frames/public_frame.php
+++ system/application/views/frames/public_frame.php
@@ -109,7 +109,7 @@ if (isset($medium_type)) { ?>
 					<li><a href="/lifestyle/">Lifestyle</a></li>
 					<li><a href="/arts/">Arts</a></li>
 					<li><a href="/blogs/">Blogs</a></li>
-					<li><a href="/reviews/food">Food</a></li>
+					<li><a href="/food/">Food</a></li>
 					<li><a href="/reviews/drink">Drink</a></li>
 					<?php /*
 					<li><a href="/reviews/culture">Culture</a></li>
