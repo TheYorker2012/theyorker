358942de07923f8df38e6461fcecf7d77da13b19
James Hogan <james_hogan@theyorker.co.uk>
2009-04-05 21:23:38 +0100

    add alt tags on public category page crossword images to improve validation

--- system/application/views/crosswords/index.php
+++ system/application/views/crosswords/index.php
@@ -40,7 +40,7 @@
 				?><div style="clear: both"></div><?php
 				?><div class="crossword_preview"><?php
 					?><a href="<?php echo(site_url('crosswords/'.$crossword['id'])); ?>"><?php
-						?><img src="<?php echo(site_url('images/crosswords/xw.png')); ?>" /><?php
+						?><img alt="" src="<?php echo(site_url('images/crosswords/xw.png')); ?>" /><?php
 					?></a><?php
 				?></div><?php
 				?><div class="crossword_title"><?php
