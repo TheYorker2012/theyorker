337b54f68ea5ca9905cea626bbc7c5ae51c14245
Chris Travis <ctravis@gmail.com>
2009-05-08 02:29:53 +0100

    Testing new adsense ad

--- system/application/views/general/home.php
+++ system/application/views/general/home.php
@@ -72,8 +72,17 @@ function changePreview (option, article_id, article_type, photo_id, photo_title)
 <div class="clear"></div>
 
 <div class="FlexiBox Box12">
-	<div class="FlexiBox Box12b">
-		<br /><br /><br /><br /><br />
+	<div class="FlexiBox Box12">
+		<script type="text/javascript">
+		<!--
+		google_ad_client = "pub-8676956632365960";
+		/* 468x60, created 07/05/09 */
+		google_ad_slot = "5537704417";
+		google_ad_width = 468;
+		google_ad_height = 60;
+		//-->
+		</script>
+		<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 	</div>
 	<div class="ArticleListBox FlexiBox Box12">
 		<div class="ArticleListTitle"><a href="/crosswords">latest crosswords</a></div>
