1f5c6794212fd84fca356f47adbddad9d2c5e910
root <root@theyorker.co.uk>
2009-06-10 16:53:26 +0000

    Fixing typo

--- system/application/libraries/Account_personal.php
+++ system/application/libraries/Account_personal.php
@@ -40,7 +40,7 @@ class Account_personal extends FramesFrame
 		$rules['facebook'] = '';
 		$rules['nick'] = 'trim|required|alpha_numeric';
 		$rules['gender'] = 'trim';
-		$rules['college'] = 'trim|required|is)numeric';
+		$rules['college'] = 'trim|required|is_numeric';
 		$rules['year'] = 'trim|required|is_numeric';
 		$rules['time'] = 'trim|required|numeric';
 		$CI->validation->set_rules($rules);
